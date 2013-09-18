e23e8d10a78ff7187de6326076c8a953be17e79d
Michael Gevlich <zemledelec@openglobus.org>
2013-09-18 15:26:23 +0400

    ShowFps Control creates dom element div by myself.

--- src/og/control/showFps.js
+++ src/og/control/showFps.js
@@ -4,7 +4,15 @@ og.control.ShowFps = function (options) {
 
 og._class_.extend(og.control.ShowFps, og.control.Control);
 
+og.control.ShowFps.prototype.init = function () {
+    var d = document.createElement('div');
+    d.className = 'defaultText ';
+    d.id = "ogShowFpsControl";
+    document.body.appendChild(d);
+};
+
+
 og.control.ShowFps.prototype.draw = function () {
-    print2d("lbFps", this.renderer.ctx.fps.toFixed(1), this.renderer.ctx.gl._viewportWidth - 40, 0);
+    print2d("ogShowFpsControl", this.renderer.ctx.fps.toFixed(1), this.renderer.ctx.gl._viewportWidth - 40, 0);
 };
 
