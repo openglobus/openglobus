133a672b5dd71fd57a3f664d746efeccc4623226
Michael Gevlich <mgevlich@gmail.com>
2013-09-21 11:04:41 +0400

    Improve Layer switcher style.

--- examples/main.js
+++ examples/main.js
@@ -27,6 +27,7 @@ og.start = function() {
 
     var layer = new og.layer.XYZ("OpenStreetMap", { isBaseLayer: true, url: og.layer.MapServersProxy.OSMb.url });
     var satlayer = new og.layer.XYZ("MapQuest Satellite", { isBaseLayer: true, url: og.layer.MapServersProxy.MapQuestSat.url, visibility: true });
+    var mqosm = new og.layer.XYZ("MapQuest", { isBaseLayer: true, url: og.layer.MapServersProxy.MapQuest.url });
     var kosmosnim = new og.layer.XYZ("Kosmosnimki", { isBaseLayer: true, url: og.layer.MapServersProxy.Cosmosnimki.url });
     var kray5m = new og.layer.WMS("geoserver:Kray5m", { isBaseLayer: true, url: "http://127.0.0.1/geoserver/gwc/service/", layers: "lem3d:kray5m" });
 
@@ -36,7 +37,7 @@ og.start = function() {
         minZoom: og.terrainProvider.TerrainServers.OpenGlobus.minZoom
     });
 
-    planet.addLayers([layer, satlayer, kosmosnim, kray5m]);
+    planet.addLayers([layer, satlayer, mqosm, kosmosnim, kray5m]);
     planet.setBaseLayer(satlayer);
     planet.setTerrainProvider(terrain);
 
