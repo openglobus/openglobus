c381f60b3040dd1566442f87dd289731b0b14369
Michael Gevlich <mgevlich@gmail.com>
2014-01-07 13:10:30 +0400

    Framebuffer experiment has started.

--- src/og/webgl/framebuffer.js
+++ src/og/webgl/framebuffer.js
@@ -15,29 +15,42 @@ og.webgl.Framebuffer.prototype.initialize = function () {
     this.fbo = gl.createFramebuffer();
     this.width = gl._viewportWidth;
     this.height = gl._viewportHeight;
+    this.createBufferTexture();
+};
+
+og.webgl.Framebuffer.prototype.createBufferTexture = function () {
+    var gl = this.handler.gl;
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.fbo);
+
+    this.fboTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, this.fboTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.width, this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+    var renderbuffer = gl.createRenderbuffer();
+    gl.bindRenderbuffer(gl.RENDERBUFFER, renderbuffer);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, this.width, this.height);
+
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.fboTexture, 0);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, renderbuffer);
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 };
 
 og.webgl.Framebuffer.prototype.setSize = function (width, height) {
     this.width = width;
     this.height = height;
-    this.createTexture();
+    this.createBufferTexture();
 };
 
-og.webgl.Framebuffer.prototype.createTexture = function () {
-    var gl = this.handler.gl;
-    this.fboTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, this.fboTexture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.width, this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.fboTexture, 0);
-};
-
-og.webgl.Framebuffer.prototype.bind = function () {
+og.webgl.Framebuffer.prototype.startFrame = function () {
     var gl = this.handler.gl;
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.fbo);
 };
 
-og.webgl.Framebuffer.prototype.clear = function () {
+og.webgl.Framebuffer.prototype.endFrame = function () {
     var gl = this.handler.gl;
-    gl.bindTexture(gl.TEXTURE_2D, null);
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 };
