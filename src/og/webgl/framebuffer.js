0eb0e334dbd73197b5494415fe5b9050829bc241
Michael Gevlich <mgevlich@gmail.com>
2014-01-08 12:28:18 +0400

    Safety framebuffer.

--- src/og/webgl/framebuffer.js
+++ src/og/webgl/framebuffer.js
@@ -8,6 +8,7 @@ og.webgl.Framebuffer = function (handler) {
     this.width;
     this.height;
     this.fboTexture = null;
+    this.activated = false;
 };
 
 og.webgl.Framebuffer.prototype.initialize = function () {
@@ -15,14 +16,13 @@ og.webgl.Framebuffer.prototype.initialize = function () {
     this.fbo = gl.createFramebuffer();
     this.width = gl._viewportWidth;
     this.height = gl._viewportHeight;
-    this.createBufferTexture();
+    this._createTexture();
 };
 
-og.webgl.Framebuffer.prototype.createBufferTexture = function () {
+og.webgl.Framebuffer.prototype._createTexture = function () {
     var gl = this.handler.gl;
 
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this.fbo);
-
+    this.activate();
     this.fboTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, this.fboTexture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.width, this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
@@ -36,30 +36,42 @@ og.webgl.Framebuffer.prototype.createBufferTexture = function () {
 
     gl.bindTexture(gl.TEXTURE_2D, null);
     gl.bindRenderbuffer(gl.RENDERBUFFER, null);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    this.deactivate();
 };
 
 og.webgl.Framebuffer.prototype.setSize = function (width, height) {
     this.width = width;
     this.height = height;
-    this.createBufferTexture();
+    this._createTexture();
 };
 
 og.webgl.Framebuffer.prototype.isComplete = function () {
-    var gl = this.handler.gl;
-    var status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
-    if (status == gl.FRAMEBUFFER_COMPLETE)
-        return true;
+    if (this.activated) {
+        var gl = this.handler.gl;
+        var status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
+        if (status == gl.FRAMEBUFFER_COMPLETE)
+            return true;
+    }
     return false;
 };
-
 
 og.webgl.Framebuffer.prototype.activate = function () {
     var gl = this.handler.gl;
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.fbo);
+    this.activated = true;
+};
+
+og.webgl.Framebuffer.prototype.clear = function () {
+    if (this.activated) {
+        var gl = this.handler.gl;
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    }
 };
 
 og.webgl.Framebuffer.prototype.deactivate = function () {
-    var gl = this.handler.gl;
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    if (this.activated) {
+        var gl = this.handler.gl;
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        this.activated = false;
+    }
 };
