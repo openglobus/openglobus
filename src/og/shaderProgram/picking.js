54bfa369047fe102d3c8ae3db9e690ff43297ea5
Michael Gevlich <mgevlich@gmail.com>
2014-01-09 19:12:34 +0400

    Picking shader added. Planet segment color picking begins! Get prepare for the depth texture picking:)

---
+++ src/og/shaderProgram/picking.js
@@ -0,0 +1,18 @@
+goog.provide('og.shaderProgram.picking');
+
+goog.require('og.shaderProgram');
+goog.require('og.shaderProgram.ShaderProgram');
+goog.require('og.shaderProgram.types');
+goog.require('og.utils');
+
+og.shaderProgram.picking = new og.shaderProgram.ShaderProgram("picking", {
+    uniforms: {
+        uPMVMatrix: { type: og.shaderProgram.types.MAT4 },
+        uColor: { type: og.shaderProgram.types.VEC3 }
+    },
+    attributes: {
+        aVertexPosition: { type: og.shaderProgram.types.VEC3, enableArray: true }
+    },
+    vertexShader: og.utils.readTextFile(og.shaderProgram.SHADERS_URL + "picking_vs.txt"),
+    fragmentShader: og.utils.readTextFile(og.shaderProgram.SHADERS_URL + "picking_fs.txt")
+});
