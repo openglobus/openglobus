4a7643ac1662d9dda9c16658b863e1ce0e11cac7
Michael Gevlich <mgevlich@gmail.com>
2014-02-06 23:01:23 +0400

    Added event stop listening.

--- src/og/events/events.js
+++ src/og/events/events.js
@@ -4,18 +4,25 @@ og.Events = function () { };
 
 og.Events.prototype.registerNames = function (eventNames) {
     for (var i = 0; i < eventNames.length; i++) {
-        this[eventNames[i]] = [];
+        this[eventNames[i]] = { "ready": true, "handlers": [] };
     }
 };
 
 og.Events.prototype.addEvent = function (name, sender, callback) {
-    this[name].push({ sender: sender, callback: callback });
+    this[name].handlers.push({ sender: sender, callback: callback });
 };
 
-og.Events.prototype.callEvents = function (events, obj) {
-    var i = events.length;
-    while (i--) {
-        var e = events[i];
-        e.callback.call(e.sender, obj);
+og.Events.prototype.stopListen = function (event) {
+    event.ready = false;
+};
+
+og.Events.prototype.callEvents = function (event, obj) {
+    if (event.ready) {
+        var h = event.handlers;
+        var i = h.length;
+        while (i--) {
+            var e = h[i];
+            e.callback.call(e.sender, obj);
+        }
     }
 };
