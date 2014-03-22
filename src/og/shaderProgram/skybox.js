6156b176017de6fcd9815f857c83bf911dcdd31b
Michael Gevlich <mgevlich@gmail.com>
2014-03-22 14:14:45 +0400

    SkyBox shaders refactoring.

--- src/og/shaderProgram/skybox.js
+++ src/og/shaderProgram/skybox.js
@@ -8,12 +8,11 @@ goog.require('og.utils');
 og.shaderProgram.skybox = new og.shaderProgram.ShaderProgram("skybox", {
     uniforms: {
         uPMVMatrix: { type: og.shaderProgram.types.MAT4 },
-        uSampler: { type: og.shaderProgram.types.SAMPLER2D },
+        uSampler: { type: og.shaderProgram.types.SAMPLERCUBE },
         pos: { type: og.shaderProgram.types.VEC3 }
     },
     attributes: {
-        aVertexPosition: { type: og.shaderProgram.types.VEC3, enableArray: true },
-        aTextureCoord: { type: og.shaderProgram.types.VEC2, enableArray: true }
+        aVertexPosition: { type: og.shaderProgram.types.VEC3, enableArray: true }
     },
     vertexShader: og.utils.readTextFile(og.shaderProgram.SHADERS_URL + "skybox_vs.txt"),
     fragmentShader: og.utils.readTextFile(og.shaderProgram.SHADERS_URL + "skybox_fs.txt")
