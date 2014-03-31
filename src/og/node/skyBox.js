e4b5f6f7b4341774afbc64c5e0757b2b6c7cba3b
Michael Gevlich <mgevlich@gmail.com>
2014-03-31 18:35:44 +0400

    SkyBos frame optimization.

--- src/og/node/skyBox.js
+++ src/og/node/skyBox.js
@@ -23,18 +23,30 @@ og.node.SkyBox.prototype.initialization = function () {
 
 og.node.SkyBox.prototype.frame = function () {
     var h = this.renderer.handler;
-    h.gl.disable(h.gl.DEPTH_TEST);
-
+    var gl = h.gl;
+    var cam = this.renderer.activeCamera;
+    gl.disable(h.gl.DEPTH_TEST);
     h.shaderPrograms.skybox.activate();
-    h.shaderPrograms.skybox.set({
-        uPMVMatrix: this.renderer.activeCamera.pmvMatrix._m,
-        pos: this.renderer.activeCamera.eye.toVec(),
-        uSampler: this.texture,
-        aVertexPosition: this.vertexPositionBuffer
-    });
-    h.shaderPrograms.skybox.drawArray(this.drawMode, this.vertexPositionBuffer.numItems);
-
+    sh = h.shaderPrograms.skybox._program;
+    var shu = sh.uniforms;
+    gl.uniformMatrix4fv(shu.uPMVMatrix._pName, false, cam.pmvMatrix._m);
+    gl.uniform3fv(shu.pos._pName, cam.eye.toVec());
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, this.texture);
+    gl.uniform1i(shu.uSampler._pName, 0);
+    var buf = this.vertexPositionBuffer;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+    gl.vertexAttribPointer(sh.attributes.aVertexPosition._pName, buf.itemSize, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(this.drawMode, 0, buf.numItems);
     h.gl.enable(h.gl.DEPTH_TEST);
+    //The same but slower
+    //h.shaderPrograms.skybox.set({
+    //    uPMVMatrix: this.renderer.activeCamera.pmvMatrix._m,
+    //    pos: this.renderer.activeCamera.eye.toVec(),
+    //    uSampler: this.texture,
+    //    aVertexPosition: this.vertexPositionBuffer
+    //});
+    //h.shaderPrograms.skybox.drawArray(this.drawMode, this.vertexPositionBuffer.numItems);
 };
 
 og.node.SkyBox.prototype.createBuffers = function () {
