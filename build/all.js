5a8ed1f6fc377e6052b9c7e738f1020c45429f42
Michael Gevlich <mgevlich@gmail.com>
2014-04-01 22:42:23 +0400

    last build just compiled.

--- build/all.js
+++ build/all.js
@@ -8,6 +8,7 @@ goog.require('og.layer');
 goog.require('og.layer.XYZ');
 goog.require('og.layer.WMS');
 goog.require('og.terrainProvider.TerrainProvider');
+goog.require('og.node.SkyBox');
 goog.require('og.control.MouseNavigation');
 goog.require('og.control.KeyboardNavigation');
 goog.require('og.control.LayerSwitcher');
@@ -15,5 +16,6 @@ goog.require('og.control.ToggleWireframe');
 goog.require('og.control.LoadingSpinner');
 goog.require('og.control.MousePosition');
 goog.require('og.ellipsoid.wgs84');
+goog.require('og.Globus');
 
 og.all = function(){};
