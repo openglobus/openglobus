e99b5f2de832682ef5e1b9bedface3aa42bc979d
Zemledelec <zemledelec@openglobus.org>
2014-08-08 17:29:56 +0400

    Poles WMS query extents fixed.

--- src/og/layer/wms.js
+++ src/og/layer/wms.js
@@ -13,7 +13,7 @@ og.inheritance.extend(og.layer.WMS, og.layer.XYZ);
 og.layer.WMS.prototype.GetHTTPRequestString = function (segment) {
     return this.url + "wms?" + "LAYERS=" + this.layers +
             "&FORMAT=image/jpeg&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap" +
-            "&SRS=" + "EPSG:900913" +
+            "&SRS=" + segment._projection.code +
             "&BBOX=" + segment.extent.getWest() + "," + segment.extent.getSouth() + "," + segment.extent.getEast() + "," + segment.extent.getNorth() +
             "&WIDTH=" + (this.width ? this.width : 256) +
             "&HEIGHT=" + (this.height ? this.height : 256);
