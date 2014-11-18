c195260ced42f7e40e3d8673881455ad6ea486f9
Zemledelec <zemledelec@openglobus.org>
2014-11-18 15:49:39 +0300

    Sorted by codes.

--- src/og/input/input.js
+++ src/og/input/input.js
@@ -1,20 +1,22 @@
 goog.provide('og.input');
 
-og.input.KEY_SPACE = 32;
-og.input.KEY_H = 72;
-og.input.KEY_C = 67;
-og.input.KEY_X = 88;
-og.input.KEY_E = 69;
-og.input.KEY_Q = 81;
-og.input.KEY_W = 87;
-og.input.KEY_S = 83;
-og.input.KEY_A = 65;
-og.input.KEY_D = 68;
 og.input.KEY_SHIFT = 16;
+og.input.KEY_SPACE = 32;
 og.input.KEY_LEFT = 37;
-og.input.KEY_RIGHT = 39;
 og.input.KEY_UP = 38;
+og.input.KEY_RIGHT = 39;
 og.input.KEY_DOWN = 40;
+og.input.KEY_A = 65;
+og.input.KEY_C = 67;
+og.input.KEY_D = 68;
+og.input.KEY_E = 69;
+og.input.KEY_H = 72;
+og.input.KEY_L = 76;
+og.input.KEY_Q = 81;
+og.input.KEY_S = 83;
+og.input.KEY_V = 86;
+og.input.KEY_W = 87;
+og.input.KEY_X = 88;
 og.input.KEY_F1 = 112;
 
 og.input.MB_LEFT = 0;
