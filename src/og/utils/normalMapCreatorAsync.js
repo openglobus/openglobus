5d815d3ec05d980939b51e444ad7d30a7390fd5c
Zemledelec <zemledelec@openglobus.org>
2014-11-18 15:48:44 +0300

    Normal map creators added.

---
+++ src/og/utils/normalMapCreatorAsync.js
@@ -0,0 +1,52 @@
+goog.provide('og.utils.NormalMapCreatorAsync');
+
+goog.require('og.inheritance');
+goog.require('og.utils.NormalMapCreator');
+
+og.utils.NormalMapCreatorAsync = function (width, height) {
+    og.inheritance.base(this, width, height);
+
+    this._counter = 0;
+    this._pendingsQueue = [];
+};
+
+og.inheritance.extend(og.utils.NormalMapCreatorAsync, og.utils.NormalMapCreator);
+
+og.utils.NormalMapCreatorAsync.prototype.drawAsync = function (segment) {
+    if (this._counter >= 1) {
+        this._pendingsQueue.push(segment);
+    } else {
+        this._exec(segment);
+    }
+};
+
+og.utils.NormalMapCreatorAsync.prototype._exec = function (segment) {
+    this._counter++;
+    var that = this;
+    setTimeout(function () {
+        var cnv = that.draw(segment.terrainNormals);
+        segment.createNormalMap(cnv);
+        that._dequeueRequest();
+    }, 0);
+};
+
+og.utils.NormalMapCreatorAsync.prototype._dequeueRequest = function () {
+    this._counter--;
+    if (this._pendingsQueue.length) {
+        if (this._counter < 1) {
+            var req;
+            if (req = this._whilePendings())
+                this._exec(req);
+        }
+    }
+};
+
+og.utils.NormalMapCreatorAsync.prototype._whilePendings = function () {
+    while (this._pendingsQueue.length) {
+        var req = this._pendingsQueue.shift();
+        if (req.terrainNormals.length) {
+            return req;
+        }
+    }
+    return null;
+};
