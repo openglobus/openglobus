3094279b032fc73b717709e682b1700a8cf3e052
Zemledelec <zemledelec@openglobus.org>
2014-12-21 23:30:25 +0300

    Little mess with commits. Half past 11 pm.

--- src/og/shaderProgram/shaderProgram.js
+++ src/og/shaderProgram/shaderProgram.js
@@ -3,8 +3,12 @@ goog.provide('og.shaderProgram.ShaderProgram');
 
 goog.require('og.shaderProgram.callbacks');
 
-//og.shaderProgram.SHADERS_URL = "http://www.openglobus.org/shaders/";
-og.shaderProgram.SHADERS_URL = "../../src/og/shaders/";
+if (COMPILED) {
+    //og.shaderProgram.SHADERS_URL = "http://www.openglobus.org/shaders/";
+    og.shaderProgram.SHADERS_URL = "/shaders/";
+} else {
+    og.shaderProgram.SHADERS_URL = "../../src/og/shaders/";
+}
 
 og.shaderProgram.ShaderProgram = function (name, material) {
     this.name = name;
