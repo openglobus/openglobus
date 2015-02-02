18f738ba2ea6d347ca0f88c6a71ed9b4ace7d9ea
Zemledelec <mgevlich@gmail.com>
2015-02-02 20:03:43 +0300

    Touch prevent defaults.

--- src/og/input/touchHandler.js
+++ src/og/input/touchHandler.js
@@ -7,17 +7,27 @@ og.input.TouchHandler = function (htmlObject) {
 og.input.TouchHandler.prototype.setEvent = function (event, sender, callback) {
     switch (event) {
         case "ontouchcancel": {
-            this._htmlObject.ontouchcancel = function (event) { callback.call(sender, event); };
+            this._htmlObject.ontouchcancel = function (event) {
+                event.preventDefault();
+                callback.call(sender, event);
+            };
         }
             break;
         case "ontouchstart": {
-            this._htmlObject.ontouchstart = function (event) { callback.call(sender, event); };
+            this._htmlObject.ontouchstart = function (event) {
+                event.preventDefault();
+                callback.call(sender, event);
+            };
         } break;
         case "ontouchend": {
-            this._htmlObject.ontouchend = function (event) { callback.call(sender, event); };
+            this._htmlObject.ontouchend = function (event) {
+                event.preventDefault();
+                callback.call(sender, event);
+            };
         } break;
         case "ontouchmove": {
             this._htmlObject.ontouchmove = function (event) {
+                event.preventDefault();
                 var rect = this.getBoundingClientRect();
                 callback.call(sender, event/*{ clientX: event.clientX - rect.left, clientY: event.clientY - rect.top }*/);
             };
