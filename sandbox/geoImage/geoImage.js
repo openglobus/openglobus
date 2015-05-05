e13735c057cd287e5521fdede7d8a0e8405b12dd
Zemledelec <mgevlich@gmail.com>
2015-05-05 11:46:04 +0300

    geoImage wgs84 to web mercator reproj example.

--- sandbox/geoImage/geoImage.js
+++ sandbox/geoImage/geoImage.js
@@ -4,7 +4,8 @@ goog.provide('GeoImage');
 goog.require('og.node.RenderNode');
 goog.require('og.inheritance');
 goog.require('og.Extent');
-
+goog.require('og.mercator');
+goog.require('og.webgl.Framebuffer');
 goog.require('og.LonLat');
 
 GeoImage = function () {
@@ -23,69 +24,99 @@ og.inheritance.extend(GeoImage, og.node.RenderNode);
 
 GeoImage.prototype.initialization = function () {
 
+    this._handler = this.renderer.handler;
 
+    this._framebufferPASS_ONE = new og.webgl.Framebuffer(this._handler.gl, 256, 256);
+    this._framebufferPASS_ONE.initialize();
+
+    this.imageLoaded = false;
     this._sourceImage = new Image();
     var that = this;
     this._sourceImage.onload = function () {
-        that._sourceTexture = that.renderer.handler.createTexture_n(this);
+        that._sourceTexture = that._handler.createTexture_n(this);
+        that._framebufferPASS_ONE.setSize(this.width, this.height);
+        that.projMercFrame();
+        that.imageLoaded = true;
     };
 
-    //this._sourceImage.src = "bm.jpg";
-    //var v = [og.lonLat(-180, 90), og.lonLat(180, 90), og.lonLat(180, -90), og.lonLat(-180, -90)];
-    //var ext = og.Extent.createByCoordinates(v);
-    //var tileExtent = new og.Extent(og.lonLat(-180, -90), og.lonLat(180, 90));
+    this._sourceImage.src = "bm.jpg";
+    this.wgs84corners = [og.lonLat(-180, 90), og.lonLat(180, 90), og.lonLat(180, -90), og.lonLat(-180, -90)];
+    this.mercExtent = og.Extent.createByCoordinates(this.wgs84corners);
+    if (this.mercExtent.southWest.lat < og.mercator.MIN_LAT) {
+        this.mercExtent.southWest.lat = og.mercator.MIN_LAT
+    }
+    if (this.mercExtent.northEast.lat > og.mercator.MAX_LAT) {
+        this.mercExtent.northEast.lat = og.mercator.MAX_LAT
+    }
+    var dX = 2.0 / this.mercExtent.getWidth();
+    var dY = 2.0 / this.mercExtent.getHeight();
 
-    this._sourceImage.src = "ql.jpg";
-    var v = [og.lonLat(152.02, -31.29), og.lonLat(151.59, -30.93), og.lonLat(151.86, -30.68), og.lonLat(152.29, -31.04)];
-    var ext = og.Extent.createByCoordinates(v);
-    var tileExtent = new og.Extent(og.lonLat(151.59, -31.29), og.lonLat(152.29, -30.68));
+    this.mercExtentParams = [this.mercExtent.southWest.lon, this.mercExtent.southWest.lat, dX, dY];
 
-
-    var dX = 2.0 / tileExtent.getWidth();
-    var dY = 2.0 / tileExtent.getHeight();
-
-    this.extentParams = [tileExtent.southWest.lon, tileExtent.southWest.lat, dX, dY];
-
-    this._cornersBuffer = this.renderer.handler.createArrayBuffer(new Float32Array([
-        v[3].lon, v[3].lat,
-        v[2].lon, v[2].lat,
-        v[0].lon, v[0].lat,
-        v[1].lon, v[1].lat]), 2, 4);
+    this._cornersBuffer = this._handler.createArrayBuffer(new Float32Array([
+        this.wgs84corners[3].lon, this.wgs84corners[3].lat,
+        this.wgs84corners[2].lon, this.wgs84corners[2].lat,
+        this.wgs84corners[0].lon, this.wgs84corners[0].lat,
+        this.wgs84corners[1].lon, this.wgs84corners[1].lat]), 2, 4);
 
     this._texCoordsBuffer = this.renderer.handler.createArrayBuffer(new Float32Array([
-       0.0, 1.0,
-       1.0, 1.0,
-       0.0, 0.0,
-       1.0, 0.0]), 2, 4);
+       0, 1,
+       1, 1,
+       0, 0,
+       1, 0]), 2, 4);
 
-    this.renderer.handler.deactivateFaceCulling();
+    this._handler.deactivateFaceCulling();
 
+    this._vertexBuffer = this._handler.createArrayBuffer(new Float32Array([
+    -1,  1,
+     1,  1,
+    -1, -1,
+     1, -1]), 2, 4);
 
-    var sh = this.renderer.handler.shaderPrograms.geoImage._program;
-    var sha = sh.attributes,
-        shu = sh.uniforms;
-    var gl = this.renderer.handler.gl;
-    gl.bindBuffer(gl.ARRAY_BUFFER, this._texCoordsBuffer);
-    gl.vertexAttribPointer(sha.a_texCoord._pName, this._texCoordsBuffer.itemSize, gl.FLOAT, false, 0, 0);
 };
 
-GeoImage.prototype.frame = function () {
+GeoImage.prototype.projMercFrame = function () {
+    var gl = this._handler.gl;
 
-    var sh = this.renderer.handler.shaderPrograms.geoImage._program;
+    //wgs84 image extent making
+    this._framebufferPASS_ONE.activate();
+    this._handler.shaderPrograms.geoImage.activate();
+    var sh = this._handler.shaderPrograms.geoImage._program;
     var sha = sh.attributes,
         shu = sh.uniforms;
-    var gl = this.renderer.handler.gl;
-
-    this.renderer.handler.gl.clearColor(0.0, 0.0, 0.0, 0.0);
-    this.renderer.handler.gl.clear(this.renderer.handler.gl.COLOR_BUFFER_BIT | this.renderer.handler.gl.DEPTH_BUFFER_BIT);
-
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._texCoordsBuffer);
+    gl.vertexAttribPointer(sha.a_texCoord._pName, this._texCoordsBuffer.itemSize, gl.FLOAT, false, 0, 0);
     gl.bindBuffer(gl.ARRAY_BUFFER, this._cornersBuffer);
     gl.vertexAttribPointer(sha.a_corner._pName, this._cornersBuffer.itemSize, gl.FLOAT, false, 0, 0);
-    gl.uniform4fv(shu.u_extentParams._pName, this.extentParams);
-
+    gl.uniform4fv(shu.u_extentParams._pName, this.mercExtentParams);
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, this._sourceTexture);
     gl.uniform1i(shu.u_sourceImage._pName, 0);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    this._framebufferPASS_ONE.deactivate();
 
-    gl.drawArrays(this.renderer.handler.gl.TRIANGLE_STRIP, 0, 4);
+    //mercator projection
+    this._handler.shaderPrograms.geoImageMercProj.activate();
+    sh = this._handler.shaderPrograms.geoImageMercProj._program;
+    sha = sh.attributes;
+    shu = sh.uniforms;
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._texCoordsBuffer);
+    gl.vertexAttribPointer(sha.a_texCoord._pName, this._texCoordsBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
+    gl.vertexAttribPointer(sha.a_vertex._pName, this._vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, this._framebufferPASS_ONE.texture);
+    gl.uniform1i(shu.u_sampler._pName, 0);
+    gl.uniform4fv(shu.u_extent._pName, [this.mercExtent.southWest.lon, this.mercExtent.southWest.lat, this.mercExtent.northEast.lon, this.mercExtent.northEast.lat]);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+};
+
+GeoImage.prototype.frame = function () {
+    if (this.imageLoaded) {
+        this.projMercFrame();
+    }
 };
