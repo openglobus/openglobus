2dbead77624f099ee0d51f09276b647f2bdfa998
Zemledelec <zemledelec@openglobus.org>
2015-10-23 16:36:16 +0300

    build catalog

---
+++ build/makedeps.bat
@@ -0,0 +1,1 @@
+"e:\my projects\closure-library\closure\bin\build\depswriter.py" --root_with_prefix="../src/og/ ../../../og/src/og/" > "e:\my projects\openglobus\src\og\og-deps.js"
--- private/og_depthwriter.bat
+++
@@ -1,1 +0,0 @@
-"e:\my projects\closure-library\closure\bin\build\depswriter.py" --root_with_prefix="../src/og/ ../../../og/src/og/" > "e:\my projects\openglobus\src\og\og-deps.js"
