a7282e27e15b27a07c26e80930a830b1152eb2ff
Zemledelec <zemledelec@openglobus.org>
2015-12-11 16:30:55 +0300

    Added some new documentation for render node class.

--- src/og/node/renderNode.js
+++ src/og/node/renderNode.js
@@ -9,55 +9,136 @@ goog.require('og.math.Vector3');
 goog.require('og.utils.TextureAtlas');
 goog.require('og.utils.FontAtlas');
 
+/**
+ * Render node is a logical part of a render mechanism. Represents scene rendering.
+ * Forexample one node for rendering the Earth, another one for rendering the Moon, another node for rendering stars etc.
+ * Each render node has own model view space defined with matrices(scale, rotation, translation, transformation).
+ * There are collections of ligh sources, entities and so on in the node.
+ * Access to the node is renderer.renderNodes["Earth"]
+ * @class
+ * @extends {og.node.Node}
+ * @param {string} name - Node name.
+ */
 og.node.RenderNode = function (name) {
-    og.inheritance.base(this, name);
-    this.renderer = null;
-    this.drawMode = null;
-    this.show = true;
-    this._isActive = true;
-    this._zIndex = 1000;
 
+    og.inheritance.base(this, name);
+
+    /**
+     * Renderer that calls frame() callback.
+     * @public
+     * @type {og.Renderer}
+     */
+    this.renderer = null;
+
+    this.drawMode = null;
+
+    /** Show rendering.
+     * @public
+     */
+    this.show = true;
+
+    this._isActive = true;
+
+    //Actually this matrices are not important, but could 
+    //be useful for big objects like Planet.     
     this.scaleMatrix = new og.math.Matrix4().setIdentity();
     this.rotationMatrix = new og.math.Matrix4().setIdentity();
     this.translationMatrix = new og.math.Matrix4().setIdentity();
     this.transformationMatrix = new og.math.Matrix4().setIdentity();
     this.itransformationMatrix = new og.math.Matrix4().setIdentity();
 
+    /**
+     * Lighting calculations.
+     * @public
+     * @type {boolean}
+     */
     this.lightEnabled = false;
+
+    /**
+     * Point light array.
+     * @private
+     * @type {Array.<og.light.PointLight>}
+     */
     this._pointLights = [];
     this._pointLightsTransformedPositions = [];
     this._pointLightsParamsv = [];
     this._pointLightsParamsf = [];
     this._pointLightsNames = [];
 
+    /**
+     * Entity collection array.
+     * @public
+     * @type {Array.<og.EntityCollection>}
+     */
     this.entityCollections = [];
+
+    /**
+     * Texture atlas for the billboards images. One atlas per node.
+     * @public
+     * @type {og.utils.TextureAtlas}
+     */
     this.billboardsTextureAtlas = new og.utils.TextureAtlas();
+
+    /**
+     * Texture font atlas for the font families and styles. One atlas per node.
+     * @public
+     * @type {og.utils.FontAtlas}
+     */
     this.fontAtlas = new og.utils.FontAtlas();
 
+    //TODO
     this.events = new og.Events();
 };
 
 og.inheritance.extend(og.node.RenderNode, og.node.Node);
 
+/**
+ * Adds entity collection.
+ * @public
+ * @param {og.EntityCollection} entityCollection - Entity collection.
+ * @returns {og.node.RenderNode}
+ */
 og.node.RenderNode.prototype.addEntityCollection = function (entityCollection) {
     entityCollection.addTo(this);
     return this;
 };
 
+/**
+ * Removes entity collection.
+ * @public
+ * @param {og.EntityCollection} entityCollection - Entity collection for remove.
+ */
 og.node.RenderNode.prototype.removeEntityCollection = function (entityCollection) {
     entityCollection.remove();
 };
 
+/**
+ * Adds point light source.
+ * @public
+ * @param {og.light.PointLight} light - Point light source.
+ * @returns {og.node.RenderNode}
+ */
 og.node.RenderNode.prototype.addLight = function (light) {
     light.addTo(this);
     return this;
 };
 
+/**
+ * Gets light object by its name.
+ * @public
+ * @param {string} name - Point light name.
+ * @returns {og.light.PointLight}
+ */
 og.node.RenderNode.prototype.getLightByName = function (name) {
     var li = this._pointLightsNames.indexOf(name);
     return this._pointLights[li];
 };
 
+/**
+ * Removes light source.
+ * @public
+ * @param {og.light.PointLight} light - Point light object.
+ */
 og.node.RenderNode.prototype.removeLight = function (light) {
     light.remove();
 };
@@ -88,18 +169,15 @@ og.node.RenderNode.prototype.drawNode = function () {
     }
 };
 
-og.node.RenderNode.prototype.setZIndex = function (zindex) {
-    this._zIndex = zindex;
-};
-
-og.node.RenderNode.prototype.getZIndex = function () {
-    return this._zIndex;
-};
-
 og.node.RenderNode.prototype.isActive = function () {
     return this._isActive;
 };
 
+/**
+ * Activate rendering this node.
+ * @public
+ * @param {boolean} isActive - Activation flag.
+ */
 og.node.RenderNode.prototype.setActive = function (isActive) {
     this._isActive = isActive;
     for (var i = 0; i < this.childNodes.length; i++) {
@@ -131,6 +209,10 @@ og.node.RenderNode.prototype.drawNodes = function () {
     }
 };
 
+/**
+ * This function have to be called manualy in each render node frame callback, before drawing scene geometry.
+ * @public
+ */
 og.node.RenderNode.prototype.transformLights = function () {
     var r = this.renderer;
     for (var i = 0; i < this._pointLights.length; i++) {
@@ -187,6 +269,11 @@ og.node.RenderNode.prototype.drawEntities = function () {
     }
 };
 
+/**
+ * Assign render node with renderer.
+ * @public
+ * @param {og.Renderer} renderer - Redner node's renderer.
+ */
 og.node.RenderNode.prototype.assignRenderer = function (renderer) {
     this.renderer = renderer;
     this.billboardsTextureAtlas.assignHandler(renderer.handler);
