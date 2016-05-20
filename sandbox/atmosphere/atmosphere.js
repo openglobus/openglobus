f4b11c76567761061cc2afa7475079937492063c
Zemledelec <zemledelec@openglobus.org>
2016-05-20 15:59:04 +0300

    og.shape.Icosphere added.

--- sandbox/atmosphere/atmosphere.js
+++ sandbox/atmosphere/atmosphere.js
@@ -8,6 +8,7 @@ goog.require('og.math.Vector3');
 goog.require('og.light.PointLight');
 goog.require('og.shaderProgram.sphere');
 goog.require('og.shaderProgram.atmosphereSpace');
+goog.require('og.shape.Icosphere');
 
 Atmosphere = function () {
     og.inheritance.base(this);
@@ -39,142 +40,6 @@ Atmosphere = function () {
 };
 
 og.inheritance.extend(Atmosphere, og.node.RenderNode);
-
-
-
-
-var IcoSphereCreator = function () {
-
-    var index = 0;
-    var geometry = [];
-    var indices = [];
-    var scale = 1;
-
-    middlePointIndexCache = {};
-
-    // add vertex to mesh, fix position to be on unit sphere, return index
-    function addVertex(p) {
-        var length = Math.sqrt(p[0] * p[0] + p[1] * p[1] + p[2] * p[2]);
-        geometry.push(scale * p[0] / length, scale * p[1] / length, scale * p[2] / length);
-        return index++;
-    }
-
-    // return index of point in the middle of p1 and p2
-    function getMiddlePoint(p1, p2) {
-        // first check if we have it already
-        var firstIsSmaller = p1 < p2;
-        var smallerIndex = firstIsSmaller ? p1 : p2;
-        var greaterIndex = firstIsSmaller ? p2 : p1;
-
-        var key = smallerIndex + "_" + greaterIndex;//(smallerIndex << 32) + greaterIndex;
-
-        var ret = middlePointIndexCache[key];
-        if (ret) {
-            return ret;
-        }
-
-        var point1 = [geometry[p1 * 3], geometry[p1 * 3 + 1], geometry[p1 * 3 + 2]];
-        var point2 = [geometry[p2 * 3], geometry[p2 * 3 + 1], geometry[p2 * 3 + 2]];
-        var middle = [(point1[0] + point2[0]) / 2.0, (point1[1] + point2[1]) / 2.0, (point1[2] + point2[2]) / 2.0];
-
-        // add vertex makes sure point is on unit sphere
-        var i = addVertex(middle);
-        middlePointIndexCache[key] = i;
-        return i;
-    }
-
-    this.create = function (recursionLevel, s) {
-        geometry = [];
-        indices = [];
-        index = 0;
-        scale = s;
-        middlePointIndexCache = {};
-
-        // create 12 vertices of a icosahedron
-        var t = (1.0 + Math.sqrt(5.0)) / 2.0;
-
-        addVertex([-1, t, 0]);
-        addVertex([1, t, 0]);
-        addVertex([-1, -t, 0]);
-        addVertex([1, -t, 0]);
-
-        addVertex([0, -1, t]);
-        addVertex([0, 1, t]);
-        addVertex([0, -1, -t]);
-        addVertex([0, 1, -t]);
-
-        addVertex([t, 0, -1]);
-        addVertex([t, 0, 1]);
-        addVertex([-t, 0, -1]);
-        addVertex([-t, 0, 1]);
-
-
-        // create 20 triangles of the icosahedron
-        var faces = [];
-
-        // 5 faces around point 0
-        faces.push([0, 11, 5]);
-        faces.push([0, 5, 1]);
-        faces.push([0, 1, 7]);
-        faces.push([0, 7, 10]);
-        faces.push([0, 10, 11]);
-
-        // 5 adjacent faces 
-        faces.push([1, 5, 9]);
-        faces.push([5, 11, 4]);
-        faces.push([11, 10, 2]);
-        faces.push([10, 7, 6]);
-        faces.push([7, 1, 8]);
-
-        // 5 faces around point 3
-        faces.push([3, 9, 4]);
-        faces.push([3, 4, 2]);
-        faces.push([3, 2, 6]);
-        faces.push([3, 6, 8]);
-        faces.push([3, 8, 9]);
-
-        // 5 adjacent faces 
-        faces.push([4, 9, 5]);
-        faces.push([2, 4, 11]);
-        faces.push([6, 2, 10]);
-        faces.push([8, 6, 7]);
-        faces.push([9, 8, 1]);
-
-
-        // refine triangles
-        for (var i = 0; i < recursionLevel; i++) {
-            var faces2 = [];
-            for (var j = 0; j < faces.length; j++) {
-                var tri = faces[j];
-                // replace triangle by 4 triangles
-                var a = getMiddlePoint(tri[0], tri[1]);
-                var b = getMiddlePoint(tri[1], tri[2]);
-                var c = getMiddlePoint(tri[2], tri[0]);
-
-                faces2.push([tri[0], a, c]);
-                faces2.push([tri[1], b, a]);
-                faces2.push([tri[2], c, b]);
-                faces2.push([a, b, c]);
-            }
-            faces = faces2;
-        }
-
-        // done, now add triangles to mesh
-        for (var i = 0; i < faces.length; i++) {
-            var tri = faces[i];
-            indices.push(tri[0]);
-            indices.push(tri[1]);
-            indices.push(tri[2]);
-        }
-
-        return {
-            geometry: geometry,
-            indices: indices
-        };
-    }
-};
-
-
 
 
 Atmosphere.prototype._createData = function () {
@@ -255,10 +120,9 @@ Atmosphere.prototype._createData = function () {
         }
     }
 
-    var ic = new IcoSphereCreator();
-    var a = ic.create(5, 105);
-    this._outerPositionData = a.geometry;
-    this._outerIndexData = a.indices;
+    var is = new og.shape.Icosphere({ level: 5, size: 105 });
+    this._outerPositionData = is._positionData;
+    this._outerIndexData = is._indexData;
 };
 
 Atmosphere.prototype._createBuffers = function () {
