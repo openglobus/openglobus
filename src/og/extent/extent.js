8a0a2863ddb25cea2989114d62d2b4596945d30b
Zemledelec <zemledelec@openglobus.org>
2016-07-30 10:25:53 +0300

    Extent class documentation.

--- src/og/extent/extent.js
+++ src/og/extent/extent.js
@@ -23,36 +23,49 @@ og.Extent = function (sw, ne) {
 };
 
 /**
+ * Whole mercator extent.
  * @const
  */
 og.Extent.FULL_MERC = new og.Extent(og.LonLat.SW_MERC, og.LonLat.NE_MERC);
 
 /**
+ * Degrees extent from north mercator limit to north pole.
  * @const
  */
 og.Extent.NORTH_POLE_DEG = new og.Extent(og.LonLat.NW_MERC_DEG, new og.LonLat(180.0, 90.0));
 
 /**
+ * Degrees extent from south pole to south mercator limit.
  * @const
  */
 og.Extent.SOUTH_POLE_DEG = new og.Extent(new og.LonLat(-180.0, -90.0), og.LonLat.SE_MERC_DEG);
 
 /**
+ * Creates extent instance.
  * @static
+ * @param {og.LonLat} sw - South west(left bottom) extent corner.
+ * @param {og.LonLat} ne - North east(right top) extent corner.
+ * @return {og.Extent} Extent instance.
  */
 og.extent = function (sw, ne) {
     return new og.Extent(sw, ne);
 };
 
 /**
+ * Creates extent instance from values in array.
  * @static
+ * @param {Array.<number,number,number,number>} arr - South west and north east longitude and latidudes packed in array.
+ * @return {og.Extent} Extent object.
  */
 og.Extent.createFromArray = function (arr) {
     return new og.Extent(new og.LonLat(arr[0], arr[1]), new og.LonLat(arr[2], arr[3]));
 };
 
 /**
+ * Creates bound extent instance by coordinate array.
  * @static
+ * @param {Array.<og.LonLat>} arr - Coordinate array.
+ * @return {og.Extent} Extent object.
  */
 og.Extent.createByCoordinates = function (arr) {
     var lonmin = og.math.MAX, lonmax = og.math.MIN,
@@ -90,15 +103,10 @@ og.Extent.fromTile = function (x, y, z) {
 };
 
 /**
+ * Sets current bounding extent object by coordinate array.
  * @public
- */
-og.Extent.prototype.intersects = function (e) {
-    return this.southWest.lon < e.northEast.lon && this.northEast.lon > e.southWest.lon &&
-       this.southWest.lat < e.northEast.lat && this.northEast.lat > e.southWest.lat;
-};
-
-/**
- * @public
+ * @param {Array.<og.LonLat>} arr - Coordinate array.
+ * @return {og.Extent} Current extent.
  */
 og.Extent.prototype.setByCoordinates = function (arr) {
     var lonmin = og.math.MAX, lonmax = og.math.MIN,
@@ -114,10 +122,14 @@ og.Extent.prototype.setByCoordinates = function (arr) {
     this.southWest.lat = latmin;
     this.northEast.lon = lonmax;
     this.northEast.lat = latmax;
+    return this;
 };
 
 /**
+ * Determines if point inside extent.
  * @public
+ * @param {og.LonLat} lonlat - Coordinate point.
+ * @return {boolean} Returns true if point inside extent.
  */
 og.Extent.prototype.isInside = function (lonlat) {
     var sw = this.southWest,
@@ -127,28 +139,47 @@ og.Extent.prototype.isInside = function (lonlat) {
 };
 
 /**
+ * Returns true if two extent overlap each other.
  * @public
+ * @param {og.Extent} e - Another extent.
+ * @return {boolean}
+ */
+og.Extent.prototype.intersects = function (e) {
+    return this.southWest.lon < e.northEast.lon && this.northEast.lon > e.southWest.lon &&
+       this.southWest.lat < e.northEast.lat && this.northEast.lat > e.southWest.lat;
+};
+
+/**
+ * Gets extent width.
+ * @public
+ * @return {number} Extent width.
  */
 og.Extent.prototype.getWidth = function () {
     return this.northEast.lon - this.southWest.lon;
 };
 
 /**
+ * Gets extent height.
  * @public
+ * @return {number} Extent height.
  */
 og.Extent.prototype.getHeight = function () {
     return this.northEast.lat - this.southWest.lat
 };
 
 /**
+ * Creates clone instance of the current extent.
  * @public
+ * @return {og.Extent} Extent clone.
  */
 og.Extent.prototype.clone = function () {
     return new og.Extent(this.southWest.clone(), this.northEast.clone());
 };
 
 /**
+ * Gets the center coordinate of the extent.
  * @public
+ * @return {number} Center coordinate.
  */
 og.Extent.prototype.getCenter = function () {
     var sw = this.southWest, ne = this.northEast;
@@ -216,21 +247,28 @@ og.Extent.prototype.equals = function (extent) {
 };
 
 /**
+ * Converts extent coordinates to mercator projection coordinates.
  * @public
+ * @return {og.Extent} New instance of the current extent.
  */
 og.Extent.prototype.forwardMercator = function () {
     return new og.Extent(this.southWest.forwardMercator(), this.northEast.forwardMercator());
 };
 
 /**
+ * Converts extent coordinates from mercator projection to degrees.
  * @public
+ * @return {og.Extent} New instance of the current extent.
  */
 og.Extent.prototype.inverseMercator = function () {
     return new og.Extent(this.southWest.inverseMercator(), this.northEast.inverseMercator());
 };
 
 /**
+ * Gets cartesian bounding bounds of the current ellipsoid.
  * @public
+ * @param {og.Ellipsoid} ellipsoid - Ellipsoid.
+ * @return {Array.<number,number,number,number,number,number>} Cartesian 3d coordinate array.
  */
 og.Extent.prototype.getCartesianBounds = function (ellipsoid) {
     var xmin = og.math.MAX, xmax = og.math.MIN, ymin = og.math.MAX,
