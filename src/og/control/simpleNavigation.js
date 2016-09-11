a14ee217f0112f74c92c78399412aeb2f805b963
Zemledelec <zemledelec@openglobus.org>
2016-09-11 22:08:10 +0300

    simpleNavigation camera attached manually.

--- src/og/control/simpleNavigation.js
+++ src/og/control/simpleNavigation.js
@@ -12,11 +12,14 @@ goog.require('og.input');
  */
 og.control.SimpleNavigation = function (options) {
     og.inheritance.base(this, options);
+    this.camera = null;
+    this.speed = 10;
 };
 
 og.inheritance.extend(og.control.SimpleNavigation, og.control.BaseControl);
 
 og.control.SimpleNavigation.prototype.oninit = function () {
+    this.camera = this.renderer.activeCamera;
     this.renderer.events.on("keypress", this, this.onCameraMoveForward, og.input.KEY_W);
     this.renderer.events.on("keypress", this, this.onCameraMoveBackward, og.input.KEY_S);
     this.renderer.events.on("keypress", this, this.onCameraStrifeLeft, og.input.KEY_A);
@@ -30,61 +33,61 @@ og.control.SimpleNavigation.prototype.oninit = function () {
 };
 
 og.control.SimpleNavigation.prototype.onCameraMoveForward = function (event) {
-    var camera = this.renderer.activeCamera;
-    camera.slide(0, 0, -10);
+    var camera = this.camera;
+    camera.slide(0, 0, -this.speed);
     camera.update();
 };
 
 og.control.SimpleNavigation.prototype.onCameraMoveBackward = function (event) {
-    var camera = this.renderer.activeCamera;
-    camera.slide(0, 0, 10);
+    var camera = this.camera;
+    camera.slide(0, 0, this.speed);
     camera.update();
 };
 
 og.control.SimpleNavigation.prototype.onCameraStrifeLeft = function (event) {
-    var camera = this.renderer.activeCamera;
-    camera.slide(-10, 0, 0);
+    var camera = this.camera;
+    camera.slide(-this.speed, 0, 0);
     camera.update();
 };
 
 og.control.SimpleNavigation.prototype.onCameraStrifeRight = function (event) {
-    var camera = this.renderer.activeCamera;
-    camera.slide(10, 0, 0);
+    var camera = this.camera;
+    camera.slide(this.speed, 0, 0);
     camera.update();
 };
 
 og.control.SimpleNavigation.prototype.onCameraLookUp = function (event) {
-    var cam = this.renderer.activeCamera;
+    var cam = this.camera;
     cam.pitch(0.5);
     cam.update();
 };
 
 og.control.SimpleNavigation.prototype.onCameraLookDown = function (event) {
-    var cam = this.renderer.activeCamera;
+    var cam = this.camera;
     cam.pitch(-0.5);
     cam.update();
 };
 
 og.control.SimpleNavigation.prototype.onCameraTurnLeft = function (event) {
-    var cam = this.renderer.activeCamera;
+    var cam = this.camera;
     cam.yaw(0.5);
     cam.update();
 };
 
 og.control.SimpleNavigation.prototype.onCameraTurnRight = function (event) {
-    var cam = this.renderer.activeCamera;
+    var cam = this.camera;
     cam.yaw(-0.5);
     cam.update();
 };
 
 og.control.SimpleNavigation.prototype.onCameraRollLeft = function (event) {
-    var cam = this.renderer.activeCamera;
+    var cam = this.camera;
     cam.roll(-0.5);
     cam.update();
 };
 
 og.control.SimpleNavigation.prototype.onCameraRollRight = function (event) {
-    var cam = this.renderer.activeCamera;
+    var cam = this.camera;
     cam.roll(0.5);
     cam.update();
 };
