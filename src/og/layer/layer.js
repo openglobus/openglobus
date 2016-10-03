57fd11d6b9ab3be829b10c4db7faa5146d036709
Zemledelec <zemledelec@openglobus.org>
2016-10-04 00:33:12 +0300

    Default layer transparent color changed.

--- src/og/layer/layer.js
+++ src/og/layer/layer.js
@@ -77,7 +77,7 @@ og.layer.Layer = function (name, options) {
      * @public
      * @type {Array.<number,number,number>}
      */
-    this.transparentColor = options.transparentColor || [1.0, 17.0 / 255.0, 22.0 / 255.0];
+    this.transparentColor = options.transparentColor || [236 / 255, 83 / 255, 196 / 255];
 
     /**
      * Minimal zoom level when layer is visibile.
