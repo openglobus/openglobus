db40f753748233e9db472744a50302ed9b8bc258
Zemledelec <zemledelec@openglobus.org>
2016-12-27 23:35:04 +0300

    Asynchronous canvasTiles layer rendering.

--- src/og/layer/canvasTiles.js
+++ src/og/layer/canvasTiles.js
@@ -18,18 +18,34 @@ goog.require('og.ImageCanvas');
  * @param {string} [options.attribution] - Layer attribution that displayed in the attribution area on the screen.
  * @param {boolean} [options.isBaseLayer=false] - Base layer flag.
  * @param {boolean} [options.visibility=true] - Layer visibility.
+ * @param {og.layer.CanvasTiles~drawTileCallback} [options.drawTile] - Draw tile callback.
  */
 og.layer.CanvasTiles = function (name, options) {
     options = options || {};
 
     og.inheritance.base(this, name, options);
 
+
+    /**
+     * Current creating tiles couter.
+     * @protected
+     * @type {number}
+     */
+    this._counter = 0;
+
+    /**
+     * Tile pending queue that waiting for create.
+     * @protected
+     * @type {Array.<og.planetSegment.Material>}
+     */
+    this._pendingsQueue = [];//new og.QueueArray();
+
     /**
      * Draw tile callback. 
      * @type {og.layer.CanvasTiles~drawTileCallback}
      * @public
      */
-    this.drawTile = null;
+    this.drawTile = options.drawTile || null;
 };
 
 og.inheritance.extend(og.layer.CanvasTiles, og.layer.Layer);
@@ -43,8 +59,51 @@ og.layer.canvasTiles = function (name, options) {
     return new og.layer.CanvasTiles(name, options);
 };
 
+og.layer.CanvasTiles.__requestsCounter = 0;
+
 /**
- * Start to handle tile segment material.
+ * Maximum tiles per frame.
+ * @const
+ * @type {number}
+ */
+og.layer.CanvasTiles.MAX_REQUESTS = 7;
+
+
+/**
+ * Abort loading tiles.
+ * @public
+ */
+og.layer.CanvasTiles.prototype.abortLoading = function () {
+    var q = this._pendingsQueue;
+    for (var i = q._shiftIndex + 1; i < q._popIndex + 1; i++) {
+        if (q._array[i]) {
+            this.abortMaterialLoading(q._array[i]);
+        }
+    }
+    this._pendingsQueue = [];
+    //this._pendingsQueue.clear();
+};
+
+/**
+ * Sets layer visibility.
+ * @public
+ * @param {boolean} visibility - Layer visibility.
+ */
+og.layer.CanvasTiles.prototype.setVisibility = function (visibility) {
+    if (visibility != this._visibility) {
+        this._visibility = visibility;
+        if (this._isBaseLayer && visibility) {
+            this._planet.setBaseLayer(this);
+        } else if (!visibility) {
+            this.abortLoading();
+        }
+        this._planet.updateVisibleLayers();
+        this.events.dispatch(this.events.visibilitychange, this);
+    }
+};
+
+/**
+ * Start to load tile material.
  * @public
  * @virtual
  * @param {og.planetSegment.Material} mateial
@@ -52,28 +111,157 @@ og.layer.canvasTiles = function (name, options) {
 og.layer.CanvasTiles.prototype.loadMaterial = function (material) {
 
     var seg = material.segment;
-    if (seg.tileZoom >= this.minZoom &&
-        seg.tileZoom <= this.maxZoom) {
 
-        if (this.drawTile) {
-            /**
-             * Tile custom draw function.
-             * @callback og.layer.CanvasTiles~drawTileCallback
-             * @param {og.planetSegment.Material} material
-             * @param {applyCanvasCallback} applyCanvasCallback
-             */
-            this.drawTile(material,
-                /**
-                 * Apply canvas.
-                 * @callback applyCanvasCallback
-                 * @param {Object} canvas
-                 */
-                function (canvas) {
-                    material.isReady = false;
-                    material.applyTexture(canvas);
-                });
+    if (this._isBaseLayer) {
+        material.texture = seg._isNorth ? seg.planet.solidTextureOne : seg.planet.solidTextureTwo;
+    } else {
+        material.texture = seg.planet.transparentTexture;
+    }
+
+    if (this._planet.layersActivity) {
+        material.isReady = false;
+        material.isLoading = true;
+        if (og.layer.CanvasTiles.__requestsCounter >= og.layer.CanvasTiles.MAX_REQUESTS && this._counter) {
+            this._pendingsQueue.push(material);
         } else {
-            material.textureNotExists();
+            this._exec(material);
         }
+    }
+};
+
+/**
+ * Loads material image and apply it to the planet segment.
+ * @protected
+ * @param {og.planetSegment.Material} material - Loads material image.
+ */
+og.layer.CanvasTiles.prototype._exec = function (material) {
+    og.layer.CanvasTiles.__requestsCounter++;
+    this._counter++;
+    var that = this;
+    if (this.drawTile) {
+
+        /**
+         * Tile custom draw function.
+         * @callback og.layer.CanvasTiles~drawTileCallback
+         * @param {og.planetSegment.Material} material
+         * @param {applyCanvasCallback} applyCanvasCallback
+         */
+        setTimeout(function () {
+            that.drawTile(material,
+            /**
+             * Apply canvas.
+             * @callback applyCanvasCallback
+             * @param {Object} canvas
+             */
+            function (canvas) {
+                that._counter--;
+                og.layer.CanvasTiles.__requestsCounter--;
+                material.applyTexture(canvas);
+                that._dequeueRequest();
+            });
+        }, 50);
+    } else {
+        material.textureNotExists();
+    }
+};
+
+/**
+ * Abort exact material loading.
+ * @public
+ * @param {og.planetSegment.Material} material - Segment material.
+ */
+og.layer.CanvasTiles.prototype.abortMaterialLoading = function (material) {
+    if (material.isLoading && material.image) {
+        material.image.src = "";
+        this._counter--;
+        og.layer.CanvasTiles.__requestsCounter--;
+        this._dequeueRequest();
+    }
+    material.isLoading = false;
+    material.isReady = false;
+};
+
+og.layer.CanvasTiles.prototype._dequeueRequest = function () {
+    if (this._pendingsQueue.length) {
+        if (og.layer.CanvasTiles.__requestsCounter < og.layer.CanvasTiles.MAX_REQUESTS) {
+            var pmat;
+            if (pmat = this._whilePendings())
+                this._exec.call(this, pmat);
+        }
+    } else if (this._counter === 0) {
+        this.events.dispatch(this.events.loadend);
+    }
+};
+
+og.layer.CanvasTiles.prototype._whilePendings = function () {
+    while (this._pendingsQueue.length) {
+        var pmat = this._pendingsQueue.pop();
+        if (pmat.segment.node) {
+            if (pmat.segment.ready && pmat.segment.node.getState() === og.quadTree.RENDERING) {
+                return pmat;
+            }
+            pmat.isLoading = false;
+        }
+    }
+    return null;
+};
+
+
+og.layer.CanvasTiles.prototype.applyMaterial = function (material) {
+    if (material.isReady) {
+        return [0, 0, 1, 1];
+    } else {
+
+        !material.isLoading && this.loadMaterial(material);
+
+        var segment = material.segment;
+        var pn = segment.node,
+            notEmpty = false;
+
+        var mId = this._id;
+        var psegm = material;
+        while (pn.parentNode) {
+            if (psegm && psegm.isReady) {
+                notEmpty = true;
+                break;
+            }
+            pn = pn.parentNode;
+            psegm = pn.planetSegment.materials[mId];
+        }
+
+        if (notEmpty) {
+            material.appliedNodeId = pn.nodeId;
+            material.texture = psegm.texture;
+            var dZ2 = 1.0 / (2 << (segment.tileZoom - pn.planetSegment.tileZoom - 1));
+            return [
+                segment.tileX * dZ2 - pn.planetSegment.tileX,
+                segment.tileY * dZ2 - pn.planetSegment.tileY,
+                dZ2,
+                dZ2];
+        } else {
+            material.texture = segment.planet.transparentTexture;
+            return [0, 0, 1, 1];
+        }
+    }
+};
+
+og.layer.CanvasTiles.prototype.clearMaterial = function (material) {
+    if (material.isReady) {
+        material.isReady = false;
+
+        !material.texture.default &&
+            material.segment.handler.gl.deleteTexture(material.texture);
+
+        material.texture = null;
+    }
+
+    this.abortMaterialLoading(material);
+
+    material.isLoading = false;
+    material.textureExists = false;
+
+    if (material.image) {
+        material.image.src = '';
+        material.image = null;
     }
 };
