f3fcad88141ea7de70d1f741e06836f2a40a363e
Zemledelec <zemledelec@openglobus.org>
2018-03-03 22:36:28 +0300

    jd, Clock, Events
    utils - next

--- src/og/astro/jd.js
+++ src/og/astro/jd.js
@@ -1,161 +1,165 @@
-goog.provide('og.jd');
+/**
+ * @module og/astro/jd
+ */
 
-goog.require('og.utils');
+'use strict';
+
+import { utils } from '../utils/utils';
 
 /**
  * Seconds in millisecond.
  * @const
  * @default
  */
-og.jd.SECONDS_PER_MILLISECOND = 0.001;
+export const SECONDS_PER_MILLISECOND = 0.001;
 
 /**
  * Milliseconds in second.
  * @const
  * @default
  */
-og.jd.MILLISECONDS_PER_SECOND = 1000.0;
+export const MILLISECONDS_PER_SECOND = 1000.0;
 
 /**
  * Seconds in minute.
  * @const
  * @default
  */
-og.jd.SECONDS_PER_MINUTE = 60.0;
+export const SECONDS_PER_MINUTE = 60.0;
 
 /**
  * One by seconds in minute.
  * @const
  * @default
  */
-og.jd.ONE_BY_SECONDS_PER_MINUTE = 1.0 / og.jd.SECONDS_PER_MINUTE;
+export const ONE_BY_SECONDS_PER_MINUTE = 1.0 / SECONDS_PER_MINUTE;
 
 /**
  * Minutes in hour.
  * @const
  * @default
  */
-og.jd.MINUTES_PER_HOUR = 60.0;
+export const MINUTES_PER_HOUR = 60.0;
 
 /**
  * Hours in day.
  * @const
  * @default
  */
-og.jd.HOURS_PER_DAY = 24.0;
+export const HOURS_PER_DAY = 24.0;
 
 /**
  * One by hours in day.
  * @const
  * @default
  */
-og.jd.ONE_BY_HOURS_PER_DAY = 1.0 / og.jd.HOURS_PER_DAY;
+export const ONE_BY_HOURS_PER_DAY = 1.0 / HOURS_PER_DAY;
 
 /**
  * Seconds in hour.
  * @const
  * @default
  */
-og.jd.SECONDS_PER_HOUR = 3600.0;
+export const SECONDS_PER_HOUR = 3600.0;
 
 /**
  * One by seconds in hour.
  * @const
  * @default
  */
-og.jd.ONE_BY_SECONDS_PER_HOUR = 1.0 / og.jd.SECONDS_PER_HOUR;
+export const ONE_BY_SECONDS_PER_HOUR = 1.0 / SECONDS_PER_HOUR;
 
 /**
  * Seconds in 12 hours.
  * @const
  * @default
  */
-og.jd.SECONDS_PER_12_HOURS = 12.0 * og.jd.SECONDS_PER_HOUR;
+export const SECONDS_PER_12_HOURS = 12.0 * SECONDS_PER_HOUR;
 
 /**
  * Minutes in day.
  * @const
  * @default
  */
-og.jd.MINUTES_PER_DAY = 1440.0;
+export const MINUTES_PER_DAY = 1440.0;
 
 /**
  * One by minutes in day.
  * @const
  * @default
  */
-og.jd.ONE_BY_MINUTES_PER_DAY = 1.0 / og.jd.MINUTES_PER_DAY;
+export const ONE_BY_MINUTES_PER_DAY = 1.0 / MINUTES_PER_DAY;
 
 /**
  * Seconds in day.
  * @const
  * @default
  */
-og.jd.SECONDS_PER_DAY = 86400.0;
+export const SECONDS_PER_DAY = 86400.0;
 
 /**
  * Milliseconds in day.
  * @const
  * @default
  */
-og.jd.MILLISECONDS_PER_DAY = 86400000.0;
+export const MILLISECONDS_PER_DAY = 86400000.0;
 
 /**
  * One by milliseconds in day.
  * @const
  * @default
  */
-og.jd.ONE_BY_MILLISECONDS_PER_DAY = 1.0 / og.jd.MILLISECONDS_PER_DAY;
+export const ONE_BY_MILLISECONDS_PER_DAY = 1.0 / MILLISECONDS_PER_DAY;
 
 /**
  * One by seconds in day.
  * @const
  * @default
  */
-og.jd.ONE_BY_SECONDS_PER_DAY = 1.0 / og.jd.SECONDS_PER_DAY;
+export const ONE_BY_SECONDS_PER_DAY = 1.0 / SECONDS_PER_DAY;
 
 /**
  * Days in julian century.
  * @const
  * @default
  */
-og.jd.DAYS_PER_JULIAN_CENTURY = 36525.0;
+export const DAYS_PER_JULIAN_CENTURY = 36525.0;
 
 /**
  * Days in julian year.
  * @const
  * @default
  */
-og.jd.DAYS_PER_JULIAN_YEAR = 365.25;
+export const DAYS_PER_JULIAN_YEAR = 365.25;
 
 /**
  * Seconds in picosecond.
  * @const
  * @default
  */
-og.jd.PICOSECOND = 0.000000001;
+export const PICOSECOND = 0.000000001;
 
 /**
  * Modified julian date difference.
  * @const
  * @default
  */
-og.jd.MODIFIED_JULIAN_DATE_DIFFERENCE = 2400000.5;
+export const MODIFIED_JULIAN_DATE_DIFFERENCE = 2400000.5;
 
 /**
  * Julian date of 2000 year. Epoch.
  * @const
  * @default
  */
-og.jd.J2000 = 2451545.0;
+export const J2000 = 2451545.0;
 
 /**
  * Returns julian days from Epoch.
  * @param {number} jd - Julian date.
  * @returns {number} Days from epoch
  */
-og.jd.T = function (jd) {
-    return (jd - og.jd.J2000) / og.jd.DAYS_PER_JULIAN_CENTURY;
+export function T(jd) {
+    return (jd - J2000) / DAYS_PER_JULIAN_CENTURY;
 };
 
 /**
@@ -165,7 +169,7 @@ og.jd.T = function (jd) {
  * @param {number} day - Day.
  * @returns {Number} Day number
  */
-og.jd.getDayNumber = function (year, month, day) {
+export function getDayNumber(year, month, day) {
     var a = ((month - 14) / 12) | 0;
     var b = year + 4800 + a;
     return (((1461 * b) / 4) | 0) + (((367 * (month - 2 - 12 * a)) / 12) | 0) -
@@ -177,8 +181,8 @@ og.jd.getDayNumber = function (year, month, day) {
  * @param {Date} date - Date.
  * @returns {number} UTC julian date
  */
-og.jd.DateToUTC = function (date) {
-    var dayNumber = og.jd.getDayNumber(date.getUTCFullYear(), date.getUTCMonth() + 1, date.getUTCDate());
+export function DateToUTC(date) {
+    var dayNumber = getDayNumber(date.getUTCFullYear(), date.getUTCMonth() + 1, date.getUTCDate());
 
     var hour = date.getUTCHours() - 12;
     if (hour < 0) {
@@ -186,24 +190,24 @@ og.jd.DateToUTC = function (date) {
     }
 
     var secondsOfDay =
-        date.getUTCSeconds() + hour * og.jd.SECONDS_PER_HOUR +
-        date.getUTCMinutes() * og.jd.SECONDS_PER_MINUTE +
-        date.getUTCMilliseconds() * og.jd.SECONDS_PER_MILLISECOND;
+        date.getUTCSeconds() + hour * SECONDS_PER_HOUR +
+        date.getUTCMinutes() * SECONDS_PER_MINUTE +
+        date.getUTCMilliseconds() * SECONDS_PER_MILLISECOND;
 
-    if (secondsOfDay >= og.jd.SECONDS_PER_12_HOURS) {
+    if (secondsOfDay >= SECONDS_PER_12_HOURS) {
         dayNumber--;
     }
 
-    var extraDays = secondsOfDay * og.jd.ONE_BY_SECONDS_PER_DAY | 0;
+    var extraDays = secondsOfDay * ONE_BY_SECONDS_PER_DAY | 0;
     dayNumber += extraDays;
-    secondsOfDay -= og.jd.SECONDS_PER_DAY * extraDays;
+    secondsOfDay -= SECONDS_PER_DAY * extraDays;
 
     if (secondsOfDay < 0) {
         dayNumber--;
-        secondsOfDay += og.jd.SECONDS_PER_DAY;
+        secondsOfDay += SECONDS_PER_DAY;
     }
 
-    return dayNumber + secondsOfDay * og.jd.ONE_BY_SECONDS_PER_DAY;
+    return dayNumber + secondsOfDay * ONE_BY_SECONDS_PER_DAY;
 };
 
 /**
@@ -211,8 +215,8 @@ og.jd.DateToUTC = function (date) {
  * @param {Date} date - Date.
  * @returns {number} TAI julian date
  */
-og.jd.DateToTAI = function (date) {
-    return og.jd.UTCtoTAI(og.jd.DateToUTC(date));
+export function DateToTAI(date) {
+    return UTCtoTAI(DateToUTC(date));
 };
 
 /**
@@ -220,8 +224,8 @@ og.jd.DateToTAI = function (date) {
  * @param {number} jd - UTC julian date.
  * @returns {number} TAI julian date
  */
-og.jd.UTCtoTAI = function (jd) {
-    var leapSeconds = og.jd.leapSecondsTable;
+export function UTCtoTAI(jd) {
+    var leapSeconds = leapSecondsTable;
 
     var index = og.utils.binarySearch(leapSeconds, jd, function (a, b) {
         return a - b.jd;
@@ -238,12 +242,12 @@ og.jd.UTCtoTAI = function (jd) {
     var offset = leapSeconds[index].leapSeconds;
 
     if (index !== 0) {
-        if ((leapSeconds[index].jd - jd) * og.jd.SECONDS_PER_DAY > offset) {
+        if ((leapSeconds[index].jd - jd) * SECONDS_PER_DAY > offset) {
             offset = leapSeconds[index - 1].leapSeconds;
         }
     }
 
-    return jd + offset * og.jd.ONE_BY_SECONDS_PER_DAY;
+    return jd + offset * ONE_BY_SECONDS_PER_DAY;
 };
 
 /**
@@ -251,8 +255,8 @@ og.jd.UTCtoTAI = function (jd) {
  * @param {number} tai - TAI julian date.
  * @returns {number} UTC julian date
  */
-og.jd.TAItoUTC = function (tai) {
-    var leapSeconds = og.jd.leapSecondsTable;
+export function TAItoUTC(tai) {
+    var leapSeconds = leapSecondsTable;
     var index = og.utils.binarySearch(leapSeconds, tai, function (a, b) {
         return a - b.jd;
     });
@@ -262,24 +266,24 @@ og.jd.TAItoUTC = function (tai) {
     }
 
     if (index >= leapSeconds.length) {
-        return tai - leapSeconds[index - 1].leapSeconds * og.jd.ONE_BY_SECONDS_PER_DAY;
+        return tai - leapSeconds[index - 1].leapSeconds * ONE_BY_SECONDS_PER_DAY;
     }
 
     if (index === 0) {
-        return tai - leapSeconds[0].leapSeconds * og.jd.ONE_BY_SECONDS_PER_DAY;
+        return tai - leapSeconds[0].leapSeconds * ONE_BY_SECONDS_PER_DAY;
     }
 
-    var diff = (leapSeconds[index].jd - tai) * og.jd.SECONDS_PER_DAY;
+    var diff = (leapSeconds[index].jd - tai) * SECONDS_PER_DAY;
 
     if (diff === 0) {
-        return tai - leapSeconds[index].leapSeconds * og.jd.ONE_BY_SECONDS_PER_DAY;
+        return tai - leapSeconds[index].leapSeconds * ONE_BY_SECONDS_PER_DAY;
     }
 
     if (diff <= 1.0) {
         return null;
     }
 
-    return tai - leapSeconds[index - 1].leapSeconds * og.jd.ONE_BY_SECONDS_PER_DAY;
+    return tai - leapSeconds[index - 1].leapSeconds * ONE_BY_SECONDS_PER_DAY;
 };
 
 /**
@@ -287,11 +291,11 @@ og.jd.TAItoUTC = function (tai) {
  * @param {number} utc - UTC julian date.
  * @returns {Date} JavaScript Date object
  */
-og.jd.UTCtoDate = function (utc) {
+export function UTCtoDate(utc) {
     var julianDayNumber = utc | 0;
-    var secondsOfDay = (utc - julianDayNumber) * og.jd.SECONDS_PER_DAY;
+    var secondsOfDay = (utc - julianDayNumber) * SECONDS_PER_DAY;
 
-    if (secondsOfDay >= og.jd.SECONDS_PER_12_HOURS) {
+    if (secondsOfDay >= SECONDS_PER_12_HOURS) {
         julianDayNumber++;
     }
 
@@ -306,12 +310,12 @@ og.jd.UTCtoDate = function (utc) {
     var month = (J + 2 - 12 * L) | 0;
     var year = (100 * (N - 49) + I + L) | 0;
 
-    var hour = secondsOfDay * og.jd.ONE_BY_SECONDS_PER_HOUR | 0;
-    var remainingSeconds = secondsOfDay - hour * og.jd.SECONDS_PER_HOUR;
-    var minute = remainingSeconds * og.jd.ONE_BY_SECONDS_PER_MINUTE | 0;
-    remainingSeconds = remainingSeconds - minute * og.jd.SECONDS_PER_MINUTE;
+    var hour = secondsOfDay * ONE_BY_SECONDS_PER_HOUR | 0;
+    var remainingSeconds = secondsOfDay - hour * SECONDS_PER_HOUR;
+    var minute = remainingSeconds * ONE_BY_SECONDS_PER_MINUTE | 0;
+    remainingSeconds = remainingSeconds - minute * SECONDS_PER_MINUTE;
     var second = remainingSeconds | 0;
-    var millisecond = (remainingSeconds - second) * og.jd.MILLISECONDS_PER_SECOND | 0;
+    var millisecond = (remainingSeconds - second) * MILLISECONDS_PER_SECOND | 0;
 
     hour += 12;
     if (hour > 23) {
@@ -326,15 +330,15 @@ og.jd.UTCtoDate = function (utc) {
  * @param {number} tai - TAI julian date.
  * @returns {Date} JavaScript Date object
  */
-og.jd.TAItoDate = function (tai) {
+export function TAItoDate(tai) {
 
-    var utc = og.jd.TAItoUTC(tai);
+    var utc = TAItoUTC(tai);
     if (!utc) {
-        utc = og.jd.TAItoUTC(og.jd.addSeconds(tai, -1));
-        og.console.logWrn("og.jd.TAItoDate:136 - can't conv utc.");
+        utc = TAItoUTC(addSeconds(tai, -1));
+        og.console.logWrn("TAItoDate:336 - can't conv utc.");
     }
 
-    return og.jd.UTCtoDate(utc);
+    return UTCtoDate(utc);
 };
 
 /**
@@ -343,8 +347,8 @@ og.jd.TAItoDate = function (tai) {
  * @param {number} milliseconds - Milliseconds to add.
  * @returns {number} Julian date
  */
-og.jd.addMilliseconds = function (jd, milliseconds) {
-    return jd + milliseconds * og.jd.ONE_BY_MILLISECONDS_PER_DAY;
+export function addMilliseconds(jd, milliseconds) {
+    return jd + milliseconds * ONE_BY_MILLISECONDS_PER_DAY;
 };
 
 /**
@@ -353,8 +357,8 @@ og.jd.addMilliseconds = function (jd, milliseconds) {
  * @param {number} seconds - Seconds to add.
  * @returns {number} Julian date
  */
-og.jd.addSeconds = function (jd, seconds) {
-    return jd + seconds * og.jd.ONE_BY_SECONDS_PER_DAY;
+export function addSeconds(jd, seconds) {
+    return jd + seconds * ONE_BY_SECONDS_PER_DAY;
 };
 
 /**
@@ -363,8 +367,8 @@ og.jd.addSeconds = function (jd, seconds) {
  * @param {number} hours - Hours to add.
  * @returns {number} Julian date
  */
-og.jd.addHours = function (jd, hours) {
-    return jd + hours * og.jd.ONE_BY_HOURS_PER_DAY;
+export function addHours(jd, hours) {
+    return jd + hours * ONE_BY_HOURS_PER_DAY;
 };
 
 /**
@@ -373,8 +377,8 @@ og.jd.addHours = function (jd, hours) {
  * @param {number} minutes - Minutes to add.
  * @returns {number} Julian date
  */
-og.jd.addMinutes = function (jd, minutes) {
-    return jd + minutes * og.jd.MINUTES_PER_DAY;
+export function addMinutes(jd, minutes) {
+    return jd + minutes * MINUTES_PER_DAY;
 };
 
 /**
@@ -383,7 +387,7 @@ og.jd.addMinutes = function (jd, minutes) {
  * @param {number} days - Days to add.
  * @returns {number} Julian date
  */
-og.jd.addDays = function (jd, days) {
+export function addDays(jd, days) {
     return jd + days;
 };
 
@@ -392,10 +396,10 @@ og.jd.addDays = function (jd, days) {
  * @param {number} js - julian date.
  * @returns {number} Milliseconds
  */
-og.jd.getMilliseconds = function (jd) {
+export function getMilliseconds(jd) {
     var s = jd - (jd | 0);
-    s *= og.jd.SECONDS_PER_DAY;
-    return (s - (s | 0)) * og.jd.MILLISECONDS_PER_SECOND | 0;
+    s *= SECONDS_PER_DAY;
+    return (s - (s | 0)) * MILLISECONDS_PER_SECOND | 0;
 };
 
 /**
@@ -403,9 +407,9 @@ og.jd.getMilliseconds = function (jd) {
  * @param {number} js - julian date.
  * @returns {number} Seconds
  */
-og.jd.getSeconds = function (jd) {
+export function getSeconds(jd) {
     var s = jd - (jd | 0);
-    return s * og.jd.SECONDS_PER_DAY;
+    return s * SECONDS_PER_DAY;
 };
 
 /**
@@ -413,16 +417,16 @@ og.jd.getSeconds = function (jd) {
  * @param {number} js - julian date.
  * @returns {number} Hours
  */
-og.jd.getHours = function (jd) {
+export function getHours(jd) {
     var julianDayNumber = jd | 0;
-    var secondsOfDay = (jd - julianDayNumber) * og.jd.SECONDS_PER_DAY;
+    var secondsOfDay = (jd - julianDayNumber) * SECONDS_PER_DAY;
 
-    var hour = secondsOfDay * og.jd.ONE_BY_SECONDS_PER_HOUR | 0;
-    var remainingSeconds = secondsOfDay - hour * og.jd.SECONDS_PER_HOUR;
-    var minute = remainingSeconds * og.jd.ONE_BY_SECONDS_PER_MINUTE | 0;
-    remainingSeconds = remainingSeconds - minute * og.jd.SECONDS_PER_MINUTE;
+    var hour = secondsOfDay * ONE_BY_SECONDS_PER_HOUR | 0;
+    var remainingSeconds = secondsOfDay - hour * SECONDS_PER_HOUR;
+    var minute = remainingSeconds * ONE_BY_SECONDS_PER_MINUTE | 0;
+    remainingSeconds = remainingSeconds - minute * SECONDS_PER_MINUTE;
     var second = remainingSeconds | 0;
-    var millisecond = (remainingSeconds - second) * og.jd.MILLISECONDS_PER_SECOND | 0;
+    var millisecond = (remainingSeconds - second) * MILLISECONDS_PER_SECOND | 0;
 
     hour += 12 + minute / 60 + second / 3600 + millisecond / 1000;
     if (hour > 23) {
@@ -437,9 +441,9 @@ og.jd.getHours = function (jd) {
  * @param {number} js - julian date.
  * @returns {number} Minutes
  */
-og.jd.getMinutes = function (jd) {
+export function getMinutes(jd) {
     var s = jd - (jd | 0);
-    return s * og.jd.MINUTES_PER_DAY | 0;
+    return s * MINUTES_PER_DAY | 0;
 };
 
 /**
@@ -447,7 +451,7 @@ og.jd.getMinutes = function (jd) {
  * @param {number} js - julian date.
  * @returns {number} Days
  */
-og.jd.getDays = function (jd) {
+export function getDays(jd) {
     return jd | 0;
 };
 
@@ -456,8 +460,8 @@ og.jd.getDays = function (jd) {
  * @param {number} s - Seconds.
  * @returns {number} Days
  */
-og.jd.secondsToDays = function (s) {
-    return s * og.jd.ONE_BY_SECONDS_PER_DAY;
+export function secondsToDays(s) {
+    return s * ONE_BY_SECONDS_PER_DAY;
 };
 
 /**
@@ -465,45 +469,45 @@ og.jd.secondsToDays = function (s) {
  * @param {number} d - Days.
  * @returns {number} Seconds
  */
-og.jd.daysToSeconds = function (d) {
-    return d * og.jd.SECONDS_PER_DAY;
+export function daysToSeconds(d) {
+    return d * SECONDS_PER_DAY;
 };
 
-og.jd.__ls = function (jd, leapSeconds) {
+function __ls(jd, leapSeconds) {
     return {
         "jd": jd,
         "leapSeconds": leapSeconds
     };
 };
 
-og.jd.leapSecondsTable = [
-  og.jd.__ls(2441317.5, 10.0),  // 1972-01-01T00:00:00.000Z
-  og.jd.__ls(2441499.5, 11.0),  // 1972-07-01T00:00:00.000Z
-  og.jd.__ls(2441683.5, 12.0),  // 1973-01-01T00:00:00.000Z
-  og.jd.__ls(2442048.5, 13.0),  // 1974-01-01T00:00:00.000Z
-  og.jd.__ls(2442413.5, 14.0),  // 1975-01-01T00:00:00.000Z
-  og.jd.__ls(2442778.5, 15.0),  // 1976-01-01T00:00:00.000Z
-  og.jd.__ls(2443144.5, 16.0),  // 1977-01-01T00:00:00.000Z
-  og.jd.__ls(2443509.5, 17.0),  // 1978-01-01T00:00:00.000Z
-  og.jd.__ls(2443874.5, 18.0),  // 1979-01-01T00:00:00.000Z
-  og.jd.__ls(2444239.5, 19.0),  // 1980-01-01T00:00:00.000Z
-  og.jd.__ls(2444786.5, 20.0),  // 1981-07-01T00:00:00.000Z
-  og.jd.__ls(2445151.5, 21.0),  // 1982-07-01T00:00:00.000Z
-  og.jd.__ls(2445516.5, 22.0),  // 1983-01-01T00:00:00.000Z
-  og.jd.__ls(2446247.5, 23.0),  // 1985-07-01T00:00:00.000Z
-  og.jd.__ls(2447161.5, 24.0),  // 1988-01-01T00:00:00.000Z
-  og.jd.__ls(2447892.5, 25.0),  // 1990-01-01T00:00:00.000Z
-  og.jd.__ls(2448257.5, 26.0),  // 1991-01-01T00:00:00.000Z
-  og.jd.__ls(2448804.5, 27.0),  // 1992-07-01T00:00:00.000Z
-  og.jd.__ls(2449169.5, 28.0),  // 1993-07-01T00:00:00.000Z
-  og.jd.__ls(2449534.5, 29.0),  // 1994-07-01T00:00:00.000Z
-  og.jd.__ls(2450083.5, 30.0),  // 1996-01-01T00:00:00.000Z
-  og.jd.__ls(2450630.5, 31.0),  // 1997-07-01T00:00:00.000Z
-  og.jd.__ls(2451179.5, 32.0),  // 1999-01-01T00:00:00.000Z
-  og.jd.__ls(2453736.5, 33.0),  // 2006-01-01T00:00:00.000Z
-  og.jd.__ls(2454832.5, 34.0),  // 2009-01-01T00:00:00.000Z
-  og.jd.__ls(2456109.5, 35.0),  // 2012-07-01T00:00:00.000Z
-  og.jd.__ls(2457204.5, 36.0)   // 2015-07-01T00:00:00.000Z
+const leapSecondsTable = [
+    __ls(2441317.5, 10.0),  // 1972-01-01T00:00:00.000Z
+    __ls(2441499.5, 11.0),  // 1972-07-01T00:00:00.000Z
+    __ls(2441683.5, 12.0),  // 1973-01-01T00:00:00.000Z
+    __ls(2442048.5, 13.0),  // 1974-01-01T00:00:00.000Z
+    __ls(2442413.5, 14.0),  // 1975-01-01T00:00:00.000Z
+    __ls(2442778.5, 15.0),  // 1976-01-01T00:00:00.000Z
+    __ls(2443144.5, 16.0),  // 1977-01-01T00:00:00.000Z
+    __ls(2443509.5, 17.0),  // 1978-01-01T00:00:00.000Z
+    __ls(2443874.5, 18.0),  // 1979-01-01T00:00:00.000Z
+    __ls(2444239.5, 19.0),  // 1980-01-01T00:00:00.000Z
+    __ls(2444786.5, 20.0),  // 1981-07-01T00:00:00.000Z
+    __ls(2445151.5, 21.0),  // 1982-07-01T00:00:00.000Z
+    __ls(2445516.5, 22.0),  // 1983-01-01T00:00:00.000Z
+    __ls(2446247.5, 23.0),  // 1985-07-01T00:00:00.000Z
+    __ls(2447161.5, 24.0),  // 1988-01-01T00:00:00.000Z
+    __ls(2447892.5, 25.0),  // 1990-01-01T00:00:00.000Z
+    __ls(2448257.5, 26.0),  // 1991-01-01T00:00:00.000Z
+    __ls(2448804.5, 27.0),  // 1992-07-01T00:00:00.000Z
+    __ls(2449169.5, 28.0),  // 1993-07-01T00:00:00.000Z
+    __ls(2449534.5, 29.0),  // 1994-07-01T00:00:00.000Z
+    __ls(2450083.5, 30.0),  // 1996-01-01T00:00:00.000Z
+    __ls(2450630.5, 31.0),  // 1997-07-01T00:00:00.000Z
+    __ls(2451179.5, 32.0),  // 1999-01-01T00:00:00.000Z
+    __ls(2453736.5, 33.0),  // 2006-01-01T00:00:00.000Z
+    __ls(2454832.5, 34.0),  // 2009-01-01T00:00:00.000Z
+    __ls(2456109.5, 35.0),  // 2012-07-01T00:00:00.000Z
+    __ls(2457204.5, 36.0)   // 2015-07-01T00:00:00.000Z
 ];
 
-og.jd.J2000TAI = og.jd.UTCtoTAI(og.jd.J2000);
+export const J2000TAI = UTCtoTAI(J2000);
--- src/og/clock.js
+++ src/og/clock.js
@@ -1,7 +1,11 @@
-goog.provide('og.Clock');
+/**
+ * @module og/Clock
+ */
 
-goog.require('og.jd');
-goog.require('og.Events');
+'use strict';
+
+import { Events } from 'Events.js';
+import { jd } from 'astro/jd.js';
 
 /**
  * Class represents application timer that stores custom current julian datetime, and time speed multiplier.
@@ -12,143 +16,155 @@ goog.require('og.Events');
  * @param {number} [params.currentDate] - Julian current date. Default: current date.
  * @param {number} [params.multiplier=1.0] - Time speed multiolier.
  */
-og.Clock = function (params) {
-    params = params || {};
+class Clock {
 
-    this._id = og.Clock._staticCounter++;
-
-    /**
-     * Clock name.
-     * @public
-     * @type {string}
-     */
-    this.name = params.name || "";
-
-    /**
-     * Clock events.
-     * @public
-     * @type {og.Events}
-     */
-    this.events = new og.Events(og.Clock.eventList);
-
-    /**
-     * Start julian date clock loop.
-     * @public
-     * @type {number}
-     */
-    this.startDate = params.startDate || 0;
-
-    /**
-     * End julian date clock loop.
-     * @public
-     * @type {number}
-     */
-    this.endDate = params.endDate || 0;
-
-    var currentDate = params.currentDate || og.jd.DateToUTC(new Date());
-    if (params.startDate && currentDate < params.startDate) {
-        currentDate = params.startDate;
-    }
-    if (params.endDate && currentDate > params.endDate) {
-        currentDate = params.endDate;
-    }
-
-    /**
-     * Current julian datetime.
-     * @public
-     * @type {number}
-     */
-    this.currentDate = currentDate;
-
-    /**
-     * Timer speed multiplier.
-     * @public
-     * @type {number}
-     */
-    this.multiplier = params.multiplier !== undefined ? params.multiplier : 1.0;
-
-    /**
-     * Animation frame delta time.
-     * @public
-     * @readonly
-     * @type {number}
-     */
-    this.deltaTicks = 0;
-
-    /**
-     * Timer activity.
-     * @public
-     * @type {boolean}
-     */
-    this.active = true;
-};
-
-og.Clock._staticCounter = 0;
-
-og.Clock.eventList = [
-    "tick",
-    "end"
-];
-
-/**
- * Sets current clock datetime.
- * @public
- * @param {Object} date - JavaScript Date object.
- */
-og.Clock.prototype.setDate = function (date) {
-    var d = og.jd.DateToUTC(date);
-    if (this.startDate && d < this.startDate) {
-        d = this.startDate;
-    }
-    if (this.endDate && d > this.endDate) {
-        d = this.endDate;
-    }
-    this.currentDate = d;
-};
-
-/**
- * Returns current application date.
- * @public
- * @returns {Date} - Current date.
- */
-og.Clock.prototype.getDate = function () {
-    return og.jd.UTCtoDate(this.currentDate);
-};
-
-og.Clock.prototype.reset = function () {
-    if (this.startDate) {
-        this.currentDate = this.startDate;
-    }
-};
-
-og.Clock.prototype._tick = function (dt) {
-    this.deltaTicks = dt * this.multiplier;
-    if (this.active) {
-        var cd = og.jd.addMilliseconds(this.currentDate, this.deltaTicks);
-        if (this.multiplier > 0) {
-            if (this.endDate && cd > this.endDate) {
-                this.currentDate = this.startDate;
-                this.events.dispatch(this.events.end, this);
-            } else {
-                this.currentDate = cd;
-            }
-        } else { 
-            if (this.startDate && cd < this.startDate) {
-                this.currentDate = this.endDate;
-                this.events.dispatch(this.events.end, this);
-            } else {
-                this.currentDate = cd;
-            }            
+    static get _staticCounter() {
+        if (!this._counter && this._counter !== 0) {
+            this._counter = 0;
         }
-        this.events.dispatch(this.events.tick, this);
+        return this._counter;
+    }
+
+    static set _staticCounter(n) {
+        this._counter = n;
+    }
+
+    constructor(params) {
+        params = params || {};
+
+        this._id = Clock._staticCounter++;
+
+        /**
+         * Clock name.
+         * @public
+         * @type {string}
+         */
+        this.name = params.name || "";
+
+        /**
+         * Clock events.
+         * @public
+         * @type {Events}
+         */
+        this.events = new Events([
+            "tick",
+            "end"
+        ]);
+
+        /**
+         * Start julian date clock loop.
+         * @public
+         * @type {number}
+         */
+        this.startDate = params.startDate || 0;
+
+        /**
+         * End julian date clock loop.
+         * @public
+         * @type {number}
+         */
+        this.endDate = params.endDate || 0;
+
+        var currentDate = params.currentDate || jd.DateToUTC(new Date());
+        if (params.startDate && currentDate < params.startDate) {
+            currentDate = params.startDate;
+        }
+        if (params.endDate && currentDate > params.endDate) {
+            currentDate = params.endDate;
+        }
+
+        /**
+         * Current julian datetime.
+         * @public
+         * @type {number}
+         */
+        this.currentDate = currentDate;
+
+        /**
+         * Timer speed multiplier.
+         * @public
+         * @type {number}
+         */
+        this.multiplier = params.multiplier !== undefined ? params.multiplier : 1.0;
+
+        /**
+         * Animation frame delta time.
+         * @public
+         * @readonly
+         * @type {number}
+         */
+        this.deltaTicks = 0;
+
+        /**
+         * Timer activity.
+         * @public
+         * @type {boolean}
+         */
+        this.active = true;
+    }
+
+    /**
+     * Sets current clock datetime.
+     * @public
+     * @param {Object} date - JavaScript Date object.
+     */
+    setDate(date) {
+        var d = jd.DateToUTC(date);
+        if (this.startDate && d < this.startDate) {
+            d = this.startDate;
+        }
+        if (this.endDate && d > this.endDate) {
+            d = this.endDate;
+        }
+        this.currentDate = d;
+    }
+
+    /**
+     * Returns current application date.
+     * @public
+     * @returns {Date} - Current date.
+     */
+    getDate() {
+        return jd.UTCtoDate(this.currentDate);
+    }
+
+    reset() {
+        if (this.startDate) {
+            this.currentDate = this.startDate;
+        }
+    }
+
+    _tick(dt) {
+        this.deltaTicks = dt * this.multiplier;
+        if (this.active) {
+            var cd = jd.addMilliseconds(this.currentDate, this.deltaTicks);
+            if (this.multiplier > 0) {
+                if (this.endDate && cd > this.endDate) {
+                    this.currentDate = this.startDate;
+                    this.events.dispatch(this.events.end, this);
+                } else {
+                    this.currentDate = cd;
+                }
+            } else {
+                if (this.startDate && cd < this.startDate) {
+                    this.currentDate = this.endDate;
+                    this.events.dispatch(this.events.end, this);
+                } else {
+                    this.currentDate = cd;
+                }
+            }
+            this.events.dispatch(this.events.tick, this);
+        }
+    }
+
+    /**
+     * @public
+     * @param {Clock} clock - Clock instance to compare.
+     * @returns {boolean} - Returns true if a clock is the same instance.
+     */
+    equal = function (clock) {
+        return this._id === clock._id;
     }
 };
 
-/**
- * @public
- * @param {og.Clock} clock - Clock instance to compare.
- * @returns {boolean} - Returns true if a clock is the same instance.
- */
-og.Clock.prototype.equal = function (clock) {
-    return this._id === clock._id;
-};
+export { Clock };
