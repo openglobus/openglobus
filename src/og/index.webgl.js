a2e5a0da23f3c0cc7365f034257e6b2c15dec8ea
Zemledelec <mgevlich@gmail.com>
2018-12-14 12:13:03 +0300

    build

--- src/og/index.webgl.js
+++ src/og/index.webgl.js
@@ -2,7 +2,7 @@
 
 import { Framebuffer } from './webgl/Framebuffer.js';
 import { Handler } from './webgl/Handler.js';
-import { MultiFramebuffer } from './webgl/MultiFramebuffer.js';
+import { Multisample } from './webgl/Multisample.js';
 import { types } from './webgl/types.js';
 import { Program } from './webgl/Program.js';
 import { TextureAtlas } from './utils/TextureAtlas.js';
@@ -10,7 +10,7 @@ import { TextureAtlas } from './utils/TextureAtlas.js';
 export {
     Handler,
     Framebuffer,
-    MultiFramebuffer,
+    Multisample,
     types,
     Program,
     TextureAtlas
