0d78e6c7907e791c8f483cb281f59d67093890e1
Zemledelec <mgevlich@gmail.com>
2019-05-28 14:16:57 +0300

    fix og

--- src/og/scene/Planet.js
+++ src/og/scene/Planet.js
@@ -879,7 +879,7 @@ class Planet extends RenderNode {
         this._quadTree.renderTree(this.camera);
 
         //TODO:Abolish "magic" numbers
-        if (this.renderer.activeCamera.slope > 0.5 &&
+        if (this.renderer.activeCamera.slope > 0.6 &&
             this.renderer.activeCamera._lonLat.height < 850000.0 &&
             this.renderer.activeCamera._lonLat.height > 7000.0) {
 
