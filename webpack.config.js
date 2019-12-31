1f25fda25880ae9dedbd5a74b5144bd8476be253
Zemledelec <mgevlich@gmail.com>
2019-12-31 20:05:53 +0300

    Resolved conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,55 @@
 
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
 
 const LIBRARY_NAME = 'og';
 
-module.exports = {
-    entry: path.resolve(__dirname, 'src/og/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: LIBRARY_NAME + ".js",
-        library: LIBRARY_NAME,
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    plugins: [
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new ExtractTextPlugin(LIBRARY_NAME + ".css")
-    ],
-    optimization: {
-        usedExports: true,
-        concatenateModules: true,
-        occurrenceOrder: true
-    },
-    stats: {
-        maxModules: Infinity,
-        optimizationBailout: true
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [{
-                    loader: 'css-loader',
-                    options: {
-                        importLoaders: 1
-                    }
-                }]
+var config = (lib) => {
+    return {
+        entry: path.resolve(__dirname, 'src/og/index' + lib + 'js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: LIBRARY_NAME + lib + 'js',
+            library: LIBRARY_NAME,
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        },
+        plugins: [
+            new webpack.optimize.ModuleConcatenationPlugin(),
+            new MiniCssExtractPlugin({
+                filename: LIBRARY_NAME + lib + 'css',
+                chunkFilename: LIBRARY_NAME + lib + 'css'
             })
-        }]
+        ],
+        optimization: {
+            usedExports: true,
+            concatenateModules: true,
+            occurrenceOrder: true,
+            minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                hmr: false,
+                            },
+                        },
+                        'css-loader'
+                    ],
+                },
+            ],
+        }
     }
+};
+
+
+module.exports = (env, argv) => {
+    return config(argv.lib ? ('.' + argv.lib + '.') : '.');
 };
