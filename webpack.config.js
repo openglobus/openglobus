0e0015a9194b9611c1be9e637304af8a38ca6a4d
Zemledelec <mgevlich@gmail.com>
2020-01-02 21:59:04 +0300

    Making versioning.

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,21 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
+const childProcess = require('child_process');
 
 const LIBRARY_NAME = 'og';
 
 var config = (lib) => {
+
+    //NOT SURE
+    let tag = childProcess.execSync('git tag --points-at HEAD --sort -version:refname').toString().trim();
+    tag = tag.substr(1, tag.length);
+
     return {
-        entry: path.resolve(__dirname, 'src/og/index' + lib + 'js'),
+        entry: path.resolve(__dirname, 'src/og/index' + lib + '.js'),
         output: {
             path: path.resolve(__dirname, 'dist'),
-            filename: LIBRARY_NAME + lib + 'js',
+            filename: LIBRARY_NAME + lib + '-' + tag + '.js',
             library: LIBRARY_NAME,
             libraryTarget: 'umd',
             umdNamedDefine: true
@@ -21,8 +27,11 @@ var config = (lib) => {
         plugins: [
             new webpack.optimize.ModuleConcatenationPlugin(),
             new MiniCssExtractPlugin({
-                filename: LIBRARY_NAME + lib + 'css',
-                chunkFilename: LIBRARY_NAME + lib + 'css'
+                filename: LIBRARY_NAME + lib + '-' + tag + '.css',
+                chunkFilename: LIBRARY_NAME + lib + '-' + tag + '.css'
+            }),
+            new webpack.DefinePlugin({
+                __VERSION__: tag
             })
         ],
         optimization: {
@@ -52,5 +61,5 @@ var config = (lib) => {
 
 
 module.exports = (env, argv) => {
-    return config(argv.lib ? ('.' + argv.lib + '.') : '.');
+    return config(argv.lib ? ('.' + argv.lib) : '');
 };
