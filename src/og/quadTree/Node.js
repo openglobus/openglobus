fefcc99cc5d816f27429d8d96b28ffb7a26aaeaa
Zemledelec <mgevlich@gmail.com>
2020-03-29 18:31:44 +0300

    Returned previous version

--- src/og/quadTree/Node.js
+++ src/og/quadTree/Node.js
@@ -482,10 +482,15 @@ Node.prototype.renderTree = function (cam, maxZoom, terrainReadySegment, stopLoa
             this.traverseNodes(cam, maxZoom, terrainReadySegment, stopLoading);
         } else if ((!maxZoom && seg.acceptForRendering(cam)) || seg.tileZoom === maxZoom) {
             this.prepareForRendering(cam, altVis, inFrustum, terrainReadySegment, stopLoading);
-        } else if (maxZoom) {
-            this.traverseNodes(cam, maxZoom, terrainReadySegment);
-        } else if (seg.terrainReady && (seg.tileZoom < planet.terrain._maxNodeZoom)) {
-            this.traverseNodes(cam, maxZoom, seg, stopLoading);
+        } else if (((seg.tileZoom < planet.terrain._maxNodeZoom) && (seg.terrainReady && !maxZoom)) || maxZoom) {
+            if (seg.terrainReady) {
+                this.traverseNodes(cam, maxZoom, seg, stopLoading);
+            } else {
+                if (!stopLoading) {
+                    this.prepareForRendering(cam, altVis, false, terrainReadySegment);
+                }
+                this.traverseNodes(cam, maxZoom, terrainReadySegment, true);
+            }
         } else {
             this.prepareForRendering(cam, altVis, inFrustum, terrainReadySegment, stopLoading);
         }
