f1bc24d491ba0d9a36bc1e37e9dece291b8c2a19
Zemledelec <mgevlich@gmail.com>
2020-04-13 16:03:35 +0300

    Fixed normal map buffers for empty terrain provider

--- src/og/utils/NormalMapCreator.js
+++ src/og/utils/NormalMapCreator.js
@@ -123,7 +123,7 @@ NormalMapCreator.prototype._init = function () {
     this._normalMapVerticesTexture = this._handler.createEmptyTexture_l(this._width, this._height);
 
     //create vertices hasharray for different grid size segments from 2^4(16) to 2^7(128)
-    for (var p = 4; p <= 7; p++) {
+    for (var p = 1; p <= 7; p++) {
         var gs = Math.pow(2, p);
         var gs2 = (gs / 2);
         var vertices = new Float32Array((gs + 1) * (gs + 1) * 2);
