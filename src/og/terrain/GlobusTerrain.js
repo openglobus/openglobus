1dd049f60932c9a553b91ced157a89ca2358799e
Zemledelec <mgevlich@gmail.com>
2020-07-25 02:28:22 +0300

    Fixed srtm3 url path

--- src/og/terrain/GlobusTerrain.js
+++ src/og/terrain/GlobusTerrain.js
@@ -89,7 +89,7 @@ class GlobusTerrain extends EmptyTerrain {
          * @public
          * @type {string}
          */
-        this.url = options.url || "http://srtm3.openglobus.org/{z}/{y}/{x}.ddm";
+        this.url = options.url || "//srtm3.openglobus.org/{z}/{y}/{x}.ddm";
 
         this._dataType = "arrayBuffer";
 
