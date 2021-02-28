c857e45db4d3f12457d200364edfea3d940c1291
Silas Dohm <silas@sdohm.xyz>
2021-02-28 22:03:49 +0000

    SkyBox fix and SkyBox example

--- examples/skyBox/skyBox.js
+++ examples/skyBox/skyBox.js
@@ -15,7 +15,7 @@ var osm = new XYZ("OpenStreetMap", {
 
 var globus = new Globe({
 	    "target": "globus",
-	"skybox": SkyBox.createDefault('../../res/'),
+	    "skyBox": SkyBox.createDefault('../../res/'),
 	    "name": "Earth",
 	    "terrain": new GlobusTerrain(),
 	    "layers": [osm],
