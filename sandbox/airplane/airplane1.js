8940f482100bb7692a818097caef6f44e0109397
Pavel Sukhodolski <pavel.sukhodolski@microavia.com>
2021-06-30 12:12:56 +0300

    change sandbox example

--- sandbox/airplane/airplane1.js
+++ sandbox/airplane/airplane1.js
@@ -5,6 +5,7 @@ import { EntityCollection } from "../../src/og/entity/EntityCollection.js";
 import { Globe } from "../../src/og/Globe.js";
 import { XYZ } from "../../src/og/layer/XYZ.js";
 import { GlobusTerrain } from "../../src/og/terrain/GlobusTerrain.js";
+import { LonLat } from "../../src/og/LonLat.js";
 
 function rnd(min, max) {
     return Math.random() * (max - min) + min;
@@ -45,7 +46,14 @@ let globus = new Globe({
     ],
     terrain: new GlobusTerrain()
 });
-
+globus.planet.events.on("draw", () => {
+    geoObjects.each(function (e) {
+        let c = e.getLonLat();
+        e.setLonLat(new LonLat(c.lon + 0.1, c.lat > 89 ? -90 : c.lat + 0.1, c.height));
+        e.geoObject.setYaw(e.geoObject._yaw + 0.1);
+        e.geoObject.setPitch(e.geoObject._pitch + 0.2);
+        e.geoObject.setRoll(e.geoObject._roll + 0.3);
+    });
+});
 geoObjects.addTo(globus.planet);
 window.globus = globus;
-// window.go = go;
