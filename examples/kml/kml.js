44dfa43bd7b3d817e0c0a01ea0743ffc50fc7d6d
Thibaut Lassalle <thibaut.lassalle@gmail.com>
2021-09-07 18:28:48 -0700

    WIP first attempt to add Point support

--- examples/kml/kml.js
+++ examples/kml/kml.js
@@ -1,35 +1,60 @@
 
 const osm = new og.layer.XYZ('osm', { isBaseLayer: true, url: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' });
 const globus = new og.Globe({ target: 'globus', terrain: new og.terrain.GlobusTerrain(), layers: [osm] });
+const billboard = { src: './carrot.png', color: 'red' };
 let extent;
 
+/**
+ * Read the kml files as xml files and extract the coordinates tags.
+ * @param {File} file 
+ * @returns {Promise<Array>}
+ */
 const readKmlFile = file => {
   return new Promise(resolve => {
     const fileReader = new FileReader();
     fileReader.onload = async (i) => {
       const xmlDoc = (new DOMParser()).parseFromString(i.target.result, 'text/xml');
       const raw = Array.from(xmlDoc.getElementsByTagName('coordinates'));
-      const coordinates = raw.map(item => item.textContent.trim().split(' ').map(co => co.split(',').map(parseFloat)));
-      if (coordinates) {
-        const pathLonLat = coordinates.map(c => c.map(item => new og.LonLat(item[0], item[1], item[2])));
-        resolve(pathLonLat);
-      }
+      const coordinates = raw.map(item => item.textContent.trim().replace(/\n/g, ' ').split(' ').map(co => co.split(',').map(parseFloat)));
+      resolve(coordinates);
     };
     fileReader.readAsText(file);
   });
 };
 
+/**
+ * Creates billboard or polyline from array of lonlat.
+ * @param {Array} pathes 
+ * @param {string} color 
+ * @returns {Array<og.Entity>}
+ */
+const extractPathes = (pathes, color) => {
+  const entities = [];
+  pathes.forEach(kmlFile => kmlFile.forEach(path => {
+    if (path.length === 1) {
+      const lonlat = path[0];
+      const _entity = new og.Entity({ lonlat, billboard });
+      entities.push(_entity);
+    } else if (path.length > 1) {
+      const pathLonLat = path.map(item => new og.LonLat(item[0], item[1], item[2]));
+      const _entity = new og.Entity({ polyline: { pathLonLat: [pathLonLat], thickness: 3, color, isClosed: false } });
+      entities.push(_entity);
+    }
+  }));
+  return entities;
+};
+
 document.getElementById('upload').onchange = async e => {
   const color = document.getElementById('color').value;
+  billboard.color = color;
   const files = Array.from(e.target.files);
   const pathes = await Promise.all(files.map(f => readKmlFile(f)));
-  const pathLonLat = [];
-  pathes.forEach(p => p.forEach(p2 => pathLonLat.push(p2)));
-  const entity = new og.Entity({ polyline: { pathLonLat, thickness: 3, color, isClosed: false } });
-  const ptsLayer = new og.layer.Vector('pts', { entities: [entity] });
+  const entities = extractPathes(pathes, color);
+  const ptsLayer = new og.layer.Vector('pts', { entities });
   globus.planet.addLayer(ptsLayer);
-  extent = entity.getExtent();
-  globus.planet.flyExtent(entity.getExtent());
+  // extent = ptsLayer.getExtent(); doesn't work :(
+  extent = entities[0].getExtent();
+  globus.planet.flyExtent(extent);
   document.getElementById('viewExtent').style.display = 'inline';
 };
 
