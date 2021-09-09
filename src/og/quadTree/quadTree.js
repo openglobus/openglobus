c1f761ece2e2c5642cc3b2aba0ce0b92efc46c26
Zemledelec <mgevlich@gmail.com>
2021-09-09 11:27:15 +0300

    increased rendering maxNodes

--- src/og/quadTree/quadTree.js
+++ src/og/quadTree/quadTree.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 export const VISIBLE_DISTANCE = 3570.0;
-export const MAX_RENDERED_NODES = 500;
+export const MAX_RENDERED_NODES = 800;
 
 export const NW = 0;
 export const NE = 1;
@@ -25,40 +25,44 @@ export const OPSIDE = [S, W, N, E];
 /**
  * First index is {N,E,S,W} and second is {NW,NE,SW,SE}
  */
-export const NEIGHBOUR =
-    [[-1, -1, NW, NE],
+export const NEIGHBOUR = [
+    [-1, -1, NW, NE],
     [NE, -1, SE, -1],
     [SW, SE, -1, -1],
-    [-1, NW, -1, SW]];
+    [-1, NW, -1, SW]
+];
 
 /**
  * Neighbor's oposite part. For example oposite side
  * on the east neighbor side is: [S][SE] = NE
  */
-export const OPPART =
-    [[SW, SE, NW, NE],
+export const OPPART = [
+    [SW, SE, NW, NE],
     [NE, NW, SE, SW],
     [SW, SE, NW, NE],
-    [NE, NW, SE, SW]];
+    [NE, NW, SE, SW]
+];
 
 /**
  * Neighbos's opside array order. For example NW node
  * by E side array index is 0, SE node by S side is 1.
  */
-export const NOPSORD =
-    [[0, 1, 0, 1],
+export const NOPSORD = [
+    [0, 1, 0, 1],
     [0, 0, 1, 1],
     [0, 1, 0, 1],
-    [0, 0, 1, 1]];
+    [0, 0, 1, 1]
+];
 
 /**
  * First index is {NW,NE,SW,SE}, another one is {N,E,S,W}
  */
-export const COMSIDE =
-    [[true, false, false, true],
+export const COMSIDE = [
+    [true, false, false, true],
     [true, true, false, false],
     [false, false, true, true],
-    [false, true, true, false]];
+    [false, true, true, false]
+];
 
 /**
  * Gets segment part left to right or up to downo ffset against neighbour side.
@@ -66,8 +70,8 @@ export const COMSIDE =
  */
 export const PARTOFFSET = [
     /*     N  E  S  W */
-    /*NW*/[0, 1, 0, 0],
-    /*NE*/[1, 0, 0, 0],
-    /*SW*/[0, 1, 0, 1],
-    /*SE*/[1, 1, 1, 1]
+    /*NW*/ [0, 1, 0, 0],
+    /*NE*/ [1, 0, 0, 0],
+    /*SW*/ [0, 1, 0, 1],
+    /*SE*/ [1, 1, 1, 1]
 ];
