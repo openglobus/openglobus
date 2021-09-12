fffb42588d9086dcba9326919f610f0f2895ada7
Thibaut Lassalle <thibaut.lassalle@gmail.com>
2021-09-12 10:42:28 -0700

    rename functions

--- examples/kml/kml.js
+++ examples/kml/kml.js
@@ -9,7 +9,7 @@ let kmlExtent;
  * @param {File} file 
  * @returns {Promise<Array>}
  */
-const readKmlFile = file => {
+const extractCoordonatesFromKml = file => {
   return new Promise(resolve => {
     const fileReader = new FileReader();
     fileReader.onload = async (i) => {
@@ -24,12 +24,11 @@ const readKmlFile = file => {
 
 /**
  * Creates billboards or polylines from array of lonlat.
- * @param {Array} pathes 
+ * @param {Array} coordonates
  * @param {string} color 
  * @returns {Array<og.Entity>}
  */
-const extractPathes = (pathes, color) => {
-  const entities = [];
+const convertCoordonatesIntoEntities = (coordinates, color) => {
   const extent = new og.Extent(new og.LonLat(180.0, 90.0), new og.LonLat(-180.0, -90.0));
   const addToExtent = (c) => {
     const lon = c[0], lat = c[1];
@@ -38,22 +37,23 @@ const extractPathes = (pathes, color) => {
     if (lon > extent.northEast.lon) extent.northEast.lon = lon;
     if (lat > extent.northEast.lat) extent.northEast.lat = lat;
   };
-
-  pathes.forEach(kmlFile => kmlFile.forEach(path => {
+  const _pathes = [];
+  coordinates.forEach(kmlFile => kmlFile.forEach(p => _pathes.push(p)));
+  const entities = _pathes.map(path => {
     if (path.length === 1) {
       const lonlat = path[0];
       const _entity = new og.Entity({ lonlat, billboard });
-      entities.push(_entity);
       addToExtent(lonlat);
+      return _entity;
     } else if (path.length > 1) {
       const pathLonLat = path.map(item => {
         addToExtent(item);
         return new og.LonLat(item[0], item[1], item[2]);
       });
       const _entity = new og.Entity({ polyline: { pathLonLat: [pathLonLat], thickness: 3, color, isClosed: false } });
-      entities.push(_entity);
+      return _entity;
     }
-  }));
+  });
   return { entities, extent };
 };
 
@@ -61,8 +61,8 @@ document.getElementById('upload').onchange = async e => {
   const color = document.getElementById('color').value;
   billboard.color = color;
   const files = Array.from(e.target.files);
-  const pathes = await Promise.all(files.map(f => readKmlFile(f)));
-  const { entities, extent } = extractPathes(pathes, color);
+  const coordonates = await Promise.all(files.map(f => extractCoordonatesFromKml(f)));
+  const { entities, extent } = convertCoordonatesIntoEntities(coordonates, color);
   const ptsLayer = new og.layer.Vector('kmls', { entities });
   globus.planet.addLayer(ptsLayer);
   kmlExtent = extent;
