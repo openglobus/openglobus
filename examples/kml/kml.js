d5b22d99aacbe12b6cebbbe26c713b7264d731b7
Thibaut Lassalle <thibaut.lassalle@gmail.com>
2021-09-12 09:53:34 -0700

    use a custom extent

--- examples/kml/kml.js
+++ examples/kml/kml.js
@@ -2,7 +2,7 @@
 const osm = new og.layer.XYZ('osm', { isBaseLayer: true, url: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' });
 const globus = new og.Globe({ target: 'globus', terrain: new og.terrain.GlobusTerrain(), layers: [osm] });
 const billboard = { src: './carrot.png', color: 'red' };
-let extent;
+let kmlExtent;
 
 /**
  * Read the kml files as xml files and extract the coordinates tags.
@@ -23,25 +23,38 @@ const readKmlFile = file => {
 };
 
 /**
- * Creates billboard or polyline from array of lonlat.
+ * Creates billboards or polylines from array of lonlat.
  * @param {Array} pathes 
  * @param {string} color 
  * @returns {Array<og.Entity>}
  */
 const extractPathes = (pathes, color) => {
   const entities = [];
+  const extent = new og.Extent(new og.LonLat(180.0, 90.0), new og.LonLat(-180.0, -90.0));
+  const addToExtent = (c) => {
+    const lon = c[0], lat = c[1];
+    if (lon < extent.southWest.lon) extent.southWest.lon = lon;
+    if (lat < extent.southWest.lat) extent.southWest.lat = lat;
+    if (lon > extent.northEast.lon) extent.northEast.lon = lon;
+    if (lat > extent.northEast.lat) extent.northEast.lat = lat;
+  };
+
   pathes.forEach(kmlFile => kmlFile.forEach(path => {
     if (path.length === 1) {
       const lonlat = path[0];
       const _entity = new og.Entity({ lonlat, billboard });
       entities.push(_entity);
+      addToExtent(lonlat);
     } else if (path.length > 1) {
-      const pathLonLat = path.map(item => new og.LonLat(item[0], item[1], item[2]));
+      const pathLonLat = path.map(item => {
+        addToExtent(item);
+        return new og.LonLat(item[0], item[1], item[2]);
+      });
       const _entity = new og.Entity({ polyline: { pathLonLat: [pathLonLat], thickness: 3, color, isClosed: false } });
       entities.push(_entity);
     }
   }));
-  return entities;
+  return { entities, extent };
 };
 
 document.getElementById('upload').onchange = async e => {
@@ -49,15 +62,14 @@ document.getElementById('upload').onchange = async e => {
   billboard.color = color;
   const files = Array.from(e.target.files);
   const pathes = await Promise.all(files.map(f => readKmlFile(f)));
-  const entities = extractPathes(pathes, color);
-  const ptsLayer = new og.layer.Vector('pts', { entities });
+  const { entities, extent } = extractPathes(pathes, color);
+  const ptsLayer = new og.layer.Vector('pts', { entities, clampToGround: true });
   globus.planet.addLayer(ptsLayer);
-  // extent = ptsLayer.getExtent(); doesn't work :(
-  extent = entities[0].getExtent();
+  kmlExtent = extent;
   globus.planet.flyExtent(extent);
   document.getElementById('viewExtent').style.display = 'inline';
 };
 
 document.getElementById('viewExtent').onclick = () => {
-  globus.planet.flyExtent(extent);
+  globus.planet.flyExtent(kmlExtent);
 };
