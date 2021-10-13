2460bc5b4865997f9e0a46eaf1e53d866ab17df8
matthieu.barbot <matthieu.barbot@he-arc.ch>
2021-10-14 00:05:53 +0200

    EntityCollection units tests

---
+++ tests/entity/EntityCollection.test.js
@@ -0,0 +1,26 @@
+import { Entity } from '../../src/og/entity/Entity';
+import { EntityCollection } from '../../src/og/entity/EntityCollection';
+
+
+test('Testing Entity', () => {
+    let entityCollection = new EntityCollection();
+
+    entityCollection.setVisibility(true);
+    expect(entityCollection.getVisibility()).toBe(true);
+
+
+    entityCollection.setOpacity(10);
+    expect(entityCollection.getOpacity()).toBe(10);
+
+    let entity = new Entity();
+
+    expect(entityCollection.addEntities([entity])).toBe(entityCollection);
+    expect(entityCollection.belongs(entity)).toBe(true);
+    entityCollection.removeEntity(entity);
+    expect(entityCollection.belongs(entity)).toBeFalsy();
+    entityCollection.addEntities([entity]);
+    entityCollection.clear();
+    expect(entityCollection.belongs(entity)).toBeFalsy();
+
+
+});
