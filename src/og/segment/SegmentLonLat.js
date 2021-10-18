d97eebc3ab8a9879e13532dc93fcef3e47c3e556
Zemledelec <mgevlich@gmail.com>
2021-10-18 22:21:58 +0300

    Fixed isNorth property

--- src/og/segment/SegmentLonLat.js
+++ src/og/segment/SegmentLonLat.js
@@ -26,7 +26,6 @@ let _tempHigh = new Vec3(),
  * @param {og.Extent} extent - Segment WGS84 extent.
  */
 class SegmentLonLat extends Segment {
-
     /**
      * @param {quadTree.Node} node - Segment node.
      * @param {scene.Planet} planet - Current planet scene.
@@ -35,22 +34,26 @@ class SegmentLonLat extends Segment {
      */
     constructor(node, planet, tileZoom, extent) {
         super(node, planet, tileZoom, extent);
-        this._isNorth = false;
+
+        // TODO:
+        // be carefull with functions in constructor _assignTileIndexes
+        // this._isNorth = false;
+
         this._projection = EPSG4326;
         this._extentMerc = new Extent(
             extent.southWest.forwardMercatorEPS01(),
             extent.northEast.forwardMercatorEPS01()
         );
         this.isPole = true;
-    };
+    }
 
     _setExtentLonLat() {
         this._extentLonLat = this._extent;
-    };
+    }
 
     projectNative(coords) {
         return coords;
-    };
+    }
 
     getTerrainPoint(xyz, insideSegmentPosition, res, normal) {
         res.copy(this.planet.ellipsoid.hitRay(xyz, xyz.negateTo().normalize()));
@@ -58,7 +61,7 @@ class SegmentLonLat extends Segment {
             normal.copy(res.normal());
         }
         return xyz.length() - res.length();
-    };
+    }
 
     acceptForRendering(camera) {
         var maxPoleZoom;
@@ -72,8 +75,10 @@ class SegmentLonLat extends Segment {
             let Yz = Math.floor((mercator.MIN_LAT - lat) / _pieceSize);
             maxPoleZoom = 12 - Math.floor(Yz / 16);
         }
-        return Segment.prototype.acceptForRendering.call(this, camera) || this.tileZoom >= maxPoleZoom;
-    };
+        return (
+            Segment.prototype.acceptForRendering.call(this, camera) || this.tileZoom >= maxPoleZoom
+        );
+    }
 
     _assignTileIndexes() {
         var tileZoom = this.tileZoom;
@@ -97,7 +102,7 @@ class SegmentLonLat extends Segment {
                 (mercator.MIN_LAT - lat) / (extent.northEast.lat - extent.southWest.lat)
             );
         }
-   
+
         var p2 = 1 << tileZoom;
         this.tileXE = (this.tileX + 1) % p2;
         this.tileXW = (p2 + this.tileX - 1) % p2;
@@ -106,127 +111,127 @@ class SegmentLonLat extends Segment {
         this.tileYS = this.tileY + 1;
 
         this.tileIndex = Layer.getTileIndex(this.tileX, this.tileY, tileZoom);
-    };
+    }
 
     _createPlainVertices() {
-      var gridSize = this.planet.terrain.gridSizeByZoom[this.tileZoom];
+        var gridSize = this.planet.terrain.gridSizeByZoom[this.tileZoom];
 
-      var e = this._extent,
-          fgs = this.planet.terrain.plainGridSize;
-      var lonSize = e.getWidth();
-      var latSize = e.getHeight();
-      var llStep = lonSize / Math.max(fgs, gridSize);
-      var ltStep = latSize / gridSize;
-      var esw_lon = e.southWest.lon,
-          ene_lat = e.northEast.lat;
-      var dg = Math.max(fgs / gridSize, 1),
-          gs = Math.max(fgs, gridSize) + 1;
-      var r2 = this.planet.ellipsoid._invRadii2;
-      var ind = 0,
-          nmInd = 0;
-      const gsgs = gs * gs;
+        var e = this._extent,
+            fgs = this.planet.terrain.plainGridSize;
+        var lonSize = e.getWidth();
+        var latSize = e.getHeight();
+        var llStep = lonSize / Math.max(fgs, gridSize);
+        var ltStep = latSize / gridSize;
+        var esw_lon = e.southWest.lon,
+            ene_lat = e.northEast.lat;
+        var dg = Math.max(fgs / gridSize, 1),
+            gs = Math.max(fgs, gridSize) + 1;
+        var r2 = this.planet.ellipsoid._invRadii2;
+        var ind = 0,
+            nmInd = 0;
+        const gsgs = gs * gs;
 
-      var gridSize3 = (gridSize + 1) * (gridSize + 1) * 3;
+        var gridSize3 = (gridSize + 1) * (gridSize + 1) * 3;
 
-      this.plainNormals = new Float32Array(gridSize3);
-      this.plainVertices = new Float64Array(gridSize3);
-      this.plainVerticesHigh = new Float32Array(gridSize3);
-      this.plainVerticesLow = new Float32Array(gridSize3);
+        this.plainNormals = new Float32Array(gridSize3);
+        this.plainVertices = new Float64Array(gridSize3);
+        this.plainVerticesHigh = new Float32Array(gridSize3);
+        this.plainVerticesLow = new Float32Array(gridSize3);
 
-      this.normalMapNormals = new Float32Array(gsgs * 3);
-      this.normalMapVertices = new Float64Array(gsgs * 3);
-      this.normalMapVerticesHigh = new Float32Array(gsgs * 3);
-      this.normalMapVerticesLow = new Float32Array(gsgs * 3);
+        this.normalMapNormals = new Float32Array(gsgs * 3);
+        this.normalMapVertices = new Float64Array(gsgs * 3);
+        this.normalMapVerticesHigh = new Float32Array(gsgs * 3);
+        this.normalMapVerticesLow = new Float32Array(gsgs * 3);
 
-      let xmin = 549755748352.0,
-          xmax = -549755748352.0,
-          ymin = 549755748352.0,
-          ymax = -549755748352.0,
-          zmin = 549755748352.0,
-          zmax = -549755748352.0;
+        let xmin = 549755748352.0,
+            xmax = -549755748352.0,
+            ymin = 549755748352.0,
+            ymax = -549755748352.0,
+            zmin = 549755748352.0,
+            zmax = -549755748352.0;
 
-      var verts = this.plainVertices,
-          vertsHigh = this.plainVerticesHigh,
-          vertsLow = this.plainVerticesLow,
-          norms = this.plainNormals,
-          nmVerts = this.normalMapVertices,
-          nmVertsHigh = this.normalMapVerticesHigh,
-          nmVertsLow = this.normalMapVerticesLow,
-          nmNorms = this.normalMapNormals;
+        var verts = this.plainVertices,
+            vertsHigh = this.plainVerticesHigh,
+            vertsLow = this.plainVerticesLow,
+            norms = this.plainNormals,
+            nmVerts = this.normalMapVertices,
+            nmVertsHigh = this.normalMapVerticesHigh,
+            nmVertsLow = this.normalMapVerticesLow,
+            nmNorms = this.normalMapNormals;
 
-      for (var k = 0; k < gsgs; k++) {
-          var j = k % gs,
-              i = ~~(k / gs);
+        for (var k = 0; k < gsgs; k++) {
+            var j = k % gs,
+                i = ~~(k / gs);
 
-          var v = this.planet.ellipsoid.lonLatToCartesian(
-              new LonLat(esw_lon + j * llStep, ene_lat - i * ltStep)
-          );
-          var nx = v.x * r2.x,
-              ny = v.y * r2.y,
-              nz = v.z * r2.z;
-          var l = 1.0 / Math.sqrt(nx * nx + ny * ny + nz * nz);
-          var nxl = nx * l,
-              nyl = ny * l,
-              nzl = nz * l;
+            var v = this.planet.ellipsoid.lonLatToCartesian(
+                new LonLat(esw_lon + j * llStep, ene_lat - i * ltStep)
+            );
+            var nx = v.x * r2.x,
+                ny = v.y * r2.y,
+                nz = v.z * r2.z;
+            var l = 1.0 / Math.sqrt(nx * nx + ny * ny + nz * nz);
+            var nxl = nx * l,
+                nyl = ny * l,
+                nzl = nz * l;
 
-          Vec3.doubleToTwoFloats(v, _tempHigh, _tempLow);
+            Vec3.doubleToTwoFloats(v, _tempHigh, _tempLow);
 
-          nmVerts[nmInd] = v.x;
-          nmVertsHigh[nmInd] = _tempHigh.x;
-          nmVertsLow[nmInd] = _tempLow.x;
-          nmNorms[nmInd++] = nxl;
+            nmVerts[nmInd] = v.x;
+            nmVertsHigh[nmInd] = _tempHigh.x;
+            nmVertsLow[nmInd] = _tempLow.x;
+            nmNorms[nmInd++] = nxl;
 
-          nmVerts[nmInd] = v.y;
-          nmVertsHigh[nmInd] = _tempHigh.y;
-          nmVertsLow[nmInd] = _tempLow.y;
-          nmNorms[nmInd++] = nyl;
+            nmVerts[nmInd] = v.y;
+            nmVertsHigh[nmInd] = _tempHigh.y;
+            nmVertsLow[nmInd] = _tempLow.y;
+            nmNorms[nmInd++] = nyl;
 
-          nmVerts[nmInd] = v.z;
-          nmVertsHigh[nmInd] = _tempHigh.z;
-          nmVertsLow[nmInd] = _tempLow.z;
-          nmNorms[nmInd++] = nzl;
+            nmVerts[nmInd] = v.z;
+            nmVertsHigh[nmInd] = _tempHigh.z;
+            nmVertsLow[nmInd] = _tempLow.z;
+            nmNorms[nmInd++] = nzl;
 
-          if (i % dg === 0 && j % dg === 0) {
-              verts[ind] = v.x;
-              vertsHigh[ind] = _tempHigh.x;
-              vertsLow[ind] = _tempLow.x;
-              norms[ind++] = nxl;
+            if (i % dg === 0 && j % dg === 0) {
+                verts[ind] = v.x;
+                vertsHigh[ind] = _tempHigh.x;
+                vertsLow[ind] = _tempLow.x;
+                norms[ind++] = nxl;
 
-              verts[ind] = v.y;
-              vertsHigh[ind] = _tempHigh.y;
-              vertsLow[ind] = _tempLow.y;
-              norms[ind++] = nyl;
+                verts[ind] = v.y;
+                vertsHigh[ind] = _tempHigh.y;
+                vertsLow[ind] = _tempLow.y;
+                norms[ind++] = nyl;
 
-              verts[ind] = v.z;
-              vertsHigh[ind] = _tempHigh.z;
-              vertsLow[ind] = _tempLow.z;
-              norms[ind++] = nzl;
+                verts[ind] = v.z;
+                vertsHigh[ind] = _tempHigh.z;
+                vertsLow[ind] = _tempLow.z;
+                norms[ind++] = nzl;
 
-              if (v.x < xmin) xmin = v.x;
-              if (v.x > xmax) xmax = v.x;
-              if (v.y < ymin) ymin = v.y;
-              if (v.y > ymax) ymax = v.y;
-              if (v.z < zmin) zmin = v.z;
-              if (v.z > zmax) zmax = v.z;
-          }
-      }
+                if (v.x < xmin) xmin = v.x;
+                if (v.x > xmax) xmax = v.x;
+                if (v.y < ymin) ymin = v.y;
+                if (v.y > ymax) ymax = v.y;
+                if (v.z < zmin) zmin = v.z;
+                if (v.z > zmax) zmax = v.z;
+            }
+        }
 
-      this.terrainVertices = verts;
-      this.terrainVerticesHigh = vertsHigh;
-      this.terrainVerticesLow = vertsLow;
+        this.terrainVertices = verts;
+        this.terrainVerticesHigh = vertsHigh;
+        this.terrainVerticesLow = vertsLow;
 
-      //store raw normals
-      this.normalMapNormalsRaw = new Float32Array(nmNorms.length);
-      this.normalMapNormalsRaw.set(nmNorms);
+        //store raw normals
+        this.normalMapNormalsRaw = new Float32Array(nmNorms.length);
+        this.normalMapNormalsRaw.set(nmNorms);
 
-      let x = (xmax - xmin) * 0.5,
-          y = (ymax - ymin) * 0.5,
-          z = (zmax - zmin) * 0.5;
+        let x = (xmax - xmin) * 0.5,
+            y = (ymax - ymin) * 0.5,
+            z = (zmax - zmin) * 0.5;
 
-      this._plainRadius = Math.sqrt(x * x + y * y + z * z);
+        this._plainRadius = Math.sqrt(x * x + y * y + z * z);
 
-      this.plainReady = true;
-    };
+        this.plainReady = true;
+    }
 
     _assignGlobalTextureCoordinates() {
         var e = this._extent;
@@ -234,7 +239,7 @@ class SegmentLonLat extends Segment {
         this._globalTextureCoordinates[1] = (90 - e.northEast.lat) / 180.0;
         this._globalTextureCoordinates[2] = (e.northEast.lon + 180.0) / 360.0;
         this._globalTextureCoordinates[3] = (90 - e.southWest.lat) / 180.0;
-    };
+    }
 
     _collectVisibleNodes() {
         if (this._isNorth) {
@@ -242,11 +247,11 @@ class SegmentLonLat extends Segment {
         } else {
             this.planet._visibleNodesSouth[this.node.nodeId] = this.node;
         }
-    };
+    }
 
     isEntityInside(e) {
         return this._extent.isInside(e._lonlat);
-    };
+    }
 
     _getLayerExtentOffset(layer) {
         var v0s = layer._extent;
@@ -258,23 +263,23 @@ class SegmentLonLat extends Segment {
         var dSize_x = (v0t.northEast.lon - v0t.southWest.lon) / sSize_x;
         var dSize_y = (v0t.northEast.lat - v0t.southWest.lat) / sSize_y;
         return [dV0s_x, dV0s_y, dSize_x, dSize_y];
-    };
+    }
 
     layerOverlap(layer) {
         return this._extent.overlaps(layer._extent);
-    };
+    }
 
     getDefaultTexture() {
         return this._isNorth ? this.planet.solidTextureOne : this.planet.solidTextureTwo;
-    };
+    }
 
     getExtentLonLat() {
         return this._extent;
-    };
+    }
 
     getExtentMerc() {
         return this._extentMerc;
-    };
+    }
 
     getNodeState() {
         var vn;
@@ -284,11 +289,10 @@ class SegmentLonLat extends Segment {
             vn = this.planet._visibleNodesSouth[this.node.nodeId];
         }
         return (vn && vn.state) || quadTree.NOTRENDERING;
-    };
+    }
 
     _freeCache() {
         //empty for a time
-    };
-
+    }
 }
 export { SegmentLonLat };
