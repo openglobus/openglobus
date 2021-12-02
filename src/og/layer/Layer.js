3c93abb6f8cb5f3f44c53959f10fd0d7e2ba7a39
Zemledelec <mgevlich@gmail.com>
2021-12-02 11:38:16 +0300

    #438 WIP.

--- src/og/layer/Layer.js
+++ src/og/layer/Layer.js
@@ -22,7 +22,6 @@ export const FADING_FACTOR = 0.29;
  * @param {String} [name="noname"] - Layer name.
  * @param {Object} [options] - Layer options:
  * @param {number} [options.opacity=1.0] - Layer opacity.
- * @param {Array.<number>} [options.transparentColor=[-1,-1,-1]] - RGB color that defines transparent color. (exactly 3 entries)
  * @param {number} [options.minZoom=0] - Minimal visibility zoom level.
  * @param {number} [options.maxZoom=0] - Maximal visibility zoom level.
  * @param {string} [options.attribution] - Layer attribution that displayed in the attribution area on the screen.
