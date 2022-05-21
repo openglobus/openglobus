3df869f24810c1e8435c66acad12d9e46b6970b2
--t-bo-- <thibaut@astra.com>
2022-05-21 09:00:43 -0700

    add unit tests for kml

--- tests/layer/KML.test.js
+++ tests/layer/KML.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import { KML } from '../../src/og/layer/KML'
 
-const readFile = (name) => {
+const readKml = (name) => {
     return new Promise((resolve) => {
         fs.readFile(name, 'utf8', (err, xmlString) => {
             const parser = new DOMParser();
@@ -20,9 +20,14 @@ describe('kml files', () => {
 
     it('styled kml', async () => {
         const kml = new KML('name', {})
-        const xmlDoc = await readFile('./tests/layer/KML.styled.kml')
+        const xmlDoc = await readKml('./tests/layer/KML.styled.kml')
         const { entities, extent } = kml._convertKMLintoEntities(xmlDoc)
         expect(entities.length).toBe(1)
+        expect(extent.northEast.lon).toBe(138.64)
+        expect(extent.northEast.lat).toBe(-34.91)
+        expect(extent.southWest.lon).toBe(138.6)
+        expect(extent.southWest.lat).toBe(-34.94)
+        expect(entities.at(0).properties.name).toBe('')
     })
 
 })
