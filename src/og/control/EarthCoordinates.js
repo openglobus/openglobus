48e709b94cc3bfa4b57049862aba81621531f20b
Zemledelec <mgevlich@gmail.com>
2022-07-21 16:14:56 +0400

    #536 improved EarthCoordinates control

--- src/og/control/EarthCoordinates.js
+++ src/og/control/EarthCoordinates.js
@@ -1,64 +1,36 @@
-/**
- * @module og/control/EarthCoordinates
- */
+'use strict';
 
-"use strict";
+import * as units from '../utils/units.js';
+import { heightMode } from '../utils/units.js';
+import { Control } from './Control.js';
+import { throttle } from '../utils/shared.js';
+import { LonLat } from '../LonLat.js';
 
-import { Control } from "./Control.js";
+const DECIMAL_TEMPLATE =
+    `<div class="og-lat-side"></div><div class="og-lat-val"></div>
+    <div class="og-lon-side"></div><div class="og-lon-val"></div>
+    <div class="og-height"></div>
+    <div class="og-units-height"></div>`;
 
-function dec2deg(base) {
-    var t;
-    var degrees = base < 0 ? Math.ceil(base) : Math.floor(base);
-    var minutes = Math.floor((t = Math.abs(base - degrees) * 60));
-    var seconds = Math.floor((t - minutes) * 6000);
-    seconds = seconds / 100.0;
-    return (
-        numToFixedString(degrees, 3) +
-        "\u00B0" +
-        numToFixedString(minutes, 2) +
-        "\u0027" +
-        numToFixedString(seconds.toFixed(2), 2) +
-        "\u0022"
-    );
-}
+const DEGREE_TEMPLATE =
+    `<div class="og-lat-side"></div><div class="og-lat-val"></div>
+    <div class="og-lon-side"></div><div class="og-lon-val"></div>
+    <div class="og-height"></div>
+    <div class="og-units-height"></div>`;
 
-function numToFixedString(num, fixed) {
-    var dl = num.toString().split(".")[0].length;
-    var white = "&nbsp;";
-    for (var i = dl; i < fixed; i++) {
-        white += "&nbsp;&nbsp;";
-    }
-    return white + num.toString();
-}
+const CENTER_SVG = '<svg width="12" height="12"><g><path stroke-width="1" stroke-opacity="1" d="M6 0L6 12M0 6L12 6" stroke="#337ab7"></path></g></svg>';
 
-function toDecimal(ll) {
-    return ll.lat.toFixed(5) + ", " + ll.lon.toFixed(5);
-}
-
-function toDegrees(ll) {
-    return dec2deg(ll.lat) + ", " + dec2deg(ll.lon);
-}
-
-function toMercator(ll) {
-    var m = ll.forwardMercator();
-    return m.lat.toFixed(5) + ", " + m.lon.toFixed(5);
-}
-
-const DisplayTypesConverters = [toDecimal, toDegrees, toMercator];
+const TYPE_HTML = [DECIMAL_TEMPLATE, DEGREE_TEMPLATE];
 
 /**
  * Control displays mouse or screen center Earth coordinates.
  * @class
- * @extends {Control}
+ * @extends {og.control.Control}
  * @param {Object} [options] - Options:
  * @param {Boolean} [options.center] - Earth coordiantes by screen center otherwise mouse pointer. False is default.
  * @param {Boolean} [options.type] - Coordinates shown: 0 - is decimal degrees, 1 - degrees, 2 - mercator geodetic coordinates.
  */
 class EarthCoordinates extends Control {
-    /**
-     *
-     * @param {Object} [options] - Options:
-     */
     constructor(options) {
         super(options);
 
@@ -69,154 +41,159 @@ class EarthCoordinates extends Control {
          * @private
          * @type {Boolean}
          */
-        this._displayType = options.type || 0;
+        this._type = options.type || 0;
 
-        /**
-         * Current coordinates type converter.
-         * @private
-         * @function
-         */
-        this._converter = DisplayTypesConverters[0];
+        this._TYPE_FUNC = [this._SHOW_DECIMAL, this._SHOW_DEGREE];
 
-        /**
-         * Display dom element.
-         * @private
-         * @type {Object}
-         */
-        this._display = null;
-
-        /**
-         * Screen center or mouse pointer coordinates show flag.
-         * @private
-         * @type {Boolean}
-         */
-
-        var pad = false;
-        if (
-            /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-                navigator.userAgent
-            )
-        ) {
-            pad = true;
-        }
-
-        this._center = options.center || pad;
-
-        this._centerDiv = null;
+        this._showFn = null;
 
         /**
          * Current position.
          * @public
-         * @type {Vec3}
+         * @type {og.Vec3}
          */
-        this.position = null;
+        this._lonLat = new LonLat();
+
+        this._latSideEl = null;
+        this._lonSideEl = null;
+        this._latValEl = null;
+        this._lonValEl = null;
+        this._heightEl = null;
+
+        this._altUnitVal = options.altitudeUnit || "m";
+        this._heightModeVal = options.heightMode || "ell";
+
+        this._altUnit = units[this._altUnitVal];
+        this._heightMode = heightMode[this._heightModeVal];
+    }
+
+    _SHOW_DECIMAL(ll) {
+        if (ll) {
+            let lat = ll.lat,
+                lon = ll.lon;
+
+            if (lat >= 0) {
+                this._latSideEl.innerHTML = 'N';
+            } else {
+                this._latSideEl.innerHTML = 'S';
+            }
+
+            if (lon >= 0) {
+                this._lonSideEl.innerHTML = 'E';
+            } else {
+                this._lonSideEl.innerHTML = 'W';
+            }
+
+            this._latValEl.innerHTML = Math.abs(lat).toFixed(5) + '°';
+            this._lonValEl.innerHTML = Math.abs(lon).toFixed(5) + '°';
+        }
+    }
+
+    _SHOW_DEGREE(ll) {
+        if (ll) {
+            let lat = ll.lat,
+                lon = ll.lon;
+
+            if (lat >= 0) {
+                this._latSideEl.innerHTML = 'N';
+            } else {
+                this._latSideEl.innerHTML = 'S';
+            }
+
+            if (lon >= 0) {
+                this._lonSideEl.innerHTML = 'E';
+            } else {
+                this._lonSideEl.innerHTML = 'W';
+            }
+
+            let t = 0;
+
+            let deg = lat < 0 ? Math.ceil(lat) : Math.floor(lat);
+            let min = Math.floor(t = Math.abs((lat - deg)) * 60);
+            let sec = Math.floor((t - min) * 6000) / 100.0;
+            this._latValEl.innerHTML = deg + '°' + min + "'" + sec.toFixed(0) + '"';
+
+            deg = lon < 0 ? Math.ceil(lon) : Math.floor(lon);
+            min = Math.floor(t = Math.abs((lon - deg)) * 60);
+            sec = Math.floor((t - min) * 6000) / 100.0;
+            this._lonValEl.innerHTML = deg + '°' + min + "'" + sec.toFixed(0) + '"';
+        }
+    }
+
+    _createCenterEl() {
+        let el = document.createElement('div');
+        el.className = 'og-center-icon';
+        el.innerHTML = CENTER_SVG;
+        return el;
+    }
+
+    _updateUnits() {
+        this._heightMode = heightMode[this._heightModeVal];
+        this._altUnit = units[this._altUnitVal];
+        this._el.querySelector(".og-units-height").innerHTML = units.toString(this._altUnit);
+        this._showHeight();
+    }
+
+    _refreshCoordinates() {
+
+        if (this._type >= this._TYPE_FUNC.length) {
+            this._type = 0;
+        }
+
+        this._el.innerHTML = TYPE_HTML[this._type];
+
+        this._latSideEl = this._el.querySelector(".og-lat-side");
+        this._lonSideEl = this._el.querySelector(".og-lon-side");
+        this._latValEl = this._el.querySelector(".og-lat-val");
+        this._lonValEl = this._el.querySelector(".og-lon-val");
+        this._heightEl = this._el.querySelector(".og-height");
+
+        this._showFn = this._TYPE_FUNC[this._type];
+        this._showFn(this._lonLat);
     }
 
     oninit() {
-        this._display = document.createElement("div");
-        this._display.className = "ogEarthCoordinatesControl";
-        var that = this;
+        this._el = document.createElement('div');
+        this._el.classList.add("og-coordinates");
 
-        function _refresh(el) {
-            if (that._displayType >= DisplayTypesConverters.length) {
-                that._displayType = 0;
-            }
+        this.renderer.div.appendChild(this._el);
 
-            if (that._displayType == 0) {
-                el.style.width = "275px";
-            } else if (that._displayType == 1) {
-                el.style.width = "355px";
-            } else if (that._displayType == 2) {
-                el.style.width = "350px";
-            }
-            that._converter = DisplayTypesConverters[that._displayType];
-            that._showPosition();
-        }
+        this._el.addEventListener("click", () => {
+            this._type++;
+            this._refreshCoordinates();
+            this._updateUnits();
+            this._showHeight();
+        });
 
-        this._display.onclick = function (e) {
-            that._displayType += 1;
-            _refresh(this);
-        };
+        this.renderer.div.appendChild(this._createCenterEl());
 
-        this.renderer.div.appendChild(this._display);
+        this.renderer.activeCamera.events.on("moveend", this._grabCoordinates, this);
+        this.renderer.activeCamera.events.on("moveend", throttle((e) => this._showHeight(), 800, true), this);
 
-        _refresh(this._display);
+        this._refreshCoordinates();
 
-        this._centerDiv = document.createElement("div");
-        this._centerDiv.className = "ogCenterIcon";
-        this._centerDiv.innerHTML =
-            '<svg width="12" height="12"><g><path stroke-width="1" stroke-opacity="1" d="M6 0L6 12M0 6L12 6" stroke="#009DFF"></path></g></svg>';
-        this.renderer.div.appendChild(this._centerDiv);
-
-        if (this._center) {
-            this.renderer.activeCamera.events.on("moveend", this._grabCoordinates, this);
-            this._centerDiv.style.display = "block";
-        } else {
-            this.renderer.events.on("mousemove", this._onMouseMove, this);
-            this._centerDiv.style.display = "none";
-        }
-    }
-
-    /**
-     * Sets coordinates capturing type.
-     * @public
-     * @param {Boolean} center - True - capture screen center, false - mouse pointer.
-     */
-    setCenter(center) {
-        if (center != this._center) {
-            this._center = center;
-            if (center) {
-                this.renderer.events.off("mousemove", this._onMouseMove);
-                this.renderer.activeCamera.events.on("moveend", this._grabCoordinates, this);
-                this._centerDiv.style.display = "block";
-            } else {
-                this.renderer.events.off("draw", this._draw);
-                this.renderer.events.on("mousemove", this._onMouseMove, this);
-                this._centerDiv.style.display = "none";
-            }
-        }
-    }
-
-    _showPosition() {
-        if (this.position) {
-            this.position.height =
-                this.position.height > 10000 || this.position.height < -10000
-                    ? 0
-                    : this.position.height;
-            this._display.innerHTML =
-                "Lat/Lon: " +
-                this._converter(this.position) +
-                " h(m): " +
-                (this.position.height > 0
-                    ? "~" + (Math.round(this.position.height) / 1000).toFixed(3) * 1000
-                    : "-");
-        } else {
-            this._display.innerHTML = "Lat/Lon: " + "_____________________";
-        }
+        this._updateUnits();
     }
 
     _grabCoordinates() {
-        var r = this.renderer;
-        this.position = this.planet.getLonLatFromPixelTerrain(r.handler.getCenter());
-        this._showPosition();
+        let r = this.renderer;
+        this._lonLat = this.planet.getLonLatFromPixelTerrain(r.handler.getCenter());
+        this._showFn(this._lonLat);
     }
 
-    _onMouseMove() {
-        var r = this.renderer;
-        var ms = r.events.mouseState;
-        if (
-            !(ms.leftButtonDown || ms.rightButtonDown) &&
-            r.controlsBag.scaleRot <= 0 &&
-            !r.activeCamera._flying
-        ) {
-            this.position = this.planet.getLonLatFromPixelTerrain(ms, true);
-            this._showPosition();
+    async _showHeight() {
+        if (this._lonLat) {
+            let alt = 0;
+            if (this._heightMode === heightMode.ell) {
+                alt = await this.planet.getHeightAboveELL(this._lonLat);
+                alt = units.convertExt(true, units.m, this._altUnit, alt);
+            } else if (this._heightMode === heightMode.msl) {
+                alt = await this.planet.getHeightDefault(this._lonLat);
+                alt = units.convertExt(true, units.m, this._altUnit, alt);
+            }
+            this._heightEl.innerHTML = alt.toString();
         }
     }
-}
-
-export function earthCoordinates(options) {
-    return new EarthCoordinates(options);
 }
 
 export { EarthCoordinates };
