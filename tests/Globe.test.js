867f0e148701860ec94b44098e967b2a5e40872d
Pavel Sukhodolski <pavel.sukhodolski@microavia.com>
2022-09-02 17:52:46 +0300

    remove travis pipelines remove jsdocs directory and use doc generation from config and package parallelize build pipeline and make it faster remove .vscode update dependencies fix vulnerability remove fonts dependencies nad make it from npx fix vulnerability in code

--- tests/Globe.test.js
+++ tests/Globe.test.js
@@ -1,12 +1,7 @@
-import { Globe } from "../src/og/Globe";
 import { Worker } from "./worker";
-import { JSDOM } from "jsdom";
 
 window.Worker = Worker;
 
-const dom = new JSDOM('<html><div id="globus_viewport_0"></p>');
-global.document = dom.window.document;
-global.window = dom.window;
 
 global.URL.createObjectURL = jest.fn(() => "");
 /**
