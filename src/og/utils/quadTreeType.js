aaf516b01b933e2eed05a9c8e52ffc659ab07933
Zemledelec <mgevlich@gmail.com>
2022-11-07 23:49:13 +0400

    Update index with quadTree startegy

--- src/og/utils/quadTreeType.js
+++ src/og/utils/quadTreeType.js
@@ -1,8 +1,8 @@
 "use strict";
+
 import { EarthQuadTreeStrategy } from "../quadTree/EarthQuadTreeStrategy.js";
 import { MarsQuadTreeStrategy } from "../quadTree/MarsQuadTreeStrategy.js";
 import { Wgs84QuadTreeStrategy } from "../quadTree/Wgs84QuadTreeStrategy.js";
-
 
 export const quadTreeStrategyType = {
     earth: EarthQuadTreeStrategy,
