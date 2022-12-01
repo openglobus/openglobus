b58a3f8fa7eb54ef35d45fc82735864662f793bb
busatoa <abusato@inogs.it>
2022-12-01 11:11:58 +0100

    #611 change filed name activationIconClasses and updated class info

--- src/og/control/selection/Selection.js
+++ src/og/control/selection/Selection.js
@@ -14,12 +14,17 @@ import { SelectionScene } from "./SelectionScene.js";
  * @class 
  * @extends {Control}
  * @param {Object} [options] - Control options.
+ * @param {Array} [options.activationIconClasses] - array with a list of classes to change the button icons
  * @param {boolean} [options.ignoreTerrain=false].
  * @param {function} options.onSelect - callback (extent) => {} where extent is selected extent array [minLon,minLat,maxLon,maxLat]
  * @param {boolean} [options.autoSelectionHide=false] - clear selection rectangle  after passing extent to callback
  * @example:
+ * to use bootstrap icons, include 
+ *  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+ *  
  * new Selection({
- *      ignoreTerrain: false,
+ *       activationIconClasses: ['bi', 'bi-bounding-box-circles'],
+ *       ignoreTerrain: false,
  *       autoSelectionHide:true,
  *       onSelect: (extent) => {
  *
@@ -49,7 +54,7 @@ import { SelectionScene } from "./SelectionScene.js";
 class Selection extends Control {
     constructor(options = {}) {
         super(options);
-        this._activationButtonClasses = options.activationButtonClasses || null;
+        this._activationIconClasses = options.activationIconClasses || null;
       
         this._selectorScene = new SelectionScene({
             name: `selectionScene:${this._id}`,
@@ -99,8 +104,8 @@ class Selection extends Control {
                     
        btnIcon.setAttribute("style", "font-size:24px; display: flex; justify-content: center; align-items: center; background: none !important");
        
-        if (this._activationButtonClasses){
-            btnIcon.classList.add(... this._activationButtonClasses);
+        if (this._activationIconClasses){
+            btnIcon.classList.add(... this._activationIconClasses);
         } else {
             let defaultIcon = document.createElement("span");
             defaultIcon.setAttribute("style", "width: 24px;height: 24px;border: 1px solid black;");
