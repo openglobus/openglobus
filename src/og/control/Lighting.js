a00a936164cd0c86e2419172805535a4fd84e9c8
Zemledelec <mgevlich@gmail.com>
2023-02-13 11:03:17 +0400

    #510 wip.lighting control with new sliders

--- src/og/control/Lighting.js
+++ src/og/control/Lighting.js
@@ -8,125 +8,37 @@ import { Control } from "./Control.js";
 import { Dialog } from '../ui/Dialog.js';
 import { View } from '../ui/View.js';
 import { ToggleButton } from "../ui/ToggleButton.js";
+import { Slider } from "../ui/ToggleButton.js";
 
-
-const TEMPLATE = `<div class="og-lighing">
-
-       <div class="og-screen-options">
-
-         <div class="og-option">
-          <div class="og-caption">Lighting enabled<input type="checkbox" id="lighting" name="light"/></div>
-         </div>
+const TEMPLATE =
+    `<div class="og-lighing">
 
          <div class="og-option">
-            <div class="og-caption">Gamma</div>
-            <div class="og-slider">
-                <input type="range" id="gamma" name="gamma" value="0.0" min="0.0" max="5.0" step="0.01" />
-            </div>
-            <div class="og-value gamma"></div>
+           <div class="og-caption">Lighting enabled<input type="checkbox" id="lighting" name="light"/></div>
          </div>
-         <div class="og-option">
-            <div class="og-caption">Exposure</div>
-            <div class="og-slider">
-                <input type="range" id="exposure" name="exposure" value="0.0" min="0.0" max="12.0" step="0.01" />
-            </div>
-            <div class="og-value exposure"></div>
+
+         <div class="og-option og-gamma">
          </div>
-       </div>
+         
+         <div class="og-option og-exposure">
+         </div>
        
-       <div class="og-layers">
-         <div class="og-caption">Select layer:</div>
-         <select id="layers"></select>
-       </div>
+         <div class="og-layers">
+           <div class="og-caption">Select layer:</div>
+           <select id="layers"></select>
+         </div>
 
-        <div class="og-caption">Opacity</div>
-            <div class="og-option">
-            <div class="og-label">A</div>
-            <div class="og-slider">
-                <input type="range" id="opacity" name="opacity" value="0.0" min="0.0" max="2.0" step="0.01" />
-            </div>
-            <div class="og-value opacity"></div>
-        </div>
+         <div class="og-option og-opacity">
+         </div>
+
+         <div class="og-option og-diffuse">
+         </div>
       
-       <div class="og-color-options">
-         <div class="og-caption">Ambient</div>
-         <div class="og-option">
-            <div class="og-label">R</div>
-            <div class="og-slider">
-                <input type="range" id="ambient-r" name="ambient-r" value="0.0" min="0.0" max="5.0" step="0.01" />
-            </div>
-            <div class="og-value ambient-r"></div>
-         </div>
-         <div class="og-option">
-            <div class="og-label">G</div>
-            <div class="og-slider">
-                <input type="range" id="ambient-g" name="ambient-g" value="0.0" min="0.0" max="5.0" step="0.01" />
-            </div>
-            <div class="og-value ambient-g"></div>
-         </div>
-         <div class="og-option">
-            <div class="og-label">B</div>
-            <div class="og-slider">
-                <input type="range" id="ambient-b" name="ambient-b" value="0.0" min="0.0" max="5.0" step="0.01" />
-            </div>
-            <div class="og-value ambient-b"></div>
+         <div class="og-option og-ambient">
          </div>
 
-         <div class="og-caption">Diffuse</div>
-         <div class="og-option">
-            <div class="og-label">R</div>
-            <div class="og-slider">
-                <input type="range" id="diffuse-r" name="diffuse-r" value="0.0" min="0.0" max="5.0" step="0.01" />
-            </div>
-            <div class="og-value diffuse-r"></div>
-         </div>
-         <div class="og-option">
-            <div class="og-label">G</div>
-            <div class="og-slider">
-                <input type="range" id="diffuse-g" name="diffuse-g" value="0.0" min="0.0" max="5.0" step="0.01" />
-            </div>
-            <div class="og-value diffuse-g"></div>
-         </div>
-         <div class="og-option">
-            <div class="og-label">B</div>
-            <div class="og-slider">
-                <input type="range" id="diffuse-b" name="diffuse-b" value="0.0" min="0.0" max="5.0" step="0.01" />
-            </div>
-            <div class="og-value diffuse-b"></div>
-         </div>
-
-
-         <div class="og-caption">Specular</div>
-         <div class="og-option">
-            <div class="og-label">R</div>
-            <div class="og-slider">
-                <input type="range" id="specular-r" name="specular-r" value="0.0" min="0.0" max="0.05" step="0.000001" />
-            </div>
-            <div class="og-value specular-r"></div>
-         </div>
-         <div class="og-option">
-            <div class="og-label">G</div>
-            <div class="og-slider">
-                <input type="range" id="specular-g" name="specular-g" value="0.0" min="0.0" max="0.05" step="0.000001" />
-            </div>
-            <div class="og-value specular-g"></div>
-         </div>
-         <div class="og-option">
-            <div class="og-label">B</div>
-            <div class="og-slider">
-                <input type="range" id="specular-b" name="specular-b" value="0.0" min="0.0" max="0.05" step="0.000001" />
-            </div>
-            <div class="og-value specular-b"></div>
-         </div>
-
-         <div class="og-caption">Shininess</div>
-         <div class="og-option" style="margin-left: 8px;">
-            <div class="og-slider">
-                <input type="range" id="shininess" name="shininess" value="0.0" min="0.0" max="1000.0" step="0.1" />
-            </div>
-            <div class="og-value shininess"></div>
-         </div>
-       </div>
+         <div class="og-option og-specular">
+         </div>        
 
     </div>`;
 
@@ -212,102 +124,103 @@ class Lighting extends Control {
             this.bindLayer(_this.planet.getLayerByName(e.target.value));
         });
 
-        document.getElementById("gamma").addEventListener("input", function (e) {
-            _this.planet.renderer.gamma = Number(this.value);
-            document.querySelector(".og-value.gamma").innerText = this.value;
-        });
-        document.getElementById("exposure").addEventListener("input", function (e) {
-            _this.planet.renderer.exposure = Number(this.value);
-            document.querySelector(".og-value.exposure").innerText = this.value;
-        });
 
-        document.querySelector(".og-value.gamma").innerText = this.planet.renderer.gamma.toString();
-        document.querySelector(".og-value.exposure").innerText =
-            this.planet.renderer.exposure.toString();
-
-        document.getElementById("gamma").value = this.planet.renderer.gamma;
-        document.getElementById("exposure").value = this.planet.renderer.exposure;
-
-        document.getElementById("opacity").addEventListener("input", function (e) {
-            if (_this._selectedLayer) {
-                _this._selectedLayer.opacity = Number(this.value);
-            }
-            document.querySelector(".og-value.opacity").innerText = this.value;
-        });
-
-        document.getElementById("ambient-r").addEventListener("input", function (e) {
-            _this._selectedLayer._ambient[0] = Number(this.value);
-            document.querySelector(".og-value.ambient-r").innerText = this.value;
-        });
-        document.getElementById("ambient-g").addEventListener("input", function (e) {
-            _this._selectedLayer._ambient[1] = Number(this.value);
-            document.querySelector(".og-value.ambient-g").innerText = this.value;
-        });
-        document.getElementById("ambient-b").addEventListener("input", function (e) {
-            _this._selectedLayer._ambient[2] = Number(this.value);
-            document.querySelector(".og-value.ambient-b").innerText = this.value;
-        });
-
-        document.getElementById("diffuse-r").addEventListener("input", function (e) {
-            _this._selectedLayer._diffuse[0] = Number(this.value);
-            document.querySelector(".og-value.diffuse-r").innerText = this.value;
-        });
-        document.getElementById("diffuse-g").addEventListener("input", function (e) {
-            _this._selectedLayer._diffuse[1] = Number(this.value);
-            document.querySelector(".og-value.diffuse-g").innerText = this.value;
-        });
-        document.getElementById("diffuse-b").addEventListener("input", function (e) {
-            _this._selectedLayer._diffuse[2] = Number(this.value);
-            document.querySelector(".og-value.diffuse-b").innerText = this.value;
-        });
-
-        document.getElementById("specular-r").addEventListener("input", function (e) {
-            _this._selectedLayer._specular[0] = Number(this.value);
-            document.querySelector(".og-value.specular-r").innerText = this.value;
-        });
-        document.getElementById("specular-g").addEventListener("input", function (e) {
-            _this._selectedLayer._specular[1] = Number(this.value);
-            document.querySelector(".og-value.specular-g").innerText = this.value;
-        });
-        document.getElementById("specular-b").addEventListener("input", function (e) {
-            _this._selectedLayer._specular[2] = Number(this.value);
-            document.querySelector(".og-value.specular-b").innerText = this.value;
-        });
-
-        document.getElementById("shininess").addEventListener("input", function (e) {
-            _selectedLayer._specular[3] = Number(this.value);
-            document.querySelector(".og-value.shininess").innerText = this.value;
-        });
+        // document.getElementById("gamma").addEventListener("input", function (e) {
+        //     _this.planet.renderer.gamma = Number(this.value);
+        //     document.querySelector(".og-value.gamma").innerText = this.value;
+        // });
+        // document.getElementById("exposure").addEventListener("input", function (e) {
+        //     _this.planet.renderer.exposure = Number(this.value);
+        //     document.querySelector(".og-value.exposure").innerText = this.value;
+        // });
+        //
+        // document.querySelector(".og-value.gamma").innerText = this.planet.renderer.gamma.toString();
+        // document.querySelector(".og-value.exposure").innerText =
+        //     this.planet.renderer.exposure.toString();
+        //
+        // document.getElementById("gamma").value = this.planet.renderer.gamma;
+        // document.getElementById("exposure").value = this.planet.renderer.exposure;
+        //
+        // document.getElementById("opacity").addEventListener("input", function (e) {
+        //     if (_this._selectedLayer) {
+        //         _this._selectedLayer.opacity = Number(this.value);
+        //     }
+        //     document.querySelector(".og-value.opacity").innerText = this.value;
+        // });
+        //
+        // document.getElementById("ambient-r").addEventListener("input", function (e) {
+        //     _this._selectedLayer._ambient[0] = Number(this.value);
+        //     document.querySelector(".og-value.ambient-r").innerText = this.value;
+        // });
+        // document.getElementById("ambient-g").addEventListener("input", function (e) {
+        //     _this._selectedLayer._ambient[1] = Number(this.value);
+        //     document.querySelector(".og-value.ambient-g").innerText = this.value;
+        // });
+        // document.getElementById("ambient-b").addEventListener("input", function (e) {
+        //     _this._selectedLayer._ambient[2] = Number(this.value);
+        //     document.querySelector(".og-value.ambient-b").innerText = this.value;
+        // });
+        //
+        // document.getElementById("diffuse-r").addEventListener("input", function (e) {
+        //     _this._selectedLayer._diffuse[0] = Number(this.value);
+        //     document.querySelector(".og-value.diffuse-r").innerText = this.value;
+        // });
+        // document.getElementById("diffuse-g").addEventListener("input", function (e) {
+        //     _this._selectedLayer._diffuse[1] = Number(this.value);
+        //     document.querySelector(".og-value.diffuse-g").innerText = this.value;
+        // });
+        // document.getElementById("diffuse-b").addEventListener("input", function (e) {
+        //     _this._selectedLayer._diffuse[2] = Number(this.value);
+        //     document.querySelector(".og-value.diffuse-b").innerText = this.value;
+        // });
+        //
+        // document.getElementById("specular-r").addEventListener("input", function (e) {
+        //     _this._selectedLayer._specular[0] = Number(this.value);
+        //     document.querySelector(".og-value.specular-r").innerText = this.value;
+        // });
+        // document.getElementById("specular-g").addEventListener("input", function (e) {
+        //     _this._selectedLayer._specular[1] = Number(this.value);
+        //     document.querySelector(".og-value.specular-g").innerText = this.value;
+        // });
+        // document.getElementById("specular-b").addEventListener("input", function (e) {
+        //     _this._selectedLayer._specular[2] = Number(this.value);
+        //     document.querySelector(".og-value.specular-b").innerText = this.value;
+        // });
+        //
+        // document.getElementById("shininess").addEventListener("input", function (e) {
+        //     _selectedLayer._specular[3] = Number(this.value);
+        //     document.querySelector(".og-value.shininess").innerText = this.value;
+        // });
 
         if (this.planet) {
             this.planet.events.on("layeradd", this._onLayerAdd, this);
             this.planet.events.on("layerremove", this._onLayerRemove, this);
 
-            document.getElementById("ambient-r").value = this.planet._ambient[0];
-            document.getElementById("ambient-g").value = this.planet._ambient[1];
-            document.getElementById("ambient-b").value = this.planet._ambient[2];
-
-            document.getElementById("diffuse-r").value = this.planet._diffuse[0];
-            document.getElementById("diffuse-g").value = this.planet._diffuse[1];
-            document.getElementById("diffuse-b").value = this.planet._diffuse[2];
-
-            document.getElementById("specular-r").value = this.planet._specular[0];
-            document.getElementById("specular-g").value = this.planet._specular[1];
-            document.getElementById("specular-b").value = this.planet._specular[2];
-            document.getElementById("shininess").value = this.planet._specular[3];
-
-            document.querySelector(".og-value.ambient-r").innerText = this.planet._ambient[0].toString();
-            document.querySelector(".og-value.ambient-g").innerText = this.planet._ambient[1].toString();
-            document.querySelector(".og-value.ambient-b").innerText = this.planet._ambient[2].toString();
-
-            document.querySelector(".og-value.diffuse-r").innerText = this.planet._diffuse[0].toString();
-            document.querySelector(".og-value.diffuse-g").innerText = this.planet._diffuse[1].toString();
-            document.querySelector(".og-value.diffuse-b").innerText = this.planet._diffuse[2].toString();
-
-            document.querySelector(".og-value.specular-r").innerText = this.planet._specular[0].toString();
-            document.querySelector(".og-value.specular-g").innerText = this.planet._specular[1].toString();
-            document.querySelector(".og-value.specular-b").innerText = this.planet._specular[2].toString();
-            document.querySelector(".og-value.shininess").innerText = this.planet._specular[3].toString();
+            // document.getElementById("ambient-r").value = this.planet._ambient[0];
+            // document.getElementById("ambient-g").value = this.planet._ambient[1];
+            // document.getElementById("ambient-b").value = this.planet._ambient[2];
+            //
+            // document.getElementById("diffuse-r").value = this.planet._diffuse[0];
+            // document.getElementById("diffuse-g").value = this.planet._diffuse[1];
+            // document.getElementById("diffuse-b").value = this.planet._diffuse[2];
+            //
+            // document.getElementById("specular-r").value = this.planet._specular[0];
+            // document.getElementById("specular-g").value = this.planet._specular[1];
+            // document.getElementById("specular-b").value = this.planet._specular[2];
+            // document.getElementById("shininess").value = this.planet._specular[3];
+            //
+            // document.querySelector(".og-value.ambient-r").innerText = this.planet._ambient[0].toString();
+            // document.querySelector(".og-value.ambient-g").innerText = this.planet._ambient[1].toString();
+            // document.querySelector(".og-value.ambient-b").innerText = this.planet._ambient[2].toString();
+            //
+            // document.querySelector(".og-value.diffuse-r").innerText = this.planet._diffuse[0].toString();
+            // document.querySelector(".og-value.diffuse-g").innerText = this.planet._diffuse[1].toString();
+            // document.querySelector(".og-value.diffuse-b").innerText = this.planet._diffuse[2].toString();
+            //
+            // document.querySelector(".og-value.specular-r").innerText = this.planet._specular[0].toString();
+            // document.querySelector(".og-value.specular-g").innerText = this.planet._specular[1].toString();
+            // document.querySelector(".og-value.specular-b").innerText = this.planet._specular[2].toString();
+            // document.querySelector(".og-value.shininess").innerText = this.planet._specular[3].toString();
         }
 
         this._fetchLayers();
