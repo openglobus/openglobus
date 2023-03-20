b19d16fd607f8d6d458dcaec7e82c1d3f26b8b8a
Pavel Sukhodolski <pavel.sukhodolski@microavia.com>
2023-03-20 13:07:25 +0400

    fix indices in object3d

--- src/og/Object3d.js
+++ src/og/Object3d.js
@@ -44,7 +44,7 @@ class Object3d {
             this._normals = data.normals || [];
         } else {
             this._normals = Object3d.getNormals(this._vertices);
-            this._indices = new Array(this._vertices.length);
+            this._indices = new Array(this._vertices.length/ 3);
             for (let i = 0, len = this._indices.length; i < len; i++) {
                 this._indices[i] = i;
             }
