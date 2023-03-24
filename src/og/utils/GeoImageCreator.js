a65ee5de4c499565ec0e1102bb1ac7da0bad8cfe
Zemledelec <mgevlich@gmail.com>
2023-03-24 11:06:37 +0400

    #646 added geoimage rte rendering

--- src/og/utils/GeoImageCreator.js
+++ src/og/utils/GeoImageCreator.js
@@ -4,6 +4,7 @@ import { LonLat } from '../LonLat.js';
 import * as utils from '../utils/shared.js';
 import { Framebuffer } from '../webgl/Framebuffer.js';
 import { Program } from '../webgl/Program.js';
+import { doubleToTwoFloats2 } from "../math/coder.js";
 
 export class GeoImageCreator {
 
@@ -32,35 +33,67 @@ export class GeoImageCreator {
      * @return{WebGLBuffer} Grid coordinates buffer.
      */
     createGridBuffer(c, toMerc) {
-        var gs = this._gridSize;
+        let gs = this._gridSize;
 
-        var v03 = new LonLat((c[3].lon - c[0].lon) / gs, (c[3].lat - c[0].lat) / gs),
+        let v03 = new LonLat((c[3].lon - c[0].lon) / gs, (c[3].lat - c[0].lat) / gs),
             v12 = new LonLat((c[2].lon - c[1].lon) / gs, (c[2].lat - c[1].lat) / gs),
             v01 = new LonLat((c[1].lon - c[0].lon) / gs, (c[1].lat - c[0].lat) / gs),
             v32 = new LonLat((c[2].lon - c[3].lon) / gs, (c[2].lat - c[3].lat) / gs);
 
-        var grid = new Float32Array((gs + 1) * (gs + 1) * 2);
-        var k = 0;
+        const size = (gs + 1) * (gs + 1) * 2,
+            halfSize = size / 2;
+
+        let gridHigh = new Float32Array(size),
+            gridLow = new Float32Array(size);
+
+        let lonLatArr = new Array(halfSize);
+
+        let kh = 0,
+            kl = 0,
+            k = 0;
+
+        let tempArr = new Float32Array(2);
+
         for (let i = 0; i <= gs; i++) {
-            var P03i = new LonLat(c[0].lon + i * v03.lon, c[0].lat + i * v03.lat),
+
+            let P03i = new LonLat(c[0].lon + i * v03.lon, c[0].lat + i * v03.lat),
                 P12i = new LonLat(c[1].lon + i * v12.lon, c[1].lat + i * v12.lat);
+
             for (let j = 0; j <= gs; j++) {
-                var P01j = new LonLat(c[0].lon + j * v01.lon, c[0].lat + j * v01.lat),
+                let P01j = new LonLat(c[0].lon + j * v01.lon, c[0].lat + j * v01.lat),
                     P32j = new LonLat(c[3].lon + j * v32.lon, c[3].lat + j * v32.lat);
-                var xx = utils.getLinesIntersectionLonLat(P03i, P12i, P01j, P32j);
-                grid[k++] = xx.lon;
-                grid[k++] = xx.lat;
+                let xx = utils.getLinesIntersectionLonLat(P03i, P12i, P01j, P32j);
+                doubleToTwoFloats2(xx.lon, tempArr);
+                gridHigh[kh++] = tempArr[0];
+                gridLow[kl++] = tempArr[1];
+                doubleToTwoFloats2(xx.lat, tempArr);
+                gridHigh[kh++] = tempArr[0];
+                gridLow[kl++] = tempArr[1];
+
+                lonLatArr[k++] = xx;
             }
         }
 
         if (toMerc) {
-            for (let i = 0; i < grid.length; i += 2) {
-                let c = new LonLat(grid[i], grid[i + 1]).forwardMercator();
-                grid[i] = c.lon;
-                grid[i + 1] = c.lat;
+            for (let i = 0; i < halfSize; i++) {
+                let c = lonLatArr[i].forwardMercator();
+                doubleToTwoFloats2(c.lon, tempArr);
+                gridHigh[i * 2] = tempArr[0];
+                gridLow[i * 2] = tempArr[1];
+
+                doubleToTwoFloats2(c.lat, tempArr);
+                gridHigh[i * 2 + 1] = tempArr[0];
+                gridLow[i * 2 + 1] = tempArr[1];
+
+                //grid[i] = c.lon;
+                //grid[i + 1] = c.lat;
             }
         }
-        return this._planet.renderer.handler.createArrayBuffer(grid, 2, grid.length / 2);
+
+        return [
+            this._planet.renderer.handler.createArrayBuffer(gridHigh, 2, halfSize),
+            this._planet.renderer.handler.createArrayBuffer(gridLow, 2, halfSize)
+        ];
     }
 
     frame() {
@@ -133,20 +166,27 @@ export class GeoImageCreator {
         this._planet.renderer.handler.addProgram(new Program("geoImageTransform", {
             uniforms: {
                 sourceTexture: "sampler2d",
-                extentParams: "vec4",
+                extentParamsHigh: "vec4",
+                extentParamsLow: "vec4",
                 isFullExtent: "bool"
             },
             attributes: {
-                corners: "vec2",
+                cornersHigh: "vec2",
+                cornersLow: "vec2",
                 texCoords: "vec2"
             },
-            vertexShader: `attribute vec2 corners; 
+            vertexShader:
+                `attribute vec2 cornersHigh; 
+                     attribute vec2 cornersLow;
                       attribute vec2 texCoords; 
-                      varying vec2 v_texCoords; 
-                      uniform vec4 extentParams; 
-                      void main() { 
+                      uniform vec4 extentParamsHigh; 
+                      uniform vec4 extentParamsLow; 
+                      varying vec2 v_texCoords;
+                      void main() {                                                             
                           v_texCoords = texCoords; 
-                          gl_Position = vec4((-1.0 + (corners - extentParams.xy) * extentParams.zw) * vec2(1.0, -1.0), 0.0, 1.0); 
+                          vec2 highDiff = cornersHigh - extentParamsHigh.xy;
+                          vec2 lowDiff = cornersLow - extentParamsLow.xy;                                        
+                          gl_Position = vec4((-1.0 + (highDiff + lowDiff) * extentParamsHigh.zw) * vec2(1.0, -1.0), 0.0, 1.0); 
                       }`,
             fragmentShader:
                 `precision highp float;
