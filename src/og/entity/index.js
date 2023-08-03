45d25601311e692b378a81930d2714dea20f11e7
Zemledelec <mgevlich@gmail.com>
2023-08-03 16:32:30 +0400

    #657 index cleanup

--- src/og/entity/index.js
+++ src/og/entity/index.js
@@ -5,5 +5,6 @@ import { Geometry } from "./Geometry.js";
 import { Label } from "./Label.js";
 import { PointCloud } from "./PointCloud.js";
 import { Polyline } from "./Polyline.js";
+import { GeoObject } from "./GeoObject.js";
 
-export { Entity, EntityCollection, Billboard, Geometry, Label, PointCloud, Polyline };
+export { Entity, EntityCollection, Billboard, Geometry, Label, PointCloud, Polyline, GeoObject };
