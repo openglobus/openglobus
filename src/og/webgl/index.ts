64a077622be1eb63ca163ea3fffce49e061e5244
Zemledelec <mgevlich@gmail.com>
2023-08-08 19:31:33 +0400

    #657 WIP.24 Framebuffer

---
+++ src/og/webgl/index.ts
@@ -0,0 +1,13 @@
+import { Framebuffer } from "./Framebuffer";
+import { Handler } from "./Handler";
+import { Multisample } from "./Multisample";
+import { Program } from "./Program";
+import { types } from "./types";
+
+export {
+    Framebuffer,
+    Handler,
+    Multisample,
+    types,
+    Program
+};
