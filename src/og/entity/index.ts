843ae2d6b2457ff5096e468baaae6f811e4463db
Zemledelec <mgevlich@gmail.com>
2023-08-11 14:29:11 +0400

    #657 WIP.29 Entity

--- src/og/entity/index.ts
+++ src/og/entity/index.ts
@@ -2,9 +2,11 @@ import { Billboard } from "./Billboard";
 import { Entity } from "./Entity";
 import { EntityCollection } from "./EntityCollection";
 import { Geometry } from "./Geometry";
+import { GeoObject } from "./GeoObject";
 import { Label } from "./Label";
 import { PointCloud } from "./PointCloud";
 import { Polyline } from "./Polyline";
-import { GeoObject } from "./GeoObject";
+import { Ray } from "./Ray";
+import { Strip } from "./Strip";
 
-export { Entity, EntityCollection, Billboard, Geometry, Label, PointCloud, Polyline, GeoObject };
+export {  Billboard, Entity, EntityCollection, Geometry, GeoObject, Label, PointCloud, Polyline, Ray, Strip };
