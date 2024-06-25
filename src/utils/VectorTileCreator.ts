56b97cdbeec6602e8e50ca6bbad9c983bc57ce7f
Zemledelec <mgevlich@gmail.com>
2024-06-25 17:01:25 +0400

    Fixed iMac jittering for VectorTile

--- src/utils/VectorTileCreator.ts
+++ src/utils/VectorTileCreator.ts
@@ -75,6 +75,7 @@ export class VectorTileCreator {
                 
                 vec2 proj(vec2 coordHigh, vec2 coordLow) {
                     vec2 highDiff = coordHigh - extentParamsHigh.xy;
+                    highDiff *= step(1.0, length(highDiff));
                     vec2 lowDiff = coordLow - extentParamsLow.xy;
                     return vec2(-1.0 + (highDiff + lowDiff) * extentParamsHigh.zw) * vec2(1.0, -1.0);
                 }
@@ -170,6 +171,7 @@ export class VectorTileCreator {
 
                 vec2 proj(vec2 coordHigh, vec2 coordLow) {
                     vec2 highDiff = coordHigh - extentParamsHigh.xy;
+                    highDiff *= step(1.0, length(highDiff));
                     vec2 lowDiff = coordLow - extentParamsLow.xy;
                     return vec2(-1.0 + (highDiff + lowDiff) * extentParamsHigh.zw) * vec2(1.0, -1.0);
                 }
