1a95a3dd36a90bfc6e37ab19eb211d45a39fe6c3
Zemledelec <mgevlich@gmail.com>
2024-07-21 13:44:40 +0400

    #824 WIP.3

--- sandbox/geoObject/geoObject.js
+++ sandbox/geoObject/geoObject.js
@@ -1,14 +1,5 @@
 import {
-    Globe,
-    control,
-    utils,
-    LonLat,
-    GlobusTerrain,
-    Vector,
-    OpenStreetMap,
-    Entity,
-    Bing,
-    GlobusRgbTerrain, Object3d
+    math, Globe, control, utils, LonLat, GlobusTerrain, Vector, OpenStreetMap, Entity, Bing, GlobusRgbTerrain, Object3d
 } from "../../lib/@openglobus/og.esm.js";
 
 
@@ -16,37 +7,53 @@ let myObjects = new Vector("MyObjects", {
     scaleByDistance: [200, 190000, 1]
 });
 
+function setPitch(a) {
+    myObjects.each((e) => {
+        e.geoObject.setPitch(a)
+    });
+}
+
+function setYaw(a) {
+    myObjects.each((e) => {
+        e.geoObject.setYaw(a);
+    });
+}
+
+function setRoll(a) {
+    myObjects.each((e) => {
+        e.geoObject.setRoll(a);
+    });
+}
+
 async function main() {
     let sat = new Bing();
     let osm = new OpenStreetMap();
 
     const planeObj3d = await Object3d.loadObj('./airplane.obj');
 
-    document.querySelector(".gpitch").addEventListener("change", (e) => {
-        setPitch();
+    document.querySelector(".gpitch").addEventListener("input", (e) => {
+        setPitch(Number(e.target.value) * math.RADIANS);
     });
-    document.querySelector(".gyaw").addEventListener("change", (e) => {
-        setYaw();
+    document.querySelector(".gyaw").addEventListener("input", (e) => {
+        setYaw(Number(e.target.value) * math.RADIANS);
     });
-    document.querySelector(".groll").addEventListener("change", (e) => {
-        setRoll();
+    document.querySelector(".groll").addEventListener("input", (e) => {
+        setRoll(Number(e.target.value) * math.RADIANS);
     });
 
-    for (let i = -70; i < 70; i += 10) {
+    for (let i = -80; i < 80; i += 10) {
         for (let j = -180; j < 180; j += 10) {
             myObjects.add(new Entity({
-                    lonlat: [j, i, 20000],
-                    geoObject: {
-                        color: "green",
-                        scale: 10.0,
-                        instanced: true,
-                        tag: "plane",
-                        object3d: planeObj3d[0],
-                        yaw: 0,
-                        pitch: 0
-                    }
-                })
-            );
+                lonlat: [j, i, 20000], geoObject: {
+                    color: "green",
+                    scale: 10.0,
+                    instanced: true,
+                    tag: "plane",
+                    object3d: planeObj3d[0],
+                    yaw: 0,
+                    pitch: 0
+                }
+            }));
         }
     }
 
@@ -57,7 +64,6 @@ async function main() {
         layers: [osm, sat, myObjects],
         atmosphereEnabled: false,
         fontsSrc: "../../res/fonts",
-        frustums: [[500000, 40000000]],
         sun: {
             stopped: false
         }
