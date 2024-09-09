6f47d1dae6d50ae96184bfe682c35688c9964321
Zemledelec <mgevlich@gmail.com>
2024-09-09 21:37:40 +0400

    moon ell

--- src/ellipsoid/moon.ts
+++ src/ellipsoid/moon.ts
@@ -4,4 +4,4 @@ import {Ellipsoid} from "./Ellipsoid";
  * Moon ellipsoid object.
  * @type {Ellipsoid}
  */
-export const moon = new Ellipsoid(1738100.0, 1736000.0); // Equitorial Radius (m), Polar Radius (m)
+export const moon = new Ellipsoid(1737400.0, 1737400.0); // Equitorial Radius (m), Polar Radius (m)
