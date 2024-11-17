9ab88ca53027948ff173fd8fafc1402ffd356ba6
Zemledelec <mgevlich@gmail.com>
2024-11-17 11:45:08 +0400

    WIP. properties dialog

--- src/control/geoObjectEditor/GeoObjectEditor.ts
+++ src/control/geoObjectEditor/GeoObjectEditor.ts
@@ -1,14 +1,19 @@
 import {Control, IControlParams} from "../Control";
 import {GeoObjectEditorScene} from "./GeoObjectEditorScene";
+import {Dialog} from "../../ui/Dialog";
+import {GeoObjectPropertiesDialog} from "./GeoObjectEditorDialog";
 
 export interface IGeoObjectEditorParams extends IControlParams {
 }
 
 export class GeoObjectEditor extends Control {
     protected _geoObjectEditopScene: GeoObjectEditorScene;
+    protected _dialog: GeoObjectPropertiesDialog;
 
     constructor(options: IGeoObjectEditorParams = {}) {
         super(options);
+
+        this._dialog = new GeoObjectPropertiesDialog();
 
         this._geoObjectEditopScene = new GeoObjectEditorScene({
             name: `geoObjectEditorScene:${this.__id}`
@@ -17,6 +22,16 @@ export class GeoObjectEditor extends Control {
 
     public override oninit() {
         this._geoObjectEditopScene.bindPlanet(this.planet!);
+        this._dialog.appendTo(this.planet!.renderer!.div!);
+
+        this._dialog.events.on("visibility", (v: boolean) => {
+            if (v) {
+                this.activate();
+            } else {
+                this.deactivate();
+            }
+        });
+
         this.activate();
     }
 
@@ -26,5 +41,6 @@ export class GeoObjectEditor extends Control {
 
     public override ondeactivate() {
         this.renderer && this.renderer.removeNode(this._geoObjectEditopScene);
+        this._dialog.hide();
     }
 }
