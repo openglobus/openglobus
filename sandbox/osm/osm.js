729f60cc4da3905cb9c351f8c9b02e1b3925071a
Zemledelec <mgevlich@gmail.com>
2025-01-06 13:22:04 +0400

    824 simple.14

--- sandbox/osm/osm.js
+++ sandbox/osm/osm.js
@@ -8,46 +8,112 @@ import {
     RgbTerrain,
     GlobusRgbTerrain,
     Object3d,
-    mercator
+    mercator,
+    Bing,
+    GeoVideo,
+    XYZ,
+    utils
 } from "../../lib/@openglobus/og.esm.js";
 
-let osm = new OpenStreetMap();
-
-let dockLayer = new Vector("dock", {
-    scaleByDistance: [1, 1, 1]
+var countries = new Vector("Countries", {
+    'visibility': true,
+    'isBaseLayer': false,
+    'diffuse': [0, 0, 0],
+    'ambient': [1, 1, 1]
 });
 
-dockLayer.add(new Entity({
-    lonlat: [0, 0, 10000],
-    geoObject: {
-        color: "red",
-        scale: 1.0,
-        instanced: true,
-        tag: `dock`,
-        object3d: Object3d.createCube(10000, 10000, 10000),
-        yaw: -52,
-        pitch: 0
+fetch("./countries.json")
+    .then(r => {
+        return r.json();
+    }).then(data => {
+
+    var f = data.features;
+    for (let i = 0; i < f.length; i++) {
+        var fi = f[i];
+        countries.add(new Entity({
+            'geometry': {
+                'type': fi.geometry.type,
+                'coordinates': fi.geometry.coordinates,
+                'style': {
+                    'fillColor': "rgba(255,255,255,0.6)"
+                }
+            }
+        }));
     }
-}));
+});
+
+let l0 = new XYZ("Stamen Watercolor", {
+    url: "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg",
+    attribution: '',
+    isBaseLayer: false,
+    maxNativeZoom: 19,
+    opacity: 0.5,
+    //defaultTextures: [{color: "#AAD3DF"}, {color: "#F2EFE9"}],
+    isSRGB: false,
+});
+
+let l2 = new XYZ("Stadia", {
+    url: "https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}.png",
+    attribution: '',
+    isBaseLayer: false,
+    maxNativeZoom: 19,
+    opacity: 0.5,
+
+    //defaultTextures: [{color: "#AAD3DF"}, {color: "#F2EFE9"}],
+    isSRGB: false,
+});
+
+let foursources = new GeoVideo("SOS_TaggedCO2_10-6-2023a_co2_foursources_quality_ScienceOnASphere_2048p30.mp4", {
+    src: "./SOS_TaggedCO2_10-6-2023a_co2_foursources_quality_ScienceOnASphere_2048p30.mp4",
+    corners: [[-180, 90], [180, 90], [180, -90], [-180, -90]],
+    visibility: true,
+    isBaseLayer: false,
+    opacity: 0.5,
+    attribution: 'Four Sources',
+    fullExtent: true
+});
+
+
+let imergac = new GeoVideo("USA precipitation 08.05.2016", {
+    minZoom: 0,
+    maxZoom: 10,
+    src: "imergac_20160508_NASA.mp4",
+    corners: [[-134.7904382939764, 55.07955352950936], [-54.984314759410594, 54.98843914299802], [-55.041854075913825, 19.820153025849297], [-134.89882012831265, 19.631495126944017]],
+    visibility: true,
+    isBaseLayer: false,
+    attribution: 'USA precipitation 08.05.2016, nasasearch.nasa.gov',
+    opacity: 0.7
+});
+
+let layers = [new OpenStreetMap(), new Bing("Micr.Bing", {
+    isBaseLayer: false
+}), countries, l0, l2, foursources, imergac];
+
+
+function setHeight(h) {
+    for (let i = 0; i < layers.length; i++) {
+        layers[i].setHeight(i * h * 10000);
+    }
+}
 
 const globus = new Globe({
-    //frustums: [[1,101100],[100000,1000000000]],
     target: "earth",
     name: "Earth",
-    terrain: new RgbTerrain("", {
-        maxNativeZoom: 13,
-        maxZoom: 13,
-        url: "http://127.0.0.1:8080/sandbox/osm/dest_geoid/{z}/{x}/{y}.png",
-    }),
-    layers: [osm, dockLayer],
+    terrain: new GlobusRgbTerrain(),
+    layers: layers,
     atmosphereEnabled: false,
     fontsSrc: "../../res/fonts",
     sun: {
         stopped: false
-    }
+    },
+    transitionOpacityEnabled: false
 });
 
 globus.planet.addControl(new control.DebugInfo());
-globus.planet.addControl(new control.KeyboardNavigation());
-globus.planet.addControl(new control.ToggleWireframe());
 globus.planet.addControl(new control.LayerSwitcher());
+
+window.setHeight = setHeight;
+
+document.body.querySelector("#slider").addEventListener("input", function (event) {
+    setHeight(parseFloat(event.target.value));
+})
