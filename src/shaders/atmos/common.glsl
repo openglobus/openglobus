07f1bad97fba0c770bcb8114ef68f566dac004a8
Zemledelec <mgevlich@gmail.com>
2025-04-14 01:51:17 +0400

    cleanup

--- src/shaders/atmos/common.glsl
+++ src/shaders/atmos/common.glsl
@@ -38,56 +38,56 @@ const float ozoneDensityWide = float(${ozoneDensityWide}); //15e3;
 
 vec3 sunWithBloom(vec3 rayDir, vec3 sunDir)
 {
-float minSunCosTheta = cos(SUN_ANGULAR_RADIUS);
-float cosTheta = dot(rayDir, sunDir);
+    float minSunCosTheta = cos(SUN_ANGULAR_RADIUS);
+    float cosTheta = dot(rayDir, sunDir);
 
-if (cosTheta >= minSunCosTheta)
-return vec3(1.0);
+    if (cosTheta >= minSunCosTheta)
+        return vec3(1.0);
 
-float offset = minSunCosTheta - cosTheta;
-float gaussianBloom = exp(- offset * 15000.0) * 0.7;
-float invBloom = 1.0 / (0.09 + offset * 200.0) * 0.01;
+    float offset = minSunCosTheta - cosTheta;
+    float gaussianBloom = exp(- offset * 15000.0) * 0.7;
+    float invBloom = 1.0 / (0.09 + offset * 200.0) * 0.01;
 
-return vec3(gaussianBloom + invBloom);
+    return vec3(gaussianBloom + invBloom);
 }
 
 float rayleighPhase(float angle)
 {
-return 3.0 / (16.0 * PI) * (1.0 + (angle * angle));
+    return 3.0 / (16.0 * PI) * (1.0 + (angle * angle));
 }
 
 float miePhase(float angle)
 {
-float g = 0.8;
-return 3.0 / (8.0 * PI) * ((1.0 - g * g) * (1.0 + angle * angle)) / ((2.0 + g * g) * pow(1.0 + g * g - 2.0 * g * angle, 1.5));
+    float g = 0.8;
+    return 3.0 / (8.0 * PI) * ((1.0 - g * g) * (1.0 + angle * angle)) / ((2.0 + g * g) * pow(1.0 + g * g - 2.0 * g * angle, 1.5));
 }
 
 vec3 opticalDepth(float height, float angle)
 {
-vec3 rayOrigin = vec3(0.0, BOTTOM_RADIUS + height, 0.0);
-vec3 rayDirection = vec3(sqrt(1.0 - angle * angle), angle, 0.0);
+    vec3 rayOrigin = vec3(0.0, BOTTOM_RADIUS + height, 0.0);
+    vec3 rayDirection = vec3(sqrt(1.0 - angle * angle), angle, 0.0);
 
-float t1, t2;
-intersectSphere(rayOrigin, rayDirection, TOP_RADIUS, t1, t2);
-float segmentLength = t2 / float(SAMPLE_COUNT);
+    float t1, t2;
+    intersectSphere(rayOrigin, rayDirection, TOP_RADIUS, t1, t2);
+    float segmentLength = t2 / float(SAMPLE_COUNT);
 
-float t = segmentLength * 0.5;
-vec3 opticalDepth = vec3(0.0);
+    float t = segmentLength * 0.5;
+    vec3 opticalDepth = vec3(0.0);
 
-for (int i = 0; i < SAMPLE_COUNT; i++)
-{
-vec3 position = rayOrigin + t * rayDirection;
-float height = length(position) - BOTTOM_RADIUS;
-opticalDepth.xy += exp(- height / rayleighMieHeights) * segmentLength;
-opticalDepth.z += (1.0 - min(abs(height - ozoneDensityHeight) / ozoneDensityWide, 1.0)) * segmentLength;
-t += segmentLength;
-}
+    for (int i = 0; i < SAMPLE_COUNT; i++)
+    {
+        vec3 position = rayOrigin + t * rayDirection;
+        float height = length(position) - BOTTOM_RADIUS;
+        opticalDepth.xy += exp(-height / rayleighMieHeights) * segmentLength;
+        opticalDepth.z += (1.0 - min(abs(height - ozoneDensityHeight) / ozoneDensityWide, 1.0)) * segmentLength;
+        t += segmentLength;
+    }
 
-return opticalDepth;
+    return opticalDepth;
 }
 
 vec3 transmittance(float height, float angle)
 {
-vec3 opticalDepth = opticalDepth(height, angle);
-return exp(- (rayleighScatteringCoefficient * opticalDepth.x + mieExtinctionCoefficient * opticalDepth.y + ozoneAbsorptionCoefficient * opticalDepth.z));
+    vec3 opticalDepth = opticalDepth(height, angle);
+    return exp(-(rayleighScatteringCoefficient * opticalDepth.x + mieExtinctionCoefficient * opticalDepth.y + ozoneAbsorptionCoefficient * opticalDepth.z));
 }
