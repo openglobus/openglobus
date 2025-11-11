58e0cbe82964637ab4bbfa8415a4e538a2a6b414
Zemledelec <mgevlich@gmail.com>
2025-11-11 21:43:20 +0400

    889 ee

--- src/shaders/ray/ray.frag.glsl
+++ src/shaders/ray/ray.frag.glsl
@@ -12,11 +12,15 @@ void main() {
     float min = v_texCoord.z;
     float height = v_texCoord.w;
 
-    float EPS = 0.5 / 1024.0; //Atlas height
+    if(height == 0.0){
+        gl_FragColor = v_rgba;
+    }else {
+        float EPS = 0.5 / 1024.0; //Atlas height
 
-    float localY = fract((uv.y + v_texOffset - min) / height * repeat);
-    uv.y = clamp(min + localY * height, min + EPS, min + height - EPS);
+        float localY = fract((uv.y + v_texOffset - min) / height * repeat);
+        uv.y = clamp(min + localY * height, min + EPS, min + height - EPS);
 
-    vec4 color = texture2D(texAtlas, uv);
-    gl_FragColor = v_rgba * color;
+        vec4 color = texture2D(texAtlas, uv);
+        gl_FragColor = v_rgba * color;
+    }
 }
