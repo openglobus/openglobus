6e041fa9e6044b1ff4a5d26a59bb755afc992aa8
mige <mikhail.gevlich@boskalis.com>
2026-02-02 17:29:42 +0400

    ortho rot works here

--- src/control/SimpleTouchNavigation.ts
+++ src/control/SimpleTouchNavigation.ts
@@ -75,12 +75,14 @@ export class SimpleTouchNavigation extends Control {
         let cam = r.activeCamera;
         const handler = r.handler;
         let grabbedPoint = r.getCartesianFromPixel(new Vec2(x, y));
-        let direction = cam.unproject(x, y);
+        let direction = cam.unproject(x, y, cam.eye.y);
+
         if (!grabbedPoint) {
             let p0 = new Vec3(0, 0, 0),
                 p1 = new Vec3(1, 0, 0),
                 p2 = new Vec3(0, 0, 1);
             let px = new Vec3();
+
             if (new Ray(cam.eye, direction).hitPlaneRes(Plane.fromPoints(p0, p1, p2), px) === Ray.INSIDE) {
                 grabbedPoint = px;
             } else {
@@ -90,7 +92,6 @@ export class SimpleTouchNavigation extends Control {
         return {
             grabbedPoint,
             screenPoint: new Vec2(x / handler.getWidth(), y / handler.getHeight()),
-            direction,
         }
     }
 
@@ -104,33 +105,50 @@ export class SimpleTouchNavigation extends Control {
         let pointers = sys.pointers;
 
         if (pointers.length === 1) {
-            let gp = this._getPoi(
+            let t0 = this._getPoi(
                 (pointers[0].clientX - sys.offsetLeft) * handler.pixelRatio,
                 (pointers[0].clientY - sys.offsetTop) * handler.pixelRatio
             );
 
-            this._grabbedPoint = gp.grabbedPoint;
-            this._grabbedScreenPoint = gp.screenPoint;
+            this._grabbedPoint = t0.grabbedPoint;
+            this._grabbedScreenPoint = t0.screenPoint;
 
         } else if (pointers.length === 2) {
 
-            this._prev_t0 = new Vec2(
+            let t0 = new Vec2(
                 (pointers[0].clientX - sys.offsetLeft) * handler.pixelRatio,
                 (pointers[0].clientY - sys.offsetTop) * handler.pixelRatio
             );
 
-            this._prev_t1 = new Vec2(
+            let t1 = new Vec2(
                 (pointers[1].clientX - sys.offsetLeft) * handler.pixelRatio,
                 (pointers[1].clientY - sys.offsetTop) * handler.pixelRatio
             );
 
-            let gp1 = this._getPoi(this._prev_t0.x, this._prev_t0.y);
-            let gp2 = this._getPoi(this._prev_t1.x, this._prev_t1.y);
+            this._prev_t0.copy(t0);
+            this._prev_t1.copy(t1);
 
-            this._grabbedPoint = gp1.grabbedPoint.add(gp2.grabbedPoint).scale(0.5);
-            this._grabbedScreenPoint = gp1.screenPoint.add(gp2.screenPoint).scale(0.5);
-            this._lookPos.copy(this._grabbedPoint);
+            let middle_t = t0.add(t1).scale(0.5);
+            this._grabbedScreenPoint = new Vec2(middle_t.x / handler.getWidth(), middle_t.y / handler.getHeight());
 
+            this._grabbedPoint = this.renderer.getCartesianFromPixel(middle_t);
+
+            if (!this._grabbedPoint) {
+                let cam = this.renderer.activeCamera;
+                let p0 = new Vec3(0, 0, 0),
+                    p1 = new Vec3(1, 0, 0),
+                    p2 = new Vec3(0, 0, 1);
+                let plane = Plane.fromPoints(p0, p1, p2);
+
+                let direction = cam.unproject(middle_t.x, middle_t.y, cam.eye.y);
+                let px = new Vec3();
+
+                if (new Ray(cam.eye, direction).hitPlaneRes(plane, px) === Ray.INSIDE) {
+                    this._grabbedPoint = px;
+                } else {
+                    this._grabbedPoint = cam.eye.add(direction.scale(10));
+                }
+            }
         } else {
             this._grabbedPoint = undefined;
             return;
@@ -212,6 +230,10 @@ export class SimpleTouchNavigation extends Control {
             let nx = middle.x / handler.getWidth() - this._grabbedScreenPoint.x;
             let ny = middle.y / handler.getHeight() - this._grabbedScreenPoint.y;
 
+            // const d0 = new Vec2(t0.x - this._prev_t0.x, t0.y - this._prev_t0.y);
+            // const d1 = new Vec2(t1.x - this._prev_t1.x, t1.y - this._prev_t1.y);
+            // const dot = d0.x * d1.x + d0.y * d1.y;
+
             if (cam.isOrthographic) {
                 let f = cam.frustum;
                 let dx = -(f.right - f.left) * nx,
@@ -237,37 +259,6 @@ export class SimpleTouchNavigation extends Control {
                 }
             }
 
-            // cam.update();
-            //
-            // let plane = new Plane(this._grabbedPoint, Vec3.UP);
-            // let dir_t0 = cam.unproject2v(t0);
-            // let dir_t1 = cam.unproject2v(t1);
-            //
-            // let px_t0 = new Vec3(),
-            //     px_t1 = new Vec3();
-            //
-            // if (new Ray(cam.eye, dir_t0).hitPlaneRes(plane, px_t0) === Ray.INSIDE) {
-            //
-            // }
-            //
-            // if (new Ray(cam.eye, dir_t1).hitPlaneRes(plane, px_t1) === Ray.INSIDE) {
-            //
-            // }
-
-            // const d0 = new Vec2(t0.x - this._prev_t0.x, t0.y - this._prev_t0.y);
-            // const d1 = new Vec2(t1.x - this._prev_t1.x, t1.y - this._prev_t1.y);
-            // const dot = d0.x * d1.x + d0.y * d1.y;
-            // const len0 = Math.sqrt(d0.x * d0.x + d0.y * d0.y);
-            // const len1 = Math.sqrt(d1.x * d1.x + d1.y * d1.y);
-            // const dead = 0.5; // px
-            //
-            // let c = getLinesIntersection2v(t0, t1, this._prev_t0, this._prev_t1, true);
-            //
-            // if (c) {
-            //
-            // }
-
-
             const vPrev = this._prev_t1.sub(this._prev_t0);
             const vCurr = t1.sub(t0);
 
@@ -276,16 +267,14 @@ export class SimpleTouchNavigation extends Control {
             const lenCurr = Math.hypot(vCurr.x, vCurr.y);
 
             let rotAngle = 0;
-
             if (lenPrev > dead && lenCurr > dead) {
-
-                // signed angle via atan2(cross, dot)
                 const dot = vPrev.x * vCurr.x + vPrev.y * vCurr.y;
                 const cross = vPrev.x * vCurr.y - vPrev.y * vCurr.x;
-
                 rotAngle = Math.atan2(cross, dot);
-
-                cam.rotateHorizontal(-rotAngle, false, this._lookPos, Vec3.UP);
+                cam.rotateHorizontal(-rotAngle, false, this._grabbedPoint, Vec3.UP);
+                if (cam.isOrthographic) {
+                    this.onTouchStart(e);
+                }
             }
 
             this._prev_t0.copy(t0);
