773a26c213e0e4cfd18ba2941a55ce722ec69d35
mige <mikhail.gevlich@boskalis.com>
2026-02-09 16:19:03 +0400

    cleanup

--- src/control/SimpleTouchNavigation.ts
+++ src/control/SimpleTouchNavigation.ts
@@ -228,65 +228,62 @@ export class SimpleTouchNavigation extends Control {
 
             const d0 = new Vec2(t0.x - this._prev_t0.x, t0.y - this._prev_t0.y);
             const d1 = new Vec2(t1.x - this._prev_t1.x, t1.y - this._prev_t1.y);
-            const dot = d0.x * d1.x + d0.y * d1.y;
+            //const dot = d0.x * d1.x + d0.y * d1.y;
 
-            // Pinch fingers move in opposite directions - ZOOM
-            if (/*dot < 0*/true) {
-                const vPrev = this._prev_t1.sub(this._prev_t0);
-                const vCurr = t1.sub(t0);
+            const vPrev = this._prev_t1.sub(this._prev_t0);
+            const vCurr = t1.sub(t0);
 
-                const lenPrev = Math.hypot(vPrev.x, vPrev.y);
-                const lenCurr = Math.hypot(vCurr.x, vCurr.y);
+            const lenPrev = Math.hypot(vPrev.x, vPrev.y);
+            const lenCurr = Math.hypot(vCurr.x, vCurr.y);
 
-                if (lenPrev > this._dead && lenCurr > this._dead) {
-                    let scale = lenPrev / lenCurr;
-                    if (scale < 0.25) scale = 0.25;
-                    if (scale > 4.0) scale = 4.0;
+            if (lenPrev > this._dead && lenCurr > this._dead) {
+                let scale = lenPrev / lenCurr;
+                if (scale < 0.25) scale = 0.25;
+                if (scale > 4.0) scale = 4.0;
 
-                    const anchor = this._grabbedPoint;
+                const anchor = this._grabbedPoint;
 
-                    if (cam.isOrthographic) {
-                        const fBefore = cam.frustum;
-                        const widthBefore = (fBefore.right - fBefore.left);
-                        const heightBefore = (fBefore.top - fBefore.bottom);
-                        const dxBefore = -(widthBefore) * (0.5 - this._grabbedScreenPoint.x);
-                        const dyBefore = (heightBefore) * (0.5 - this._grabbedScreenPoint.y);
-                        const worldBefore = cam.eye
-                            .add(cam.getRight().scale(dxBefore))
-                            .add(cam.getUp().scale(dyBefore));
+                if (cam.isOrthographic) {
+                    const fBefore = cam.frustum;
+                    const widthBefore = (fBefore.right - fBefore.left);
+                    const heightBefore = (fBefore.top - fBefore.bottom);
+                    const dxBefore = -(widthBefore) * (0.5 - this._grabbedScreenPoint.x);
+                    const dyBefore = (heightBefore) * (0.5 - this._grabbedScreenPoint.y);
+                    const worldBefore = cam.eye
+                        .add(cam.getRight().scale(dxBefore))
+                        .add(cam.getUp().scale(dyBefore));
 
-                        cam.focusDistance = Math.max(this._orthoMinFocusDistance, cam.focusDistance * scale);
-                        cam.update();
+                    cam.focusDistance = Math.max(this._orthoMinFocusDistance, cam.focusDistance * scale);
+                    cam.update();
 
-                        const fAfter = cam.frustum;
-                        const widthAfter = (fAfter.right - fAfter.left);
-                        const heightAfter = (fAfter.top - fAfter.bottom);
-                        const dxAfter = -(widthAfter) * (0.5 - this._grabbedScreenPoint.x);
-                        const dyAfter = (heightAfter) * (0.5 - this._grabbedScreenPoint.y);
-                        const worldAfter = cam.eye
-                            .add(cam.getRight().scale(dxAfter))
-                            .add(cam.getUp().scale(dyAfter));
+                    const fAfter = cam.frustum;
+                    const widthAfter = (fAfter.right - fAfter.left);
+                    const heightAfter = (fAfter.top - fAfter.bottom);
+                    const dxAfter = -(widthAfter) * (0.5 - this._grabbedScreenPoint.x);
+                    const dyAfter = (heightAfter) * (0.5 - this._grabbedScreenPoint.y);
+                    const worldAfter = cam.eye
+                        .add(cam.getRight().scale(dxAfter))
+                        .add(cam.getUp().scale(dyAfter));
 
-                        cam.eye = cam.eye.add(worldBefore.sub(worldAfter));
-                        this._eye0.copy(cam.eye);
+                    cam.eye = cam.eye.add(worldBefore.sub(worldAfter));
+                    this._eye0.copy(cam.eye);
+                } else {
+                    cam.eye = anchor.add(cam.eye.sub(anchor).scale(scale));
+
+                    const camSlope = Math.abs(cam.getForward().dot(Vec3.UP));
+                    let p1: Vec3, p2: Vec3;
+                    if (camSlope > 0.7) {
+                        p1 = Vec3.add(anchor, Vec3.LEFT);
+                        p2 = Vec3.add(anchor, cam.getRight());
                     } else {
-                        cam.eye = anchor.add(cam.eye.sub(anchor).scale(scale));
+                        p1 = Vec3.add(anchor, cam.getRight());
+                        p2 = Vec3.add(anchor, Vec3.UP);
+                    }
 
-                        const camSlope = Math.abs(cam.getForward().dot(Vec3.UP));
-                        let p1: Vec3, p2: Vec3;
-                        if (camSlope > 0.7) {
-                            p1 = Vec3.add(anchor, Vec3.LEFT);
-                            p2 = Vec3.add(anchor, cam.getRight());
-                        } else {
-                            p1 = Vec3.add(anchor, cam.getRight());
-                            p2 = Vec3.add(anchor, Vec3.UP);
-                        }
-
-                        const px = new Vec3();
-                        const dir = cam.unproject(middle.x, middle.y);
-                        if (new Ray(cam.eye, dir).hitPlaneRes(Plane.fromPoints(anchor, p1, p2), px) === Ray.INSIDE) {
-                            cam.eye = cam.eye.add(anchor.sub(px));
-                        }
+                    const px = new Vec3();
+                    const dir = cam.unproject(middle.x, middle.y);
+                    if (new Ray(cam.eye, dir).hitPlaneRes(Plane.fromPoints(anchor, p1, p2), px) === Ray.INSIDE) {
+                        cam.eye = cam.eye.add(anchor.sub(px));
                     }
                 }
             }
@@ -317,21 +314,14 @@ export class SimpleTouchNavigation extends Control {
                 }
             }
 
-            const vPrev = this._prev_t1.sub(this._prev_t0);
-            const vCurr = t1.sub(t0);
-
-            const lenPrev = Math.hypot(vPrev.x, vPrev.y);
-            const lenCurr = Math.hypot(vCurr.x, vCurr.y);
-
             let rotAngle = 0;
-            if (lenPrev > this._dead && lenCurr > this._dead) {
-                const dot = vPrev.x * vCurr.x + vPrev.y * vCurr.y;
-                const cross = vPrev.x * vCurr.y - vPrev.y * vCurr.x;
-                rotAngle = Math.atan2(cross, dot);
-                cam.rotateHorizontal(-rotAngle, false, this._grabbedPoint, Vec3.UP);
-                if (cam.isOrthographic) {
-                    this.onTouchStart(e, true);
-                }
+            const dot = vPrev.x * vCurr.x + vPrev.y * vCurr.y;
+            const cross = vPrev.x * vCurr.y - vPrev.y * vCurr.x;
+            rotAngle = Math.atan2(cross, dot);
+            cam.rotateHorizontal(-rotAngle, false, this._grabbedPoint, Vec3.UP);
+
+            if (cam.isOrthographic) {
+                this.onTouchStart(e, true);
             }
 
             this._prev_t0.copy(t0);
