12804e24fe556905bdfcafa5df0a1ac161fbd36d
Zemledelec <mgevlich@gmail.com>
2026-02-14 12:50:00 +0400

    wip.f

--- src/shaders/strip/strip.frag.glsl
+++ src/shaders/strip/strip.frag.glsl
@@ -8,7 +8,8 @@ layout(location=0) out vec4 accumColor;
 layout(location=1) out float accumAlpha;
 
 float weight(float z, float a) {
-    return clamp(pow(min(1.0, a * 10.0) + 0.01, 3.0) * 1e8 * pow(1.0 - z * 0.9, 3.0), 1e-2, 3e3);
+    return clamp(pow(min(1.0, a * 10.0) + 0.01, 3.0) * 3e3 * pow(1.0 - z, 3.0), 1e-6, 3e3);
+    //return clamp(pow(min(1.0, a * 10.0) + 0.01, 3.0) * 1e8 * pow(1.0 - z * 0.9, 3.0), 1e-2, 3e3);
 }
 
 void main(void) {
