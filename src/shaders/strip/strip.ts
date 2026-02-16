104e814e94973eb5153ba2e3fc45cc7ee7c3dfc3
Zemledelec <mgevlich@gmail.com>
2026-02-16 20:47:49 +0400

    wip.hhh

--- src/shaders/strip/strip.ts
+++ src/shaders/strip/strip.ts
@@ -2,9 +2,10 @@ import {Program} from '../../webgl/Program';
 
 import strip_vert from './strip.vert.glsl';
 import strip_frag from './strip.frag.glsl';
+import strip_opaque_frag from './stripOpaque.frag.glsl';
 
-export function stripScreen(): Program {
-    return new Program("strip", {
+export function stripTransparentScreen(): Program {
+    return new Program("stripTransparent", {
         uniforms: {
             projectionMatrix: "mat4",
             viewMatrix: "mat4",
@@ -19,5 +20,24 @@ export function stripScreen(): Program {
         },
         vertexShader: strip_vert,
         fragmentShader: strip_frag
+    });
+}
+
+export function stripForwardScreen(): Program {
+    return new Program("stripForward", {
+        uniforms: {
+            projectionMatrix: "mat4",
+            viewMatrix: "mat4",
+            eyePositionHigh: "vec3",
+            eyePositionLow: "vec3",
+            uColor: "vec4",
+            uOpacity: "float"
+        },
+        attributes: {
+            aVertexPositionHigh: "vec3",
+            aVertexPositionLow: "vec3"
+        },
+        vertexShader: strip_vert,
+        fragmentShader: strip_opaque_frag
     });
 }
