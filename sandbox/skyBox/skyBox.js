b12e0788b56fd2365562ea0733fd25b01cd84e5c
Zemledelec <mgevlich@gmail.com>
2026-04-04 02:09:43 +0400

    test x4

--- sandbox/skyBox/skyBox.js
+++ sandbox/skyBox/skyBox.js
@@ -73,7 +73,7 @@ let pointLayer = new Vector("points", {
 let globe = new Globe({
     target: "earth",
     name: "Earth",
-    //frustums: [[20, 200000000]],
+    frustums: [[100000, 200000000]],
     terrain: new GlobusRgbTerrain(),
     layers: [new OpenStreetMap(), new Bing(), objLayer, collection, pointLayer],
     msaa: 0
@@ -86,8 +86,8 @@ globe.planet.addControls([
     new control.TimelineControl(),
 ]);
 
-globe.planet.renderer.controls.SimpleSkyBackground.colorOne = "#555555";
-globe.planet.renderer.controls.SimpleSkyBackground.colorTwo = "#555555";
+//globe.planet.renderer.controls.SimpleSkyBackground.colorOne = "#555555";
+//globe.planet.renderer.controls.SimpleSkyBackground.colorTwo = "#555555";
 
 const EPSILON_NEAR = 1e-3;
 const planetDiameter = globe.planet.ellipsoid.equatorialSize * 2.0;
@@ -95,16 +95,13 @@ const planetDiameter = globe.planet.ellipsoid.equatorialSize * 2.0;
 function updateSkyBoxFrustum() {
     const camera = globe.planet.camera;
     const altitude = camera.getAltitude();
-    if (!Number.isFinite(altitude)) {
-        return;
-    }
-
-    const near = Math.max(altitude, EPSILON_NEAR);
-    camera.setNearFar(near, near + planetDiameter, 0);
+    const near = altitude;
+    console.log(near);
+    camera.setNearFar(near - near / 3, near + planetDiameter, 0);
 }
 
-// globe.planet.camera.events.on("viewchange", updateSkyBoxFrustum);
-// updateSkyBoxFrustum();
+globe.planet.camera.events.on("viewchange", updateSkyBoxFrustum);
+updateSkyBoxFrustum();
 
 let draggableObject = null;
 let dragStartClick = new Vec2();
