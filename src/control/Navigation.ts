2e61d19f947d0464ef4577d52a7b3d4e2c54fa33
Zemledelec <mgevlich@gmail.com>
2026-05-22 18:48:46 +0400

    Rollback to stable

--- src/control/Navigation.ts
+++ src/control/Navigation.ts
@@ -163,7 +163,6 @@ export class Navigation extends Control {
 
     protected _defaultVelInertia: number;
     protected _velInertia: number;
-    protected _prevDistanceToTarget: number;
 
     protected _hold: boolean = false;
 
@@ -194,7 +193,6 @@ export class Navigation extends Control {
         this.inertia = options.inertia ?? 1;
         this._defaultVelInertia = options.velInertia ?? DEFAULT_VELINERTIA;
         this._velInertia = this._defaultVelInertia;
-        this._prevDistanceToTarget = 0;
         this.minSlope = options.minSlope ?? MIN_SLOPE;
         this.dragInertia = options.dragInertia ?? DEFAULT_DRAG_INERTIA;
         this.zoomSpeed = options.zoomSpeed ?? 1;
@@ -430,11 +428,6 @@ export class Navigation extends Control {
 
     protected _getTargetPoint(p: Vec2): Vec3 | null {
         if (this.planet) {
-            const depthPoint = this.renderer!.getCartesianFromPixel(p);
-            if (depthPoint) {
-                return depthPoint;
-            }
-
             if (this.planet.camera.isOrthographic) {
                 return this.renderer!.getCartesianFromPixel(p) || null;
             }
@@ -448,10 +441,6 @@ export class Navigation extends Control {
         return null;
     }
 
-    protected _checkJump(distanceToTarget: number) {
-        return this._prevDistanceToTarget !== 0 && distanceToTarget > this._prevDistanceToTarget * 2;
-    }
-
     protected _onMouseWheel = (e: IMouseState) => {
         if (this.planet) {
             this._targetRotationPoint = null;
@@ -461,21 +450,20 @@ export class Navigation extends Control {
                 sy = e.y;
 
             let cam = this.planet.camera;
-            const wheelDirection = Math.sign(e.wheelDelta);
 
-            let _targetZoomPoint = this._getTargetPoint(new Vec2(sx, sy));
-            if (!_targetZoomPoint) return;
+            if (cam.isOrthographic) {
+                let _targetZoomPoint = this._getTargetPoint(new Vec2(sx, sy));
+                if (!_targetZoomPoint) return;
 
-            const distanceToTarget = cam.eye.distance(_targetZoomPoint);
+                this._targetZoomPoint = _targetZoomPoint;
+                this._grabbedSphere.radius = this._targetZoomPoint.length();
+            } else {
+                let _targetZoomPoint = this._getTargetPoint(new Vec2(sx, sy));
+                if (!_targetZoomPoint) return;
 
-            if (this._checkJump(distanceToTarget)) {
-                this._prevDistanceToTarget = 0;
-                return;
+                this._targetZoomPoint = _targetZoomPoint;
+                this._grabbedSphere.radius = this._targetZoomPoint.length();
             }
-            this._prevDistanceToTarget = distanceToTarget;
-
-            this._targetZoomPoint = _targetZoomPoint;
-            this._grabbedSphere.radius = this._targetZoomPoint.length();
 
             this._curPitch = cam.getPitch();
             this._curYaw = cam.getYaw();
@@ -943,7 +931,6 @@ export class Navigation extends Control {
         this.vel_h = 0;
         this.vel_v = 0;
         this._velInertia = this._defaultVelInertia;
-        this._prevDistanceToTarget = 0;
         this._targetZoomPoint = null;
         this._grabbedPoint = null;
         this._targetRotationPoint = null;
