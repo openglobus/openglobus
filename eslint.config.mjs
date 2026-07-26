3864d571db603ba9b08c7f99e49f2e8efb19bf6d
Zemledelec <mgevlich@gmail.com>
2026-07-26 19:30:09 +0400

    Updated packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,9 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import {fileURLToPath} from "node:url";
 import js from "@eslint/js";
-import {FlatCompat} from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
 export default [
-    ...compat.extends("eslint:recommended"),
+    js.configs.recommended,
 
     {
         plugins: {
@@ -44,6 +33,7 @@ export default [
             "no-unused-vars": 0,
             "no-useless-escape": 0,
             "no-loss-of-precision": 0,
+            "no-useless-assignment": 0,
         },
 
     },
