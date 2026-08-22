52709dbccbca723073e57d22f016489580c0fb1e
Zemledelec <mgevlich@gmail.com>
2026-08-22 09:27:36 +0400

    wip.xxx fixed order-based shadow maps: combining, empty texel reads as lit

--- src/control/depthCamera/DepthCamera.ts
+++ src/control/depthCamera/DepthCamera.ts
@@ -315,7 +315,7 @@ export class DepthCamera {
 
         const quadTreeStrategy = this._getQuadTreeStrategy(depthCamera);
 
-        gl.clearColor(0.0, 0.0, 0.0, 0.0);
+        gl.clearColor(1.0, 1.0, 1.0, 0.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         gl.disable(gl.BLEND);
 
